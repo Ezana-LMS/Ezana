1ac839a4a2947a0c4710e61bd6c0c1ee9dc47403
MartMbithi <martdevelopers254@gmail.com>
2020-04-17 11:12:00 +0300

    Add Calendar Event Backend Logic

---
+++ calendar/add_event.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../configs/config.php";
+
+$title = isset($_POST['title']) ? $_POST['title'] : "";
+$start = isset($_POST['start']) ? $_POST['start'] : "";
+$end = isset($_POST['end']) ? $_POST['end'] : "";
+
+$sqlInsert = "INSERT INTO ezanaLMS_Events (title, start, end) VALUES ('".$title."','".$start."','".$end ."')";
+
+$result = mysqli_query($mysqli, $sqlInsert);
+
+if (! $result) {
+    $result = mysqli_error($mysqli);
+}
