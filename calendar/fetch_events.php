90079a1eb0615159481579f8f4618d0212809a0e
MartMbithi <martdevelopers254@gmail.com>
2020-04-25 11:12:00 +0300

    Added Fetch Events From Calendar Event Backend Logic

---
+++ calendar/fetch_events.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "../configs/config.php";
+
+    $json = array();
+    $sqlQuery = "SELECT * FROM ezanaLMS_Events ORDER BY id";
+
+    $result = mysqli_query($mysqli, $sqlQuery);
+    $eventArray = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($eventArray, $row);
+    }
+    mysqli_free_result($result);
+
+    mysqli_close($mysqli);
+    echo json_encode($eventArray);
