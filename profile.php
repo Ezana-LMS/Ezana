3e2c7f87f495a9c70162b49b877180a5bea9d619
MartMbithi <martdevelopers254@gmail.com>
2020-10-19 15:59:56 +0300

    Minor Profile Update

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@ session_start();
 require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
-if (isset($_POST['change_profile'])) {
+if (isset($_POST['profile_update'])) {
 
     $id = $_SESSION['id'];
     $name = $_POST['name'];
@@ -196,8 +196,8 @@ require_once('partials/_head.php');
                                                         <div class="col-sm-10">
                                                             <div class="input-group">
                                                                 <div class="custom-file">
-                                                                    <input type="file" class="custom-file-input" id="exampleInputFile">
-                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                    <input type="file" name="profile_pic" class="custom-file-input" id="exampleInputFile">
+                                                                    <label class="custom-file-label"  for="exampleInputFile">Choose file</label>
                                                                 </div>
                                                             </div>
                                                         </div>
