120b5ec1b22cf71bf6645914a584c0f3d4b3d179
MartMbithi <martdevelopers254@gmail.com>
2020-10-19 16:14:19 +0300

    View On System Admins

---
+++ system_admins.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+        <!-- Main Sidebar Container -->
+        <?php require_once('partials/_sidebar.php'); ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>System Administrators</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item active">System Admins</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">System Administrators Accounts</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Names</th>
+                                            <th>Email</th>
+                                            <th>Rank</th>
+                                            <th>Phone Number</th>
+                                            <th>Address</th>
+                                            <th>Created At</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id ='$id' ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($admin = $res->fetch_object()) {
+                                            $cnt = 0;
+                                        ?>
+
+                                            <tr>
+                                                <td><?php echo $cnt; ?></td>
+                                                <td><?php echo $admin->name; ?></td>
+                                                <td><?php echo $admin->email; ?></td>
+                                                <td><?php echo $admin->rank; ?></td>
+                                                <td><?php echo $admin->phone; ?></td>
+                                                <td><?php echo $admin->adr; ?></td>
+                                                <td><?php echo date('d M Y', strtotime($admin->created_at)); ?></td>
+                                                <td>
+                                                    <a class="badge badge-success" href="view_admin.php?view=<?php echo $admin->id; ?>">
+                                                        <i class="fas fa-eye"></i>
+                                                        View
+                                                    </a>
+                                                    
+                                                    <a class="badge badge-danger" href="system_admins.php?delete=<?php echo $admin->id; ?>">
+                                                        <i class="fas fa-trash"></i>
+                                                        Delete
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        <?php $cnt = $cnt + 1;
+                                        } ?>
+
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <?php require_once('partials/_footer.php'); ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
