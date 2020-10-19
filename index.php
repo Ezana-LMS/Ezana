04d8d87e486b2c1ebde80c51f60b65883d468e03
MartMbithi <martdevelopers254@gmail.com>
2020-10-19 10:27:35 +0300

    UI Auth

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+require_once('partials/_head.php')
+?>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="card">
+            <div class="card-body login-card-body">
+                <div class="login-logo">
+                    <img class="img-thumbnail" height="150" width="150" src="dist/img/logo.jpeg" alt="">
+                </div>
+                <p class="login-box-msg">Sign In To Start Your Session</p>
+
+                <form method="post">
+                    <div class="input-group mb-3">
+                        <input type="email" required name="email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" required name="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <!--  <div class="social-auth-links text-center mb-3">
+                    <p>- OR -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                    </a>
+                </div> -->
+
+                <p class="mb-1">
+                    <a href="forgot-password.php">I forgot my password</a>
+                </p>
+                <!-- <p class="mb-0">
+                    <a href="register.html" class="text-center">Register a new membership</a>
+                </p> -->
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
---
+++ partials/_scripts.php
@@ -0,0 +1,7 @@
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Ezana App Js -->
+    <script src="dist/js/adminlte.min.js"></script>
