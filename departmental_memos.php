cc9eb8f1163b955c2ced9329bb6e5604a505f82f
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 14:58:06 +0300

    Delete On Departmental Memos

--- departmental_memos.php
+++ departmental_memos.php
@@ -3,7 +3,20 @@ session_start();
 require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
-
+//Delete
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_DepartmentalMemos WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=departmental_memos.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
 require_once('partials/_head.php');
 
 ?>
