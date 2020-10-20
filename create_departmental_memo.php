8a28c0feaa94c1e3aa85e63b724601305631a820
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 15:13:01 +0300

    Departmental Memo Front End

--- create_departmental_memo.php
+++ create_departmental_memo.php
@@ -45,14 +45,15 @@ require_once('partials/_head.php');
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Add New Faculty</h1>
+                            <h1>Add New Departmental Memo</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="manage_faculties.php">Faculties</a></li>
-                                <li class="breadcrumb-item active">Add</li>
+                                <li class="breadcrumb-item"><a href="departmental_memos.php">Departments</a></li>
+                                <li class="breadcrumb-item"><a href="departmental_memos.php">Departmental Memos</a></li>
+                                <li class="breadcrumb-item active">Add Memo</li>
                             </ol>
                         </div>
                     </div>
@@ -66,32 +67,36 @@ require_once('partials/_head.php');
                         <!-- general form elements -->
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title">Fill All Required Fields</h3>
+                                <h3 class="card-title">Fill Required Fields</h3>
                             </div>
                             <!-- /.card-header -->
                             <!-- form start -->
                             <form method="post" enctype="multipart/form-data" role="form">
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Faculty Name</label>
-                                            <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
+                                        <div class="form-group col-md-12">
                                             <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
                                         </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Faculty Number / Code</label>
-                                            <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                        <div class="form-group col-md-12">
+                                            <label for="">Upload Departmental Memo (PDF Or Docx)</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input required name="attachments" type="file" class="custom-file-input">
+                                                    <label class="custom-file-label" for="exampleInputFile">Choose file </label>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
+                                    <h2 class="text-center">Or </h2>
                                     <div class="row">
                                         <div class="form-group col-md-12">
-                                            <label for="exampleInputPassword1">Faculty Description</label>
-                                            <textarea required name="details" rows="10" class="form-control"></textarea>
+                                            <label for="exampleInputPassword1">Type Departmental Memo</label>
+                                            <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer">
-                                    <button type="submit" name="add_faculty" class="btn btn-primary">Add Faculty</button>
+                                    <button type="submit" name="add_memo" class="btn btn-primary">Add Departmental Memo</button>
                                 </div>
                             </form>
                         </div>
