df30b63f5d7e8fd524846f3dacf5c776d40fdfb0
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 14:28:43 +0300

    View / Delete On Departments Per Faculty

---
+++ view_faculty_department.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+        <!-- Main Sidebar Container -->
+        <?php
+        require_once('partials/_sidebar.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Departments` WHERE faculty_name ='$faculty'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($dep = $res->fetch_object()) {
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Departments Under <?php echo $dep->faculty_name; ?></h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="add_department.php">Departments</a></li>
+                                    <li class="breadcrumb-item"><a href=""><?php echo $dep->faculty_name; ?></a></li>
+                                    <li class="breadcrumb-item active">Departments</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <!-- <h2 class="text-right">
+                                    <a class="btn btn-outline-success" href="add_faculties.php">
+                                        <i class="fas fa-user-plus"></i>
+                                        Register New Faculty
+                                    </a>
+                                    <a class="btn btn-outline-primary" href="">
+                                        <i class="fas fa-file-excel"></i>
+                                        Import Faculties From .XLS File
+                                    </a>
+                                </h2> -->
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Dep Code / Number</th>
+                                                <th>Dep Name</th>
+                                                <th>Dep HOD</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $ret = "SELECT * FROM `ezanaLMS_Departments` WHERE faculty_name ='$faculty'  ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($dep = $res->fetch_object()) {
+                                            ?>
+
+                                                <tr>
+                                                    <td><?php echo $cnt; ?></td>
+                                                    <td><?php echo $dep->code; ?></td>
+                                                    <td><?php echo $dep->name; ?></td>
+                                                    <td><?php echo $dep->hod; ?></td>
+                                                    <td>
+                                                        <a class="badge badge-primary" href="update_department.php?update=<?php echo $dep->id; ?>">
+                                                            <i class="fas fa-edit"></i>
+                                                            Update
+                                                        </a>
+
+                                                        <a class="badge badge-damger" href="view_faculty_department.php?delete=<?php echo $dep->id; ?>&faculty=<?php echo $dep->faculty_name; ?>">
+                                                            <i class="fas fa-tarsh"></i>
+                                                            Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            <?php $cnt = $cnt + 1;
+                                            } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </section>
+                <!-- /.content -->
+            </div>
+        <?php require_once('partials/_footer.php');
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
