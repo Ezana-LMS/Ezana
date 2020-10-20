dd2be561b13d3289589827d981b6aa341f7f5745
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 14:06:19 +0300

    Update On Faculties

--- update_faculty.php
+++ update_faculty.php
@@ -20,21 +20,20 @@ if (isset($_POST['update_faculty'])) {
         $err = "Faculty Name Cannot Be Empty";
     }
     if (!$error) {
-            $update = $_GETT['update'];
-            $name = $_POST['name'];
-            $code = $_POST['code'];
-            $details = $_POST['details'];
+        $update = $_GETT['update'];
+        $name = $_POST['name'];
+        $code = $_POST['code'];
+        $details = $_POST['details'];
 
-            $query = "UPDATE  ezanaLMS_Faculties SET code =?, name =?, details =? WHERE id =?";
-            $stmt = $mysqli->prepare($query);
-            $rc = $stmt->bind_param('ssss', $code, $name, $details, $update);
-            $stmt->execute();
-            if ($stmt) {
-                $success = "Faculty Added" && header("refresh:1; url=manage_faculties.php");
-            } else {
-                //inject alert that profile update task failed
-                $info = "Please Try Again Or Try Later";
-            }
+        $query = "UPDATE  ezanaLMS_Faculties SET code =?, name =?, details =? WHERE id =?";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssss', $code, $name, $details, $update);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Faculty Added" && header("refresh:1; url=manage_faculties.php");
+        } else {
+            //inject alert that profile update task failed
+            $info = "Please Try Again Or Try Later";
         }
     }
 }
@@ -48,71 +47,79 @@ require_once('partials/_head.php');
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <?php require_once('partials/_sidebar.php'); ?>
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>Add New Faculty</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="manage_faculties.php">Faculties</a></li>
-                                <li class="breadcrumb-item active">Add</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h3 class="card-title">Fill All Required Fields</h3>
+        <?php
+        require_once('partials/_sidebar.php');
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id ='$update' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($faculty = $res->fetch_object()) {
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Update <?php echo $faculty->name; ?></h1>
                             </div>
-                            <!-- /.card-header -->
-                            <!-- form start -->
-                            <form method="post" enctype="multipart/form-data" role="form">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Faculty Name</label>
-                                            <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
-                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="manage_faculties.php">Faculties</a></li>
+                                    <li class="breadcrumb-item active">Update <?php echo $faculty->name; ?></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Fill All Required Fields</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form method="post" enctype="multipart/form-data" role="form">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="form-group col-md-6">
+                                                <label for="">Faculty Name</label>
+                                                <input type="text" value="<?php echo $faculty->name; ?>" required name="name" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Faculty Number / Code</label>
+                                                <input type="text" required name="code" value="<?php echo $faculty->code; ?>" class="form-control">
+                                            </div>
                                         </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Faculty Number / Code</label>
-                                            <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="exampleInputPassword1">Faculty Description</label>
+                                                <textarea required name="details" rows="10" class="form-control"><?php echo $faculty->details; ?></textarea>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="form-group col-md-12">
-                                            <label for="exampleInputPassword1">Faculty Description</label>
-                                            <textarea required name="details" rows="10" class="form-control"></textarea>
-                                        </div>
+                                    <div class="card-footer">
+                                        <button type="submit" name="update_faculty" class="btn btn-primary">Update Faculty</button>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <button type="submit" name="add_faculty" class="btn btn-primary">Add Faculty</button>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
-        <?php require_once('partials/_footer.php'); ?>
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+        <?php require_once('partials/_footer.php');
+        } ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>
