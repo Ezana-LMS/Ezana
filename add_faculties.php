b11e2fe94d3eb419699ab6b2227b66f1121c6d6d
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 13:19:06 +0300

    Create On Faculties

--- add_faculties.php
+++ add_faculties.php
@@ -73,7 +73,7 @@ require_once('partials/_head.php');
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="manage_lectures.php">Lecturers</a></li>
+                                <li class="breadcrumb-item"><a href="">Faculties</a></li>
                                 <li class="breadcrumb-item active">Add</li>
                             </ol>
                         </div>
@@ -95,55 +95,25 @@ require_once('partials/_head.php');
                             <form method="post" enctype="multipart/form-data" role="form">
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="form-group col-md-4">
-                                            <label for="">Name</label>
+                                        <div class="form-group col-md-6">
+                                            <label for="">Faculty Name</label>
                                             <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
                                             <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
                                         </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="">Number</label>
-                                            <input type="text" required name="number" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="">ID / Passport Number</label>
-                                            <input type="text" required name="idno" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="row">
                                         <div class="form-group col-md-6">
-                                            <label for="">Email</label>
-                                            <input type="email" required name="email" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Phone Number</label>
-                                            <input type="text" required name="phone" class="form-control">
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Password</label>
-                                            <input type="password" required name="password" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Profile Picture</label>
-                                            <div class="input-group">
-                                                <div class="custom-file">
-                                                    <input required name="profile_pic" type="file" class="custom-file-input">
-                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                                </div>
-                                            </div>
+                                            <label for="">Faculty Number / Code</label>
+                                            <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="form-group col-md-12">
-                                            <label for="exampleInputPassword1">Address</label>
-                                            <textarea required name="adr" rows="5" class="form-control"></textarea>
+                                            <label for="exampleInputPassword1">Faculty Description</label>
+                                            <textarea required name="details" rows="10" class="form-control"></textarea>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer">
-                                    <button type="submit" name="add_lec" class="btn btn-primary">Submit</button>
+                                    <button type="submit" name="add_faculty" class="btn btn-primary">Add Faculty</button>
                                 </div>
                             </form>
                         </div>
