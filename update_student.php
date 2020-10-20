336ca9d9eafb05cdc98d0e92618a5ac7f1deb02c
MartMbithi <martdevelopers254@gmail.com>
2020-10-20 12:41:09 +0300

    Update On Students

---
+++ update_student.php
@@ -0,0 +1,222 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+require_once('configs/codeGen.php');
+check_login();
+if (isset($_POST['update_student'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['admno']) && !empty($_POST['admno'])) {
+        $admno = mysqli_real_escape_string($mysqli, trim($_POST['admno']));
+    } else {
+        $error = 1;
+        $err = "Admission  Number Cannot Be Empty";
+    }
+    if (isset($_POST['idno']) && !empty($_POST['idno'])) {
+        $idno = mysqli_real_escape_string($mysqli, trim($_POST['idno']));
+    } else {
+        $error = 1;
+        $err = "National ID / Passport Number Cannot Be Empty";
+    }
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot Be Empty";
+    }
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $phone = mysqli_real_escape_string($mysqli, trim($_POST['phone']));
+    } else {
+        $error = 1;
+        $err = "Phone Number Cannot Be Empty";
+    }
+
+    if (!$error) {
+        $update = $_GET['update'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $admno = $_POST['admno'];
+        $idno  = $_POST['idno'];
+        $adr = $_POST['adr'];
+        $dob = $_POST['dob'];
+        $gender = $_POST['gender'];
+        $acc_status = $_POST['acc_status'];
+        $updated_at = date('d M Y');
+        $profile_pic = $_FILES['profile_pic']['name'];
+        move_uploaded_file($_FILES["profile_pic"]["tmp_name"], "dist/img/students/" . $_FILES["profile_pic"]["name"]);
+
+        $query = "UPDATE ezanaLMS_Students SET name =?, email =?, phone =?, admno =?, idno =?, adr =?, dob =?, gender =?, acc_status =?, updated_at =?, profile_pic =? WHERE id =?";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssssssssssss', $name, $email, $phone, $admno,  $idno, $adr, $dob, $gender, $acc_status, $updated_at, $profile_pic, $update);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Updated Profile"; // && header("refresh:1; url=manage_students.php");
+        } else {
+            //inject alert that profile update task failed
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
+
+require_once('partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php
+        require_once('partials/_sidebar.php');
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `ezanaLMS_Students` WHERE id ='$update'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($std = $res->fetch_object()) {
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Update <?php echo $std->name; ?> Details</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="manage_students.php">Students</a></li>
+                                    <li class="breadcrumb-item active">Update <?php echo $std->name; ?> Profile</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Fill All Required Fields</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form method="post" enctype="multipart/form-data" role="form">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="form-group col-md-4">
+                                                <label for="">Name</label>
+                                                <input type="text" required name="name" value="<?php echo $std->name; ?>" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Admission Number</label>
+                                                <input type="text" required name="admno" value="<?php echo $std->admno; ?>" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">ID / Passport Number</label>
+                                                <input type="text" value="<?php echo $std->idno; ?>" required name="idno" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="form-group col-md-4">
+                                                <label for="">Date Of Birth</label>
+                                                <input type="text" value="<?php echo $std->dob; ?>" required name="dob" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Gender</label>
+                                                <select type="text" required name="gender" class="form-control">
+                                                    <option selected><?php echo $std->gender; ?></option>
+                                                    <option>Male</option>
+                                                    <option>Female</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Student Account Status</label>
+                                                <select type="text" required name="gender" class="form-control">
+                                                    <option selected><?php echo $std->acc_status; ?></option>
+                                                    <option>Active</option>
+                                                    <option>Disabled</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="form-group col-md-4">
+                                                <label for="">Email</label>
+                                                <input value="<?php echo $std->email; ?>" type="email" required name="email" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Phone Number</label>
+                                                <input type="text" value="<?php echo $std->phone; ?>" required name="phone" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Profile Picture</label>
+                                                <div class="input-group">
+                                                    <div class="custom-file">
+                                                        <input required name="profile_pic" type="file" class="custom-file-input">
+                                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="exampleInputPassword1">Address</label>
+                                                <textarea required name="adr" rows="5" class="form-control"><?php echo $std->adr; ?></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <button type="submit" name="update_student" class="btn btn-primary">Update Students Profile</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Student Authentication Credentials</h3>
+                                </div>
+                                <!-- form start -->
+                                <form method="post" enctype="multipart/form-data" role="form">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="">New Password</label>
+                                                <input type="password" required name="new_password" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <label for="">Confirm Password</label>
+                                                <input type="password" required name="confirm_password" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <button type="submit" name="change_passsword" class="btn btn-primary">Change Password</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+        <?php require_once('partials/_footer.php');
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
