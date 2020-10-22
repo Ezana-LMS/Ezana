971ce5c6cf1b8c0e43a6d2ff5ccbd8594816c658
MartMbithi <martdevelopers254@gmail.com>
2020-10-22 13:09:12 +0300

    Integrated DROPPIFY Plugin

--- update_group_project.php
+++ update_group_project.php
@@ -4,7 +4,7 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 require_once('configs/codeGen.php');
 check_login();
-if (isset($_POST['add_group_project'])) {
+if (isset($_POST['update_group_project'])) {
 
     $update = $_GET['update'];
     $details = $_POST['details'];
@@ -36,12 +36,13 @@ require_once('partials/_head.php');
         <!-- Main Sidebar Container -->
         <?php
         require_once('partials/_sidebar.php');
-        $group_code = $_GET['group_code'];
-        $ret = "SELECT * FROM `ezanaLMS_Groups` WHERE code = '$group_code'  ";
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `ezanaLMS_GroupsAssignments` WHERE id = '$update'  ";
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
-        while ($g = $res->fetch_object()) {
+        while ($gcode = $res->fetch_object()) {
+
         ?>
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper">
@@ -50,7 +51,7 @@ require_once('partials/_head.php');
                     <div class="container-fluid">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1>Add <?php echo $g->name; ?> Project</h1>
+                                <h1>Update <?php echo $gcode->group_name; ?> Project</h1>
                             </div>
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
@@ -58,7 +59,7 @@ require_once('partials/_head.php');
                                     <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                                     <li class="breadcrumb-item"><a href="manage_groups.php">Groups</a></li>
                                     <li class="breadcrumb-item"><a href="group_projects.php">Group Projects</a></li>
-                                    <li class="breadcrumb-item active">Add Project</li>
+                                    <li class="breadcrumb-item active">Update</li>
                                 </ol>
                             </div>
                         </div>
@@ -83,13 +84,13 @@ require_once('partials/_head.php');
                                             </div>
                                             <div class="form-group col-md-12">
                                                 <label for="exampleInputPassword1">Submission Date </label>
-                                                <input type="date" required name="submitted_on" class="form-control">
+                                                <input type="date" value="<?php echo $gcode->submitted_on; ?>" required name="submitted_on" class="form-control">
                                             </div>
                                             <div class="form-group col-md-12">
                                                 <label for="">Upload Group Project / Assignment (PDF Or Docx)</label>
                                                 <div class="input-group">
                                                     <div class="custom-file">
-                                                        <input name="attachments" type="file" class="custom-file-input">
+                                                        <input data-default-file="dist/Group_Projects/<?php echo $gcode->attachments;?>" data-max-file-size="5M" name="attachments" type="file" class="custom-file-input">
                                                         <label class="custom-file-label" for="exampleInputFile">Choose file </label>
                                                     </div>
                                                 </div>
@@ -99,12 +100,12 @@ require_once('partials/_head.php');
                                         <div class="row">
                                             <div class="form-group col-md-12">
                                                 <label for="exampleInputPassword1">Type Group Project / Assignment Or Project Description </label>
-                                                <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
+                                                <textarea name="details" id="textarea" rows="10" class="form-control"><?php echo $gcode->details; ?></textarea>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="card-footer">
-                                        <button type="submit" name="add_group_project" class="btn btn-primary">Submit</button>
+                                        <button type="submit" name="update_group_project" class="btn btn-primary">Submit</button>
                                     </div>
                                 </form>
                             </div>
@@ -117,5 +118,4 @@ require_once('partials/_head.php');
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>
-
 </html>
