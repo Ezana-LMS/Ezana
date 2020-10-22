d544b0539bf91b6316ebf9a6c86da22d0412c988
MartMbithi <martdevelopers254@gmail.com>
2020-10-22 12:18:52 +0300

    Intergrated File Upload API and CK Editor

--- add_group_project.php
+++ add_group_project.php
@@ -12,7 +12,7 @@ if (isset($_POST['add_group_project'])) {
     $type = $_GET['type'];
     $details = $_POST['details'];
     $attachments = $_FILES['attachments']['name'];
-    move_uploaded_file($_FILES["attachments"]["tmp_name"], "dist/Group_Projects/" . $_FILES["attachments"]["name"]); 
+    move_uploaded_file($_FILES["attachments"]["tmp_name"], "dist/Group_Projects/" . $_FILES["attachments"]["name"]);
     $created_at = date('d M Y g:i');
     $submitted_on = $_POST['submitted_on'];
     $query = "INSERT INTO ezanaLMS_GroupsAssignments (id, group_code, group_name, attachments, type, details, created_at, submitted_on) VALUES(?,?,?,?,?,?,?,?)";
@@ -20,7 +20,7 @@ if (isset($_POST['add_group_project'])) {
     $rc = $stmt->bind_param('ssssssss', $id, $group_code, $group_name, $attachments,  $type, $details, $created_at, $submitted_on);
     $stmt->execute();
     if ($stmt) {
-        $success = "Group Assignment / Project Submitted" && header("refresh:1; url=add_group_project.php?group_code=$group_code&group_name=$group_name");
+        $success = "Group Assignment / Project Submitted" && header("refresh:1; url=add_group_project.php?group_code=$group_code&group_name=$group_name&type=Project");
     } else {
         $info = "Please Try Again Or Try Later";
     }
@@ -59,7 +59,7 @@ require_once('partials/_head.php');
                                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                                     <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                                     <li class="breadcrumb-item"><a href="manage_groups.php">Groups</a></li>
-                                    <li class="breadcrumb-item"><a href="">Group Projects</a></li>
+                                    <li class="breadcrumb-item"><a href="group_projects.php">Group Projects</a></li>
                                     <li class="breadcrumb-item active">Add Project</li>
                                 </ol>
                             </div>
@@ -88,7 +88,7 @@ require_once('partials/_head.php');
                                                 <input type="date" required name="submitted_on" class="form-control">
                                             </div>
                                             <div class="form-group col-md-12">
-                                                <label for="">Upload Group Project / Assignment   (PDF Or Docx)</label>
+                                                <label for="">Upload Group Project / Assignment (PDF Or Docx)</label>
                                                 <div class="input-group">
                                                     <div class="custom-file">
                                                         <input name="attachments" type="file" class="custom-file-input">
@@ -97,9 +97,10 @@ require_once('partials/_head.php');
                                                 </div>
                                             </div>
                                         </div>
+                                        <h2 class="text-center">Or</h2>
                                         <div class="row">
                                             <div class="form-group col-md-12">
-                                                <label for="exampleInputPassword1">Group Project / Assignment</label>
+                                                <label for="exampleInputPassword1">Type Group Project / Assignment</label>
                                                 <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
                                             </div>
                                         </div>
