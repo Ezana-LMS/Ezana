ed1247d62ad35c811df786fe051f6c75b096c017
MartMbithi <martdevelopers254@gmail.com>
2020-10-22 10:52:07 +0300

    Details

--- add_groups.php
+++ add_groups.php
@@ -120,7 +120,12 @@ require_once('partials/_head.php');
                                             <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
                                         </div>
                                     </div>
-
+                                    <div class="row">
+                                        <div class="form-group col-md-12">
+                                            <label for="exampleInputPassword1">Group Description</label>
+                                            <textarea required id="textarea" name="details" rows="10" class="form-control"></textarea>
+                                        </div>
+                                    </div>
                                     <!-- <div class="row">
                                         <div class="form-group col-md-6">
                                             <label for="">Student Admission Number</label>
@@ -142,12 +147,6 @@ require_once('partials/_head.php');
                                             <input type="text" id="StudentName" readonly required name="student_name" class="form-control">
                                         </div>
                                     </div> -->
-                                </div>
-                                <div class="row">
-                                    <div class="form-group col-md-12">
-                                        <label for="exampleInputPassword1">Group Description</label>
-                                        <textarea required id="textarea" name="details" rows="10" class="form-control"></textarea>
-                                    </div>
                                 </div>
                                 <div class="card-footer">
                                     <button type="submit" name="add_group" class="btn btn-primary">Add Group</button>
--- update_group.php
+++ update_group.php
@@ -4,7 +4,7 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 require_once('configs/codeGen.php');
 check_login();
-if (isset($_POST['add_group'])) {
+if (isset($_POST['update_group'])) {
     //Error Handling and prevention of posting double entries
     $error = 0;
     if (isset($_POST['code']) && !empty($_POST['code'])) {
@@ -46,7 +46,7 @@ if (isset($_POST['add_group'])) {
         $rc = $stmt->bind_param('sssss', $name, $code, $updated_at, $details, $update);
         $stmt->execute();
         if ($stmt) {
-            $success = "Student Group  Updated" && header("refresh:1; url=manage_group.php");
+            $success = "Student Group  Updated" && header("refresh:1; url=manage_groups.php");
         } else {
             $info = "Please Try Again Or Try Later";
         }
@@ -62,69 +62,79 @@ require_once('partials/_head.php');
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <?php require_once('partials/_sidebar.php'); ?>
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>Update <?php echo $g->name; ?></h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="manage_groups.php">Groups</a></li>
-                                <li class="breadcrumb-item active">Update</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h3 class="card-title">Fill All Required Fields</h3>
+        <?php
+        require_once('partials/_sidebar.php');
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `ezanaLMS_Groups` WHERE id ='$update'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        $cnt = 1;
+        while ($g = $res->fetch_object()) {
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Update <?php echo $g->name; ?></h1>
                             </div>
-                            <!-- /.card-header -->
-                            <!-- form start -->
-                            <form method="post" enctype="multipart/form-data" role="form">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Group Name</label>
-                                            <input type="text" value="<?php echo $g->name; ?>" required name="name" class="form-control" id="exampleInputEmail1">
-                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="manage_groups.php">Groups</a></li>
+                                    <li class="breadcrumb-item active">Update</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Fill All Required Fields</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form method="post" enctype="multipart/form-data" role="form">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="form-group col-md-6">
+                                                <label for="">Group Name</label>
+                                                <input type="text" value="<?php echo $g->name; ?>" required name="name" class="form-control" id="exampleInputEmail1">
+                                                <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Group Number / Code</label>
+                                                <input type="text" required name="code" value="<?php echo $g->code; ?>" class="form-control">
+                                            </div>
                                         </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Group Number / Code</label>
-                                            <input type="text" required name="code" value="<?php echo $g->code; ?>" class="form-control">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="exampleInputPassword1">Group Description</label>
+                                                <textarea required id="textarea" name="details" rows="10" class="form-control"><?php echo $g->details; ?></textarea>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="row">
-                                    <div class="form-group col-md-12">
-                                        <label for="exampleInputPassword1">Group Description</label>
-                                        <textarea required id="textarea" name="details" rows="10" class="form-control"></textarea>
+                                    <div class="card-footer">
+                                        <button type="submit" name="update_group" class="btn btn-primary">Update Group</button>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <button type="submit" name="update_group" class="btn btn-primary">Update Group</button>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div>
-        <?php require_once('partials/_footer.php'); ?>
+                </section>
+            </div>
+        <?php require_once('partials/_footer.php');
+        } ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>
