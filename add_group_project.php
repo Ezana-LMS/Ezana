1573c3cdee7e9865140602cc66d3b0e961a228cd
MartMbithi <martdevelopers254@gmail.com>
2020-10-22 11:59:00 +0300

    Create Functionality On Group Projects

--- add_group_project.php
+++ add_group_project.php
@@ -4,20 +4,17 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 require_once('configs/codeGen.php');
 check_login();
-if (isset($_POST['add_project'])) {
+if (isset($_POST['add_group_project'])) {
 
     $id = $_POST['id'];
     $group_code = $_GET['group_code'];
     $group_name  = $_GET['group_name'];
     $type = $_GET['type'];
     $details = $_POST['details'];
-
     /* $attachments = $_FILES['attachments']['name'];
     move_uploaded_file($_FILES["attachments"]["tmp_name"], "dist/memos/" . $_FILES["attachments"]["name"]); */
-
     $created_at = date('d M Y g:i');
     $submitted_on = $_POST['submitted_on'];
-
     $query = "INSERT INTO ezanaLMS_GroupsAssignments (id, group_code, group_name, type, details, created_at, submitted_on) VALUES(?,?,?,?,?,?,?)";
     $stmt = $mysqli->prepare($query);
     $rc = $stmt->bind_param('sssssss', $id, $group_code, $group_name, $type, $details, $created_at, $submitted_on);
@@ -28,7 +25,6 @@ if (isset($_POST['add_project'])) {
         $info = "Please Try Again Or Try Later";
     }
 }
-
 
 require_once('partials/_head.php');
 ?>
@@ -48,15 +44,15 @@ require_once('partials/_head.php');
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Add New Departmental Notice</h1>
+                            <h1>Add <?php echo $group_name; ?> Project</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="departmental_notices.php">Departments</a></li>
-                                <li class="breadcrumb-item"><a href="departmental_notices.php">Departmental Notices</a></li>
-                                <li class="breadcrumb-item active">Add Notice</li>
+                                <li class="breadcrumb-item"><a href="manage_groups.php">Groups</a></li>
+                                <li class="breadcrumb-item"><a href="">Group Projects</a></li>
+                                <li class="breadcrumb-item active">Add Project</li>
                             </ol>
                         </div>
                     </div>
@@ -81,6 +77,10 @@ require_once('partials/_head.php');
                                             <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
                                         </div>
                                         <div class="form-group col-md-12">
+                                            <label for="exampleInputPassword1">Submission Date </label>
+                                            <input type="date" required name="submitted_on" class="form-control">
+                                        </div>
+                                        <!-- <div class="form-group col-md-12">
                                             <label for="">Upload Departmental Notice (PDF Or Docx)</label>
                                             <div class="input-group">
                                                 <div class="custom-file">
@@ -88,18 +88,17 @@ require_once('partials/_head.php');
                                                     <label class="custom-file-label" for="exampleInputFile">Choose file </label>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
-                                    <h2 class="text-center">Or </h2>
                                     <div class="row">
                                         <div class="form-group col-md-12">
-                                            <label for="exampleInputPassword1">Type Departmental Notice</label>
-                                            <textarea name="departmental_memo" id="textarea" rows="10" class="form-control"></textarea>
+                                            <label for="exampleInputPassword1">Group Project / Assignment</label>
+                                            <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer">
-                                    <button type="submit" name="add_notice" class="btn btn-primary">Add Departmental Notice</button>
+                                    <button type="submit" name="add_group_project" class="btn btn-primary">Submit</button>
                                 </div>
                             </form>
                         </div>
