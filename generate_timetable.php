853d304f18602814f34a270d21f7437d38a4184a
MartMbithi <martdevelopers254@gmail.com>
2020-10-22 09:49:25 +0300

    Generate Time table

--- generate_timetable.php
+++ generate_timetable.php
@@ -38,7 +38,7 @@ require_once('partials/_head.php');
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Classes</h1>
+                            <h1>Time Table</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
