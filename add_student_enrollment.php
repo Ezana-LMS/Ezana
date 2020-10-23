b8813c251cc29a59201c1d9995599ec3fed18ce0
MartMbithi <martdevelopers254@gmail.com>
2020-10-23 14:01:38 +0300

    Enrolled Student

---
+++ add_student_enrollment.php
@@ -0,0 +1,174 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+require_once('configs/codeGen.php');
+check_login();
+if (isset($_POST['add_enroll'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['student_name']) && !empty($_POST['student_name'])) {
+        $student_name = mysqli_real_escape_string($mysqli, trim($_POST['student_name']));
+    } else {
+        $error = 1;
+        $err = "Student Name Cannot Be Empty";
+    }
+    if (isset($_POST['course_name']) && !empty($_POST['course_name'])) {
+        $course_name = mysqli_real_escape_string($mysqli, trim($_POST['course_name']));
+    } else {
+        $error = 1;
+        $err = "Course Name Cannot Be Empty";
+    }
+    if (isset($_POST['module_name']) && !empty($_POST['module_name'])) {
+        $module_name = mysqli_real_escape_string($mysqli, trim($_POST['module_name']));
+    } else {
+        $error = 1;
+        $err = "Module Name Cannot Be Empty";
+    }
+    if (isset($_POST['semester_start']) && !empty($_POST['semester_start'])) {
+        $semester_start = mysqli_real_escape_string($mysqli, trim($_POST['semester_start']));
+    } else {
+        $error = 1;
+        $err = "Semester Start / End Dates Cannot Be Empty";
+    }
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Enrollments WHERE  student_adm ='$student_adm ' || semester_enrolled ='$semester_enrolled' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if (($student_adm && $semester_enrolled) == ($row['student_adm'] && $row['semester_enrolled'])) {
+                $err =  "Student $student_name Already Enrolled On $semester_enrolled ";
+            }
+        } else {
+            $id = $_POST['id'];
+            $code = $_POST['code'];
+            $student_adm = $_POST['student_adm'];
+            $student_name = $_POST['student_name'];
+            $semester_enrolled = $_POST['semester_enrolled'];
+            $created_at = date('d M Y');
+            $course_code = $_POST['course_code'];
+            $course_name = $_POST['course_name'];
+            $semester_start = $_POST['semester_start'];
+            $semester_end = $_POST['semester_end'];
+            $academic_year_enrolled = $_POST['academic_year_enrolled'];
+            $module_name = $_POST['module_name'];
+            $module_code = $_POST['module_code'];
+            $query = "INSERT INTO ezanaLMS_Enrollments (id, code, student_adm, student_name, semester_enrolled, created_at, course_code, course_name, semester_start, semester_end, academic_year_enrolled, module_name, module_code) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssssssssssss', $id, $code, $student_adm, $student_name, $semester_enrolled, $created_at, $course_code, $course_name, $semester_start, $semester_end, $academic_year_enrolled, $module_name, $module_code);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Student Enrolled" && header("refresh:1; url=add_student_enrollment.php");
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+require_once('partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('partials/_sidebar.php'); ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Add New Module Assign</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="assign_lecturer_module.php">Modules</a></li>
+                                <li class="breadcrumb-item active">Module Assigns</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="col-md-12">
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Fill All Required Fields</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form method="post" enctype="multipart/form-data" role="form">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-6">
+                                            <label for="">Lecturer Name</label>
+                                            <select class='form-control basic' id="LecName" onchange="getLecDetails(this.value);" name="lec_name">
+                                                <option selected>Select Lecturer Name</option>
+                                                <?php
+                                                $ret = "SELECT * FROM `ezanaLMS_Lecturers`  ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($lec = $res->fetch_object()) {
+                                                ?>
+                                                    <option><?php echo $lec->name; ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="">Lecturer ID</label>
+                                            <input type="text" id="lecID" readonly required name="lec_id" class="form-control">
+                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                        </div>
+                                        <hr>
+                                        <div class="form-group col-md-6">
+                                            <label for="">Module Name</label>
+                                            <select class='form-control basic' id="ModuleName" onchange="getModuleDetails(this.value);" name="module_name">
+                                                <option selected>Select Module Name </option>
+                                                <?php
+                                                $ret = "SELECT * FROM `ezanaLMS_Modules` WHERE ass_status = '0'  ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($mod = $res->fetch_object()) {
+                                                ?>
+                                                    <option><?php echo $mod->name; ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label for="">Module Code</label>
+                                            <input type="text" id="ModuleCode" readonly required name="module_code" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <button type="submit" name="assign_module" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <?php require_once('partials/_footer.php'); ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
