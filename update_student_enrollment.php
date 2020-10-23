0af0edc93ed1c1addd18e66c5d86f973a4161bec
MartMbithi <martdevelopers254@gmail.com>
2020-10-23 18:03:41 +0300

    Intergrated /Update endpoint on student enrollment

--- update_student_enrollment.php
+++ update_student_enrollment.php
@@ -94,139 +94,149 @@ require_once('partials/_head.php');
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <?php require_once('partials/_sidebar.php'); ?>
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>Update Student Enrollment</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="manage_student_enrollments.php">Enrollments</a></li>
-                                <li class="breadcrumb-item active">Update Enrollments</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h3 class="card-title">Fill All Required Fields</h3>
+        <?php
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `ezanaLMS_Enrollments` WHERE id ='$update'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        $cnt = 1;
+        while ($en = $res->fetch_object()) {
+            require_once('partials/_sidebar.php');
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Update Student Enrollment</h1>
                             </div>
-                            <!-- form start -->
-                            <form method="post" enctype="multipart/form-data" role="form">
-                                <div class="card-body">
-                                    <div class="row" style="display:none">
-                                        <div class="form-group col-md-6">
-                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Enroll Code</label>
-                                            <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Student Admission Number</label>
-                                            <select class='form-control basic' id="StudentAdmn" onchange="getStudentDetails(this.value);" name="student_adm">
-                                                <option selected><?php echo $en->student_admn;?></option>
-                                                <?php
-                                                $ret = "SELECT * FROM `ezanaLMS_Students`  ";
-                                                $stmt = $mysqli->prepare($ret);
-                                                $stmt->execute(); //ok
-                                                $res = $stmt->get_result();
-                                                while ($std = $res->fetch_object()) {
-                                                ?>
-                                                    <option><?php echo $std->admno; ?></option>
-                                                <?php } ?>
-                                            </select>
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Student Name</label>
-                                            <input type="text" id="StudentName" readonly required name="student_name" class="form-control">
-                                        </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="manage_student_enrollments.php">Enrollments</a></li>
+                                    <li class="breadcrumb-item active">Update Enrollments</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
 
-                                        <div class="form-group col-md-6">
-                                            <label for="">Course Code</label>
-                                            <select class='form-control basic' id="Coursecode" onchange="getCourseDetails(this.value);" name="course_code">
-                                                <option selected><?php echo $en->course_code;?></option>
-                                                <?php
-                                                $ret = "SELECT * FROM `ezanaLMS_Courses`  ";
-                                                $stmt = $mysqli->prepare($ret);
-                                                $stmt->execute(); //ok
-                                                $res = $stmt->get_result();
-                                                while ($courses = $res->fetch_object()) {
-                                                ?>
-                                                    <option><?php echo $courses->code; ?></option>
-                                                <?php } ?>
-                                            </select>
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Fill All Required Fields</h3>
+                                </div>
+                                <!-- form start -->
+                                <form method="post" enctype="multipart/form-data" role="form">
+                                    <div class="card-body">
+                                        <div class="row" style="display:none">
+                                            <div class="form-group col-md-6">
+                                                <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Enroll Code</label>
+                                                <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                            </div>
                                         </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Course Name</label>
-                                            <input type="text" id="CourseName" readonly required name="course_name" class="form-control">
-                                        </div>
-                                        <hr>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Module Name</label>
-                                            <select class='form-control basic' id="ModuleName" onchange="getModuleDetails(this.value);" name="module_name">
-                                                <option selected><?php echo $en->module_name;?></option>
-                                                <?php
-                                                $ret = "SELECT * FROM `ezanaLMS_Modules`   ";
-                                                $stmt = $mysqli->prepare($ret);
-                                                $stmt->execute(); //ok
-                                                $res = $stmt->get_result();
-                                                while ($mod = $res->fetch_object()) {
-                                                ?>
-                                                    <option><?php echo $mod->name; ?></option>
-                                                <?php } ?>
-                                            </select>
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Module Code</label>
-                                            <input type="text" id="ModuleCode" readonly required name="module_code" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="">Semester Enrolled</label>
-                                            <input type="text" value="<?php echo $en->semester_enrolled;?>" required name="semester_enrolled" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="">Semester Start Date</label>
-                                            <input type="date" value="<?php echo $en->semester_start;?>" required name="semester_start" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="">Semester End Date</label>
-                                            <input type="date" value="<?php echo $en->semester_end;?>" required name="semester_end" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-12">
-                                            <label for="">Academic Year Enrolled</label>
-                                            <input type="text" value="<?php echo $en->academic_year_enrolled;?>" required name="academic_year_enrolled" class="form-control">
+                                        <div class="row">
+                                            <div class="form-group col-md-6">
+                                                <label for="">Student Admission Number</label>
+                                                <select class='form-control basic' id="StudentAdmn" onchange="getStudentDetails(this.value);" name="student_adm">
+                                                    <option selected><?php echo $en->student_admn; ?></option>
+                                                    <?php
+                                                    $ret = "SELECT * FROM `ezanaLMS_Students`  ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($std = $res->fetch_object()) {
+                                                    ?>
+                                                        <option><?php echo $std->admno; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Student Name</label>
+                                                <input type="text" id="StudentName" readonly required name="student_name" class="form-control">
+                                            </div>
+
+                                            <div class="form-group col-md-6">
+                                                <label for="">Course Code</label>
+                                                <select class='form-control basic' id="Coursecode" onchange="getCourseDetails(this.value);" name="course_code">
+                                                    <option selected><?php echo $en->course_code; ?></option>
+                                                    <?php
+                                                    $ret = "SELECT * FROM `ezanaLMS_Courses`  ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($courses = $res->fetch_object()) {
+                                                    ?>
+                                                        <option><?php echo $courses->code; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Course Name</label>
+                                                <input type="text" id="CourseName" readonly required name="course_name" class="form-control">
+                                            </div>
+                                            <hr>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Module Name</label>
+                                                <select class='form-control basic' id="ModuleName" onchange="getModuleDetails(this.value);" name="module_name">
+                                                    <option selected><?php echo $en->module_name; ?></option>
+                                                    <?php
+                                                    $ret = "SELECT * FROM `ezanaLMS_Modules`   ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($mod = $res->fetch_object()) {
+                                                    ?>
+                                                        <option><?php echo $mod->name; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Module Code</label>
+                                                <input type="text" id="ModuleCode" readonly required name="module_code" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Semester Enrolled</label>
+                                                <input type="text" value="<?php echo $en->semester_enrolled; ?>" required name="semester_enrolled" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Semester Start Date</label>
+                                                <input type="date" value="<?php echo $en->semester_start; ?>" required name="semester_start" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Semester End Date</label>
+                                                <input type="date" value="<?php echo $en->semester_end; ?>" required name="semester_end" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <label for="">Academic Year Enrolled</label>
+                                                <input type="text" value="<?php echo $en->academic_year_enrolled; ?>" required name="academic_year_enrolled" class="form-control">
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <button type="submit" name="update_enroll" class="btn btn-primary">Submit</button>
-                                </div>
-                            </form>
+                                    <div class="card-footer">
+                                        <button type="submit" name="update_enroll" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
-        <?php require_once('partials/_footer.php'); ?>
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+        <?php require_once('partials/_footer.php');
+        } ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>
