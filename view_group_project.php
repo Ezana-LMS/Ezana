7bc4930f2ea3ddb15b67d0916667e0a79059e3e9
MartMbithi <martdevelopers254@gmail.com>
2020-10-26 12:29:03 +0300

    Refactored Group Projects Added Call Back URL

--- view_group_project.php
+++ view_group_project.php
@@ -3,6 +3,21 @@ session_start();
 require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
+
+if (isset($_GET['delete'])) {
+    $view = $_GET['view'];
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_GroupsAssignmentsGrades WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=view_group_project.php?view=$view");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
 require_once('partials/_head.php');
 ?>
 
