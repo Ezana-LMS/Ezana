0949ec9c562f8a88487062e0209d2671f85cc6a8
MartMbithi <martdevelopers254@gmail.com>
2020-10-26 13:47:56 +0300

    Modules Reports:

---
+++ modules_reports.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+
+require_once('partials/_head.php');
+
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+        <!-- Main Sidebar Container -->
+        <?php require_once('partials/_sidebar.php'); ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Modules</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="modules_reports.php">Advacned Reporting</a></li>
+                                <li class="breadcrumb-item active">Modules</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <table id="export-dt" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Code</th>
+                                            <th>Course</th>
+                                            <th>Duration</th>
+                                            <th>Weight Percentage</th>
+                                            <th>Lectures Per Week</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $ret = "SELECT * FROM `ezanaLMS_Modules`  ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        $cnt = 1;
+                                        while ($mod = $res->fetch_object()) {
+                                        ?>
+
+                                            <tr>
+                                                <td><?php echo $cnt; ?></td>
+                                                <td><?php echo $mod->name; ?></td>
+                                                <td><?php echo $mod->code; ?></td>
+                                                <td><?php echo $mod->course_name; ?></td>
+                                                <td><?php echo $mod->course_duration; ?></td>
+                                                <td><?php echo $mod->weight_percentage; ?></td>
+                                                <td><?php echo $mod->lectures_number; ?></td>
+                                            </tr>
+                                        <?php $cnt = $cnt + 1;
+                                        } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <?php require_once('partials/_footer.php'); ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
