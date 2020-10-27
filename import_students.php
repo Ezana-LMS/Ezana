ae97a8f4e18714ced8ae81d005da4d2de367b788
MartMbithi <martdevelopers254@gmail.com>
2020-10-27 11:03:52 +0300

    Intergrated PHPOFFICE API

--- import_students.php
+++ import_students.php
@@ -4,7 +4,103 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 require_once('configs/codeGen.php');
 check_login();
-if (isset($_POST['upload'])) {
+
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+
+require_once('vendor/autoload.php');
+
+if (isset($_POST["import"])) {
+
+    $allowedFileType = [
+        'application/vnd.ms-excel',
+        'text/xls',
+        'text/xlsx',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ];
+
+    if (in_array($_FILES["file"]["type"], $allowedFileType)) {
+
+        $targetPath = 'uploads/' . $_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
+
+        $Reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+
+        $spreadSheet = $Reader->load($targetPath);
+        $excelSheet = $spreadSheet->getActiveSheet();
+        $spreadSheetAry = $excelSheet->toArray();
+        $sheetCount = count($spreadSheetAry);
+
+        for ($i = 0; $i <= $sheetCount; $i++) {
+            $id = "";
+            if (isset($spreadSheetAry[$i][1])) {
+                $id = mysqli_real_escape_string($conn, $spreadSheetAry[$i][1]);
+            }
+            $admno = "";
+            if (isset($spreadSheetAry[$i][2])) {
+                $admno = mysqli_real_escape_string($conn, $spreadSheetAry[$i][2]);
+            }
+            $name = "";
+            if (isset($spreadSheetAry[$i][3])) {
+                $name = mysqli_real_escape_string($conn, $spreadSheetAry[$i][3]);
+            }
+            $email = "";
+            if (isset($spreadSheetAry[$i][4])) {
+                $email = mysqli_real_escape_string($conn, $spreadSheetAry[$i][4]);
+            }
+            $phone = "";
+            if (isset($spreadSheetAry[$i][5])) {
+                $phone = mysqli_real_escape_string($conn, $spreadSheetAry[$i][5]);
+            }
+            $adr = "";
+            if (isset($spreadSheetAry[$i][6])) {
+                $adr = mysqli_real_escape_string($conn, $spreadSheetAry[$i][6]);
+            }
+            $dob = "";
+            if (isset($spreadSheetAry[$i][7])) {
+                $dob = mysqli_real_escape_string($conn, $spreadSheetAry[$i][7]);
+            }
+            $idno = "";
+            if (isset($spreadSheetAry[$i][8])) {
+                $idno = mysqli_real_escape_string($conn, $spreadSheetAry[$i][8]);
+            }
+            $gender = "";
+            if (isset($spreadSheetAry[$i][9])) {
+                $gender = mysqli_real_escape_string($conn, $spreadSheetAry[$i][9]);
+            }
+
+
+            if (!empty($name) || !empty($admno)) {
+                $query = "insert into ezanaLMS_Students (id, admno, name, email, phone, adr, dob, idno, gender) values(?,?,?,?,?,?,?,?,?)";
+                $paramType = "sssssssss";
+                $paramArray = array(
+                    $id,
+                    $admno,
+                    $name,
+                    $email,
+                    $phone,
+                    $phone,
+                    $adr,
+                    $dob,
+                    $idno,
+                    $gender
+                );
+                $insertId = $db->insert($query, $paramType, $paramArray);
+                // $query = "insert into tbl_info(name,description) values('" . $name . "','" . $description . "')";
+                // $result = mysqli_query($conn, $query);
+
+                if (!empty($insertId)) {
+                    $type = "success";
+                    $message = "Excel Data Imported into the Database";
+                } else {
+                    $type = "error";
+                    $message = "Problem in Importing Excel Data";
+                }
+            }
+        }
+    } else {
+        $type = "error";
+        $message = "Invalid File Type. Upload Excel File.";
+    }
 }
 
 require_once('partials/_head.php');
@@ -52,7 +148,7 @@ require_once('partials/_head.php');
                             <form method="post" enctype="multipart/form-data" role="form">
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-12">
                                             <label for="exampleInputFile">Select File</label>
                                             <div class="input-group">
                                                 <div class="custom-file">
