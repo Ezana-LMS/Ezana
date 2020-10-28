8af02fc517d6e13796f91c8d50cb814e15792d08
MartMbithi <martdevelopers254@gmail.com>
2020-10-28 10:58:30 +0300

    Refactored Mark Assignment

--- mark_assignment.php
+++ mark_assignment.php
@@ -28,7 +28,7 @@ require_once('partials/_head.php');
         <?php require_once('partials/_nav.php'); ?>
         <!-- Main Sidebar Container -->
         <?php
-            require_once('partials/_sidebar.php');
+        require_once('partials/_sidebar.php');
         ?>
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
@@ -57,18 +57,18 @@ require_once('partials/_head.php');
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
-                            <!-- <div class="card-header">
+                            <div class="card-header">
                                 <h2 class="text-right">
-                                    <a class="btn btn-outline-success" href="upload_assignment.php">
+                                    <a class="btn btn-outline-success" href="upload_assignment_attempt.php?exam_id=<?php echo $exam_id; ?>">
                                         <i class="fas fa-file-upload"></i>
-                                        Upload Assignment
+                                        Upload Attempted Assignment
                                     </a>
                                     <!-- <a class="btn btn-outline-primary" href="">
                                         <i class="fas fa-file-excel"></i>
                                         Import Faculties From .XLS File
                                     </a> -->
                                 </h2>
-                            </div> -->
+                            </div>
                             <div class="card-body">
                                 <table id="example1" class="table table-bordered table-striped">
                                     <thead>
@@ -83,7 +83,7 @@ require_once('partials/_head.php');
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <?
+                                        <?php
                                         $exam_id = $_GET['exam_id'];
                                         $ret = "SELECT * FROM `ezanaLMS_StudentAnswers`  WHERE  exam_id  ='$exam_id' ";
                                         $stmt = $mysqli->prepare($ret);
@@ -96,8 +96,8 @@ require_once('partials/_head.php');
                                                 <td><?php echo $cnt; ?></td>
                                                 <td><?php echo $exam->module_code; ?></td>
                                                 <td><?php echo $exam->module_name; ?></td>
-                                                <td><?php echo $exam->student_regno;?></td>
-                                                <td><?php echo $exam->student_name;?></td>
+                                                <td><?php echo $exam->student_regno; ?></td>
+                                                <td><?php echo $exam->student_name; ?></td>
                                                 <td><?php echo date('d M Y - g:i', strtotime($exam->created_at)); ?></td>
                                                 <td>
                                                     <a class="badge badge-success" href="grade_assignment.php?grade=<?php echo $exam->id; ?>">
