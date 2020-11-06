a25ea959f8c1d10c8373ce675f93c6f2c1bca312
MartMbithi <martdevelopers254@gmail.com>
2020-11-06 13:46:16 +0300

    Updated CheckLogin

--- configs/checklogin.php
+++ configs/checklogin.php
@@ -1,14 +1,11 @@
 <?php
 function check_login()
 {
-	if(strlen($_SESSION['id'])==0)
-		{
-			$host = $_SERVER['HTTP_HOST'];
-			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-			$extra="index.php";
-			$_SESSION["id"]="";
-			header("Location: http://$host$uri/$extra");
-			
-		}
+	if ((strlen($_SESSION['id']) == 0) || (strlen($_SESSION['email']) == 0) || (strlen($_SESSION['name']) == 0)) {
+		$host = $_SERVER['HTTP_HOST'];
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$extra = "index.php";
+		$_SESSION["id"] = "";
+		header("Location: http://$host$uri/$extra");
+	}
 }
-
