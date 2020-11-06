4f59029c6f73a76c58c873b8db2350a66418adcb
MartMbithi <martdevelopers254@gmail.com>
2020-11-06 13:52:43 +0300

    Swal On Head

--- partials/_head.php
+++ partials/_head.php
@@ -7,13 +7,45 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-
     <title>Ezana LMS </title>
-
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- Swal Js -->
+    <script src="dist/js/swal.js"></script>
+    <!--Inject SWAL-->
+    <?php if (isset($success)) { ?>
+        <script>
+            setTimeout(function() {
+                    /* Success */
+                    swal("Success", "<?php echo $success; ?>", "success");
+                },
+                100);
+        </script>
+
+    <?php } ?>
+
+    <?php if (isset($err)) { ?>
+        <script>
+            setTimeout(function() {
+                    /* Error */
+                    swal("Failed", "<?php echo $err; ?>", "error");
+                },
+                100);
+        </script>
+
+    <?php } ?>
+    <?php if (isset($info)) { ?>
+        <script>
+            setTimeout(function() {
+                    /* Info */
+
+                    swal("Success", "<?php echo $info; ?>", "warning");
+                },
+                100);
+        </script>
+    <?php } ?>
 </head>
