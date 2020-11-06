09c530add447aaa344c14d5b90f942dcc52e5671
MartMbithi <martdevelopers254@gmail.com>
2020-11-06 13:20:02 +0300

    Indexed

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+include('configs/config.php');
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = sha1(md5($_POST['password'])); //double encrypt to increase security
+    $stmt = $mysqli->prepare("SELECT email, password, id  FROM ezanaLMS_Admins  WHERE email =? AND password =?");
+    $stmt->bind_param('ss', $email, $password); //bind fetched parameters
+    $stmt->execute(); //execute bind 
+    $stmt->bind_result($email, $password, $id); //bind result
+    $rs = $stmt->fetch();
+    $_SESSION['id'] = $id;
+    if ($rs) {
+        header("location:dashboard.php");
+    } else {
+        $err = "Access Denied Please Check Your Credentials";
+    }
+}
+require_once('partials/_head.php')
+?>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="card">
+            <div class="card-body login-card-body">
+                <div class="login-logo">
+                    <img height="150" width="150" src="dist/img/logo.jpeg" alt="">
+                </div>
+                <p class="login-box-msg">
+                    Sign In To Start Your Session 
+                    <br>
+                    Demo Credentials
+                    <br>
+                    <span class="text-success"> Email   : sysadmin@ezana.org </span> 
+                    <br>
+                    <span class="text-success">Passsword: 123 </span>
+                </p>
+
+                <form method="post">
+                    <div class="input-group mb-3">
+                        <input type="email" required name="email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" required name="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <!--  <div class="social-auth-links text-center mb-3">
+                    <p>- OR -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                    </a>
+                </div> -->
+
+                <p class="mb-1">
+                    <a href="forgot-password.php">I forgot my password</a>
+                </p>
+                <!-- <p class="mb-0">
+                    <a href="register.html" class="text-center">Register a new membership</a>
+                </p> -->
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
