61030f74ce41f3c6526519cc17950781f1fe6293
MartMbithi <martdevelopers254@gmail.com>
2020-11-09 13:56:35 +0300

    Messed Up Ids

--- add_department.php
+++ add_department.php
@@ -52,55 +52,61 @@ if (isset($_POST['add_dept'])) {
         }
     }
 }
-require_once('partials/_head.php');
-?>
 
-<body class="hold-transition sidebar-collapse layout-top-nav">
-    <div class="wrapper">
+$faculty = $_GET['faculty'];
+$ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE name = '$faculty' ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($f = $res->fetch_object()) {
+    require_once('partials/_head.php'); ?>
 
-        <!-- Navbar -->
-        <?php require_once('partials/_nav.php'); ?>
-        <!-- /.navbar -->
+    <body class="hold-transition sidebar-collapse layout-top-nav">
+        <div class="wrapper">
 
-        <div class="content-wrapper">
-            <div class="content-header">
-                <div class="container">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Add Department</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                <li class="breadcrumb-item"><a href="dashboard.php">Faculties</a></li>
-                                <li class="breadcrumb-item"><a href="dashboard.php">Depart</a></li>
-                                <li class="breadcrumb-item active">Add Faculty</li>
-                            </ol>
+            <!-- Navbar -->
+            <?php require_once('partials/_nav.php'); ?>
+            <!-- /.navbar -->
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark">Add Department</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Faculties</a></li>
+                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty_dashboard.php?id=<?php echo $f->id; ?>&faculty=<?php echo $faculty; ?>"><?php echo $f->name; ?></a></li>
+                                    <li class="breadcrumb-item active">Add Department</li>
+                                </ol>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="content">
-                <div class="container">
-                    <section class="content">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card card-primary">
-                                    <div class="card-header">
-                                        <h3 class="card-title">Fill All Required Fields</h3>
+                <div class="content">
+                    <div class="container">
+                        <section class="content">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card card-primary">
+                                        <div class="card-header">
+                                            <h3 class="card-title">Fill All Required Fields</h3>
+                                        </div>
                                     </div>
-
                                 </div>
                             </div>
-                        </div>
-                    </section>
+                        </section>
+                    </div>
                 </div>
             </div>
+        <?php require_once('partials/_footer.php');
+    } ?>
         </div>
-        <?php require_once('partials/_footer.php'); ?>
-    </div>
-    <?php require_once('partials/_scripts.php'); ?>
-</body>
+        <?php require_once('partials/_scripts.php'); ?>
+    </body>
 
-</html>
+    </html>
