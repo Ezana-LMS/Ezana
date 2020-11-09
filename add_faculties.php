711420ff71b72545e02bc14298c4f39db01c6bfc
MartMbithi <martdevelopers254@gmail.com>
2020-11-09 11:14:38 +0300

    Create faculty front end

--- add_faculties.php
+++ add_faculties.php
@@ -82,8 +82,36 @@ require_once('partials/_head.php');
                     <section class="content">
                         <div class="row">
                             <div class="col-12">
-                                <div class="card">
-
+                                <div class="card card-primary">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Fill All Required Fields</h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <!-- form start -->
+                                    <form method="post" enctype="multipart/form-data" role="form">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="form-group col-md-6">
+                                                    <label for="">Faculty Name</label>
+                                                    <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
+                                                    <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                </div>
+                                                <div class="form-group col-md-6">
+                                                    <label for="">Faculty Number / Code</label>
+                                                    <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="form-group col-md-12">
+                                                    <label for="exampleInputPassword1">Faculty Description</label>
+                                                    <textarea id="textarea" name="details" rows="10" class="form-control"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <button type="submit" name="add_faculty" class="btn btn-primary">Add Faculty</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
