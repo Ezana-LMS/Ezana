3c042fa5730e2d024313ba1c2c879f61f56417cf
MartMbithi <martdevelopers254@gmail.com>
2020-11-09 10:18:40 +0300

    Logged in user profile

--- profile.php
+++ profile.php
@@ -106,7 +106,119 @@ require_once('partials/_head.php');
             <div class="content">
                 <div class="container">
                     <div class="row">
+                        <div class="col-md-3">
 
+                            <!-- Profile Image -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body box-profile">
+                                    <div class="text-center">
+                                        <?php echo $dpic; ?>
+                                    </div>
+
+                                    <h3 class="profile-username text-center"><?php echo $admin->name; ?></h3>
+
+                                    <p class="text-muted text-center"><?php echo $admin->rank; ?></p>
+
+                                    <ul class="list-group list-group-unbordered mb-3">
+                                        <li class="list-group-item">
+                                            <b>Email: </b> <a class="float-right"><?php echo $admin->email; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Phone: </b> <a class="float-right"><?php echo $admin->phone; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Address</b> <a class="float-right"><?php echo $admin->adr; ?></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card">
+                                <div class="card-header p-2">
+                                    <ul class="nav nav-pills">
+                                        <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+                                        <li class="nav-item"><a class="nav-link " href="#changePassword" data-toggle="tab">Change Password</a></li>
+                                    </ul>
+                                </div>
+                                <div class="card-body">
+                                    <div class="tab-content">
+                                        <div class="active tab-pane" id="settings">
+                                            <form method='post' enctype="multipart/form-data" class="form-horizontal">
+                                                <div class="form-group row">
+                                                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" required class="form-control" value="<?php echo $admin->name; ?>" name="name" id="inputName">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="email" required class="form-control" value="<?php echo $admin->email; ?>" name="email" id="inputEmail">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputName2" class="col-sm-2 col-form-label">Phone Number</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" required class="form-control" value="<?php echo $admin->phone; ?>" name="phone" id="inputName2">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputExperience" class="col-sm-2 col-form-label">Address</label>
+                                                    <div class="col-sm-10">
+                                                        <textarea required class="form-control" name="adr" id="textarea"><?php echo $admin->adr; ?></textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputSkills" class="col-sm-2 col-form-label">Profile Picture</label>
+                                                    <div class="col-sm-10">
+                                                        <div class="input-group">
+                                                            <div class="custom-file">
+                                                                <input type="file" name="profile_pic" class="custom-file-input" id="exampleInputFile">
+                                                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="offset-sm-2 col-sm-10">
+                                                        <button type="submit" name="profile_update" class="btn btn-primary">Update Profile</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+
+                                        <div class="tab-pane" id="changePassword">
+                                            <form method='post' class="form-horizontal">
+                                                <div class="form-group row">
+                                                    <label for="inputName" class="col-sm-2 col-form-label">Old Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="old_password" required class="form-control" id="inputName">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputEmail" class="col-sm-2 col-form-label">New Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="new_password" required class="form-control" id="inputEmail">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputName2" class="col-sm-2 col-form-label">Confirm New Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="confirm_password" required class="form-control" id="inputName2">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="offset-sm-2 col-sm-10">
+                                                        <button type="submit" name="change_password" class="btn btn-primary">Change Password</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
