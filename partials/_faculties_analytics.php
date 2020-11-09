e4b90285d752b8b636dc54acc6552f9bfc8f5c9b
MartMbithi <martdevelopers254@gmail.com>
2020-11-09 16:44:25 +0300

    Analytics On Dashboard

--- partials/_faculties_analytics.php
+++ partials/_faculties_analytics.php
@@ -8,3 +8,37 @@ $stmt->execute();
 $stmt->bind_result($departments);
 $stmt->fetch();
 $stmt->close();
+
+
+/* Courses */
+$query = "SELECT COUNT(*)  FROM `ezanaLMS_Courses` WHERE faculty_id = '$faculty' ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($courses);
+$stmt->fetch();
+$stmt->close();
+
+/* Modules */
+$query = "SELECT COUNT(*)  FROM `ezanaLMS_Modules` WHERE faculty_id ='$faculty' ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($modules);
+$stmt->fetch();
+$stmt->close();
+
+
+/* Lecturers */
+$query = "SELECT COUNT(*)  FROM `ezanaLMS_Lecturers` WHERE faculty_id = '$faculty' ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($lecs);
+$stmt->fetch();
+$stmt->close();
+
+/* Students */
+$query = "SELECT COUNT(*)  FROM `ezanaLMS_Students` WHERE faculty_id = '$faculty' ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($students);
+$stmt->fetch();
+$stmt->close();
