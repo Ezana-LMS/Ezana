781e00758a1098beddff8290ce0a556273358f54
MartMbithi <martdevelopers254@gmail.com>
2020-11-09 10:40:47 +0300

    Create school calendar back end logic

--- add_school_calendar.php
+++ add_school_calendar.php
@@ -68,63 +68,62 @@ require_once('partials/_head.php');
         <?php
         require_once('partials/_nav.php');
         ?>
-            <!-- /.navbar -->
-            <div class="content-wrapper">
-                <div class="content-header">
-                    <div class="container">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1 class="m-0 text-dark">Add Important Dates</h1>
-                            </div>
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="school_calendar.php">School Calendar</a></li>
-                                    <li class="breadcrumb-item active">Add  Important Dates</li>
-                                </ol>
-                            </div>
+        <!-- /.navbar -->
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Add Important Dates</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="school_calendar.php">School Calendar</a></li>
+                                <li class="breadcrumb-item active">Add Important Dates</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
-                <div class="content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="container-fluid">
-                                    <div class="col-md-12">
-                                        <div class="card card-primary">
-                                            <div class="card-header">
-                                                <h3 class="card-title">Fill All Required Fields</h3>
-                                            </div>
-                                            <form method="post" enctype="multipart/form-data" role="form">
-                                                <div class="card-body">
-                                                    <div class="row">
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Semester Name</label>
-                                                            <input type="text" value="<?php echo $cal->semester_name; ?>" required name="semester_name" class="form-control" id="exampleInputEmail1">
-                                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
-                                                        </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Academic Year Name</label>
-                                                            <input type="text" value="<?php echo $cal->academic_yr; ?>" required name="academic_yr" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Semester Opening Dates</label>
-                                                            <input type="date" value="<?php echo $cal->semester_start; ?>" required name="semester_start" class="form-control" id="exampleInputEmail1">
-                                                        </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Semester Closing Dates</label>
-                                                            <input type="date" value="<?php echo $cal->semester_end; ?>" required name="semester_end" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="card-footer">
-                                                    <button type="submit" name="update_school_calendar" class="btn btn-primary">Submit</button>
-                                                </div>
-                                            </form>
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="container-fluid">
+                                <div class="col-md-12">
+                                    <div class="card card-primary">
+                                        <div class="card-header">
+                                            <h3 class="card-title">Fill All Required Fields</h3>
                                         </div>
+                                        <form method="post" enctype="multipart/form-data" role="form">
+                                            <div class="card-body">
+                                                <div class="row">
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Semester Name</label>
+                                                        <input type="text" required name="semester_name" class="form-control" id="exampleInputEmail1">
+                                                        <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                    </div>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Academic Year Name</label>
+                                                        <input type="text" required name="academic_yr" class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Semester Opening Dates</label>
+                                                        <input type="date" required name="semester_start" class="form-control" id="exampleInputEmail1">
+                                                    </div>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Semester Closing Dates</label>
+                                                        <input type="date" required name="semester_end" class="form-control">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card-footer">
+                                                <button type="submit" name="add_school_calendar" class="btn btn-primary">Submit</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
@@ -132,8 +131,8 @@ require_once('partials/_head.php');
                     </div>
                 </div>
             </div>
-        <?php require_once('partials/_footer.php');
-        } ?>
+        </div>
+        <?php require_once('partials/_footer.php'); ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>
