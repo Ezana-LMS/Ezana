8d38db0cdfd896793633748250b1d2f0ea518e9b
MartMbithi <martdevelopers254@gmail.com>
2020-11-10 14:45:04 +0300

    Add NoteMemo Back End

--- add_departmental_notememos.php
+++ add_departmental_notememos.php
@@ -5,29 +5,37 @@ require_once('configs/checklogin.php');
 require_once('configs/codeGen.php');
 check_login();
 if (isset($_POST['add_memo'])) {
-
-    $id = $_POST['id'];
-    $department_id = $_POST['department_id'];
-    $department_name  = $_POST['department_name'];
-    $departmental_memo = $_POST['departmental_memo'];
-    $attachments = $_FILES['attachments']['name'];
-    move_uploaded_file($_FILES["attachments"]["tmp_name"], "dist/memos/" . $_FILES["attachments"]["name"]);
-    $created_at = date('d M Y g:i');
-    $type = $_POST['type'];
-    $faculty = $_GET['faculty'];
-
-    $query = "INSERT INTO ezanaLMS_DepartmentalMemos (id, department_id, department_name, departmental_memo, attachments, created_at, type, faculty_id) VALUES(?,?,?,?,?,?,?,?)";
-    $stmt = $mysqli->prepare($query);
-    $rc = $stmt->bind_param('ssssssss', $id, $department_id, $department_name, $departmental_memo, $attachments, $created_at, $type, $faculty);
-    $stmt->execute();
-    if ($stmt) {
-        $success = "Departmental NoteMemo Added" && header("refresh:1; url=add_departmental_notememos.php");
+    //Error Handling
+    $error = 0;
+    
+    if (isset($_POST['department_id']) && !empty($_POST['department_id'])) {
+        $department_id = mysqli_real_escape_string($mysqli, trim($_POST['department_id']));
     } else {
-        $info = "Please Try Again Or Try Later";
+        $error = 1;
+        $err = "Department Name / ID  Cannot Be Empty";
+    }
+    if (!$error) {
+        $id = $_POST['id'];
+        $department_id = $_POST['department_id'];
+        $department_name  = $_POST['department_name'];
+        $departmental_memo = $_POST['departmental_memo'];
+        $attachments = $_FILES['attachments']['name'];
+        move_uploaded_file($_FILES["attachments"]["tmp_name"], "EzanaLMSData/memos/" . $_FILES["attachments"]["name"]);
+        $created_at = date('d M Y g:i');
+        $type = $_POST['type'];
+        $faculty = $_GET['faculty'];
+
+        $query = "INSERT INTO ezanaLMS_DepartmentalMemos (id, department_id, department_name, departmental_memo, attachments, created_at, type, faculty_id) VALUES(?,?,?,?,?,?,?,?)";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssssssss', $id, $department_id, $department_name, $departmental_memo, $attachments, $created_at, $type, $faculty);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Departmental NoteMemo Added" && header("refresh:1; url=add_departmental_notememos.php");
+        } else {
+            $info = "Please Try Again Or Try Later";
+        }
     }
 }
-
-
 
 require_once('partials/_head.php');
 ?>
@@ -81,27 +89,56 @@ require_once('partials/_head.php');
                                                 <div class="row">
                                                     <div class="form-group col-md-4">
                                                         <label for="">Department Name</label>
-                                                        <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
-                                                        <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                        <select class='form-control basic' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
+                                                            <option selected>Select Department Name</option>
+                                                            <?php
+                                                            $ret = "SELECT * FROM `ezanaLMS_Departments`  WHERE faculty_id = '$faculty' ";
+                                                            $stmt = $mysqli->prepare($ret);
+                                                            $stmt->execute(); //ok
+                                                            $res = $stmt->get_result();
+                                                            while ($dep = $res->fetch_object()) {
+                                                            ?>
+                                                                <option><?php echo $dep->name; ?></option>
+                                                            <?php } ?>
+                                                        </select>
                                                     </div>
                                                     <div class="form-group col-md-4">
-                                                        <label for="">Department Number / Code</label>
-                                                        <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                                        <label for="">Department ID</label>
+                                                        <input type="text" id="DepartmentID" readonly required name="department_id" class="form-control">
                                                     </div>
                                                     <div class="form-group col-md-4">
-                                                        <label for="">Department HOD</label>
-                                                        <input type="text" required name="hod" class="form-control">
+                                                        <label for="">Type</label>
+                                                        <select class='form-control basic1' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
+                                                            <option selected>Select If Notice Or Memo</option>
+                                                            <option>Notice</option>
+                                                            <option>Memo</option>
+                                                        </select>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="form-group col-md-12">
-                                                        <label for="exampleInputPassword1">Department Details</label>
-                                                        <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
+                                                        <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                    </div>
+                                                    <div class="form-group col-md-12">
+                                                        <label for="">Upload Departmental Memo (PDF Or Docx)</label>
+                                                        <div class="input-group">
+                                                            <div class="custom-file">
+                                                                <input name="attachments" type="file" class="custom-file-input">
+                                                                <label class="custom-file-label" for="exampleInputFile">Choose file </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <h2 class="text-center">Or </h2>
+                                                <div class="row">
+                                                    <div class="form-group col-md-12">
+                                                        <label for="exampleInputPassword1">Type Departmental Memo</label>
+                                                        <textarea name="departmental_memo" id="textarea" rows="10" class="form-control"></textarea>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="card-footer">
-                                                <button type="submit" name="add_dept" class="btn btn-primary">Add Department</button>
+                                                <button type="submit" name="add_memo" class="btn btn-primary">Add Departmental Memo</button>
                                             </div>
                                         </form>
                                     </div>
