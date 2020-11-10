6719dc900dfb8d41808c38716d3d96c27f9acb8b
MartMbithi <martdevelopers254@gmail.com>
2020-11-10 12:22:54 +0300

    Add course front end and ajax on department details

--- add_course.php
+++ add_course.php
@@ -80,14 +80,14 @@ require_once('partials/_head.php');
                     <div class="container">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1 class="m-0 text-dark">Add Department</h1>
+                                <h1 class="m-0 text-dark">Add Course</h1>
                             </div>
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
                                     <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                     <li class="breadcrumb-item"><a href="dashboard.php">Faculties</a></li>
                                     <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $faculty; ?>"><?php echo $f->name; ?></a></li>
-                                    <li class="breadcrumb-item active">Add Department</li>
+                                    <li class="breadcrumb-item active">Add Course</li>
                                 </ol>
                             </div>
                         </div>
@@ -106,29 +106,46 @@ require_once('partials/_head.php');
                                         <form method="post" enctype="multipart/form-data" role="form">
                                             <div class="card-body">
                                                 <div class="row">
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Department Name</label>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Course Name</label>
                                                         <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
                                                         <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
                                                     </div>
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Department Number / Code</label>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Course Number / Code</label>
                                                         <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
                                                     </div>
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Department HOD</label>
-                                                        <input type="text" required name="hod" class="form-control">
+                                                </div>
+                                                <div class="row">
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Department Name</label>
+                                                        <select class='form-control basic' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
+                                                            <option selected>Select Department Name</option>
+                                                            <?php
+                                                            $ret = "SELECT * FROM `ezanaLMS_Departments`  ";
+                                                            $stmt = $mysqli->prepare($ret);
+                                                            $stmt->execute(); //ok
+                                                            $res = $stmt->get_result();
+                                                            while ($dep = $res->fetch_object()) {
+                                                            ?>
+                                                                <option><?php echo $dep->name; ?></option>
+                                                            <?php } ?>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="">Department ID</label>
+                                                        <input type="text" id="DepartmentID" readonly required name="department_id" class="form-control">
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="form-group col-md-12">
-                                                        <label for="exampleInputPassword1">Department Details</label>
-                                                        <textarea name="details" id="textarea" rows="10" class="form-control"></textarea>
+                                                        <label for="exampleInputPassword1">Course Description</label>
+                                                        <textarea required name="details" id="textarea" rows="10" class="form-control"></textarea>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="card-footer">
-                                                <button type="submit" name="add_dept" class="btn btn-primary">Add Department</button>
+                                                <button type="submit" name="add_course" class="btn btn-primary">Add Course</button>
                                             </div>
                                         </form>
                                     </div>
