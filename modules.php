f4f6cecda43ddf216146de42a5a1469591603405
MartMbithi <martdevelopers254@gmail.com>
2020-11-10 13:22:33 +0300

    Delete / View / Manage Module

---
+++ modules.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $faculty = $_GET['faculty'];
+    $adn = "DELETE FROM ezanaLMS_Modules WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=modules.php?faculty=$faculty");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+require_once('partials/_head.php');
+
+
+require_once('partials/_head.php');
+
+$faculty = $_GET['faculty'];
+$ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($f = $res->fetch_object()) {
+?>
+
+    <body class="hold-transition sidebar-collapse layout-top-nav">
+        <div class="wrapper">
+
+            <!-- Navbar -->
+            <?php require_once('partials/_faculty_nav.php'); ?>
+            <!-- /.navbar -->
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark"><?php echo $f->name; ?> Modules</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                    <li class="breadcrumb-item active"> Modules </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="container">
+                        <section class="content">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h2 class="text-right">
+                                                <a class="btn btn-outline-success" href="add_module.php?faculty=<?php echo $f->id; ?>">
+                                                    Register New Module
+                                                </a>
+                                            </h2>
+                                        </div>
+                                        <div class="card-body">
+                                            <table id="example1" class="table table-bordered table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Name</th>
+                                                        <th>Code</th>
+                                                        <th>Course</th>
+                                                        <th>Duration</th>
+                                                        <th>Weight Percentage</th>
+                                                        <th>Lectures Per Week</th>
+                                                        <th>Actions</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php
+                                                    $ret = "SELECT * FROM `ezanaLMS_Modules` WHERE faculty_id = '$f->id' ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    $cnt = 1;
+                                                    while ($mod = $res->fetch_object()) {
+                                                    ?>
+
+                                                        <tr>
+                                                            <td><?php echo $cnt; ?></td>
+                                                            <td><?php echo $mod->name; ?></td>
+                                                            <td><?php echo $mod->code; ?></td>
+                                                            <td><?php echo $mod->course_name; ?></td>
+                                                            <td><?php echo $mod->course_duration; ?></td>
+                                                            <td><?php echo $mod->weight_percentage; ?></td>
+                                                            <td><?php echo $mod->lectures_number; ?></td>
+                                                            <td>
+                                                                <a class="badge badge-success" href="view_module.php?view=<?php echo $mod->id; ?>">
+                                                                    <i class="fas fa-eye"></i>
+                                                                    View
+                                                                </a>
+
+                                                                <a class="badge badge-danger" href="modules.php?delete=<?php echo $mod->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                                    <i class="fas fa-trash"></i>
+                                                                    Delete
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    <?php $cnt = $cnt + 1;
+                                                    } ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        <?php require_once('partials/_footer.php');
+    } ?>
+        </div>
+        <?php require_once('partials/_scripts.php'); ?>
+    </body>
+
+    </html>
