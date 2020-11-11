499f2c8ea0f8156b23be17ef8a47303a26ef8b31
MartMbithi <martdevelopers254@gmail.com>
2020-11-11 19:31:00 +0300

    Delete / Manage On Class Recordings

--- class_recordings.php
+++ class_recordings.php
@@ -4,21 +4,21 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
 
+//delete
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
     $faculty = $_GET['faculty'];
-    $adn = "DELETE FROM ezanaLMS_Modules WHERE id=?";
+    $adn = "DELETE FROM ezanaLMS_ClassRecordings WHERE id=?";
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('s', $delete);
     $stmt->execute();
     $stmt->close();
     if ($stmt) {
-        $success = "Deleted" && header("refresh:1; url=modules.php?faculty=$faculty");
+        $success = "Deleted" && header("refresh:1; url=class_recordings.php?faculty=$faculty");
     } else {
         $info = "Please Try Again Or Try Later";
     }
 }
-
 require_once('partials/_head.php');
 
 $faculty = $_GET['faculty'];
@@ -31,23 +31,21 @@ while ($f = $res->fetch_object()) {
 
     <body class="hold-transition sidebar-collapse layout-top-nav">
         <div class="wrapper">
-
             <!-- Navbar -->
             <?php require_once('partials/_faculty_nav.php'); ?>
             <!-- /.navbar -->
-
             <div class="content-wrapper">
                 <div class="content-header">
                     <div class="container">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1 class="m-0 text-dark"><?php echo $f->name; ?> Modules</h1>
+                                <h1 class="m-0 text-dark">Class Recordings</h1>
                             </div>
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
                                     <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                     <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
-                                    <li class="breadcrumb-item active"> Modules </li>
+                                    <li class="breadcrumb-item active"> Class Recordings </li>
                                 </ol>
                             </div>
                         </div>
@@ -62,16 +60,10 @@ while ($f = $res->fetch_object()) {
                                     <div class="card">
                                         <div class="card-header">
                                             <h2 class="text-right">
-                                                <a class="btn btn-outline-success" href="add_module.php?faculty=<?php echo $f->id; ?>">
-                                                    Register New Module
-                                                </a>
-
-                                                <a class="btn btn-outline-success" href="module_notices.php?faculty=<?php echo $f->id; ?>">
-                                                    Module Notices
-                                                </a>
-
-                                                <a class="btn btn-outline-success" href="module_reading_materials.php?faculty=<?php echo $f->id; ?>">
-                                                    Module Reading Materials
+                                                <a class="btn btn-outline-success" href="add_class_recording.php?faculty=<?php echo $f->id; ?>">
+                                                    <i class="fas fa-plus"></i>
+                                                    <i class="fas fa-file-video"></i>
+                                                    Upload Class Recoding
                                                 </a>
                                             </h2>
                                         </div>
@@ -80,31 +72,38 @@ while ($f = $res->fetch_object()) {
                                                 <thead>
                                                     <tr>
                                                         <th>#</th>
-                                                        <th>Module Name</th>
-                                                        <th>Module Code</th>
-                                                        <th>Course Name</th>
-                                                        <th>Manage Module</th>
+                                                        <th>Class Name</th>
+                                                        <th>Lecturer </th>
+                                                        <th>Date Uploaded</th>
+                                                        <th>Actions</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     <?php
-                                                    $ret = "SELECT * FROM `ezanaLMS_Modules` WHERE faculty_id = '$f->id' ";
+                                                    $ret = "SELECT * FROM `ezanaLMS_ClassRecordings` WHERE faculty_id = '$f->id'  ";
                                                     $stmt = $mysqli->prepare($ret);
                                                     $stmt->execute(); //ok
                                                     $res = $stmt->get_result();
                                                     $cnt = 1;
-                                                    while ($mod = $res->fetch_object()) {
+                                                    while ($cr = $res->fetch_object()) {
                                                     ?>
-
-                                                        <tr class="table-row" data-href="view_module.php?view=<?php echo $mod->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                        <tr>
                                                             <td><?php echo $cnt; ?></td>
-                                                            <td><?php echo $mod->name; ?></td>
-                                                            <td><?php echo $mod->code; ?></td>
-                                                            <td><?php echo $mod->course_name; ?></td>
+                                                            <td><?php echo $cr->class_name; ?></td>
+                                                            <td><?php echo $cr->lecturer_name; ?></td>
+                                                            <td><?php echo date('d M Y', strtotime($cr->created_at)); ?></td>
                                                             <td>
-                                                                <a class="badge badge-danger" href="modules.php?delete=<?php echo $mod->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                                <a class="badge badge-success" href="view_class_recording.php?watch=<?php echo $cr->id; ?>">
+                                                                    <i class="fas fa-play"></i>
+                                                                    Watch Recording
+                                                                </a>
+                                                                <a class="badge badge-primary" href="update_class_recording.php?update=<?php echo $cr->id; ?>">
+                                                                    <i class="fas fa-edit"></i>
+                                                                    Update Recording
+                                                                </a>
+                                                                <a class="badge badge-danger" href="class_recording.php?delete=<?php echo $cr->id; ?>&faculty=<?php echo $f->id; ?>">
                                                                     <i class="fas fa-trash"></i>
-                                                                    Delete Module
+                                                                    Delete Recording
                                                                 </a>
                                                             </td>
                                                         </tr>
