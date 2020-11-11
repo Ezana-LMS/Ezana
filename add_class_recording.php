e0d9233e555afb9a81fe1dac893e59eb4116053d
MartMbithi <martdevelopers254@gmail.com>
2020-11-11 19:33:50 +0300

    Upload Class Recording Backend

---
+++ add_class_recording.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+require_once('configs/codeGen.php');
+check_login();
+if (isset($_POST['add_class_recording'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['class_name']) && !empty($_POST['class_name'])) {
+        $class_name = mysqli_real_escape_string($mysqli, trim($_POST['class_name']));
+    } else {
+        $error = 1;
+        $err = "Class Name Cannot Be Empty";
+    }
+    if (isset($_POST['lecturer_name']) && !empty($_POST['lecturer_name'])) {
+        $lecturer_name = mysqli_real_escape_string($mysqli, trim($_POST['lecturer_name']));
+    } else {
+        $error = 1;
+        $err = "Lectuer Name Cannot Be Empty";
+    }
+    if (isset($_POST['details']) && !empty($_POST['details'])) {
+        $details = mysqli_real_escape_string($mysqli, trim($_POST['details']));
+    } else {
+        $error = 1;
+        $err = "Video Transcription Cannot Be Empty";
+    }
+    if (!$error) {
+        $id = $_POST['id'];
+        $class_name = $_POST['class_name'];
+        $lecturer_name  = $_POST['lecturer_name'];
+        $external_link = $_POST['external_link'];
+        $details  = $_POST['details'];
+        $created_at  = date('d M Y');
+        $video = $_FILES['video']['name'];
+        move_uploaded_file($_FILES["video"]["tmp_name"], "dist/ClassVideos/" . $_FILES["video"]["name"]);
+
+        $query = "INSERT INTO ezanaLMS_ClassRecordings (id, class_name, lecturer_name, external_link, details, created_at, video) VALUES(?,?,?,?,?,?,?)";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('sssssss', $id, $class_name, $lecturer_name, $external_link, $details, $created_at, $video);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Class Recoding Added" && header("refresh:1; url=add_class_recording.php");
+        } else {
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
+
+require_once('partials/_head.php');
+?>
