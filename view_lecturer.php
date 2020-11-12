ea42b760e13ee435d3ecf12a4e7ac639c4fec861
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 12:25:49 +0300

    View Lec

---
+++ view_lecturer.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+?>
+
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_nav.php');
+        $view = $_GET['view'];
+        $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id ='$view' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($admin = $res->fetch_object()) {
+            //Get Default Profile Picture
+            if ($admin->profile_pic == '') {
+                $dpic = "<img class='profile-user-img img-fluid img-circle' src='dist/img/logo.jpeg' alt='User profile picture'>";
+            } else {
+                $dpic = "<img class='profile-user-img img-fluid img-circle' src='dist/img/system_admin/$admin->profile_pic' alt='User profile picture'>";
+            }
+        ?>
+            <!-- /.navbar -->
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark"> <?php echo $admin->name; ?> Profile </h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="administrators.php">Administrators</a></li>
+                                    <li class="breadcrumb-item active">View</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3">
+
+                                <!-- Profile Image -->
+                                <div class="card card-primary card-outline">
+                                    <div class="card-body box-profile">
+                                        <div class="text-center">
+                                            <?php echo $dpic; ?>
+                                        </div>
+
+                                        <h3 class="profile-username text-center"><?php echo $admin->name; ?></h3>
+
+                                        <p class="text-muted text-center"><?php echo $admin->rank; ?></p>
+
+                                        <ul class="list-group list-group-unbordered mb-3">
+                                            <li class="list-group-item">
+                                                <b>Email: </b> <a class="float-right"><?php echo $admin->email; ?></a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Phone: </b> <a class="float-right"><?php echo $admin->phone; ?></a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Address</b> <a class="float-right"><?php echo $admin->adr; ?></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-md-9">
+                                <div class="card">
+                                    <div class="card-header p-2">
+                                        <ul class="nav nav-pills">
+                                            <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+                                            <li class="nav-item"><a class="nav-link " href="#changePassword" data-toggle="tab">Change Password</a></li>
+                                        </ul>
+                                    </div><!-- /.card-header -->
+                                    <div class="card-body">
+                                        <div class="tab-content">
+                                            <div class="active tab-pane" id="settings">
+                                                <form method='post' enctype="multipart/form-data" class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" required class="form-control" value="<?php echo $admin->name; ?>" name="name" id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="email" required class="form-control" value="<?php echo $admin->email; ?>" name="email" id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Phone Number</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" required class="form-control" value="<?php echo $admin->phone; ?>" name="phone" id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">User Previledge</label>
+                                                        <div class="col-sm-10">
+                                                            <select name="rank" class="form-control basic">
+                                                                <option selected><?php echo $admin->rank; ?></option>
+                                                                <option>System Administrator</option>
+                                                                <option>Education Administrator</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputExperience" class="col-sm-2 col-form-label">Address</label>
+                                                        <div class="col-sm-10">
+                                                            <textarea required class="form-control" name="adr" id="textarea"><?php echo $admin->adr; ?></textarea>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputSkills" class="col-sm-2 col-form-label">Profile Picture</label>
+                                                        <div class="col-sm-10">
+                                                            <div class="input-group">
+                                                                <div class="custom-file">
+                                                                    <input type="file" name="profile_pic" class="custom-file-input" id="exampleInputFile">
+                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button type="submit" name="profile_update" class="btn btn-primary">Update Profile</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                            <div class="tab-pane" id="changePassword">
+                                                <form method='post' class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Old Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" name="old_password" required class="form-control" id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" name="new_password" required class="form-control" id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Confirm New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" name="confirm_password" required class="form-control" id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button type="submit" name="change_password" class="btn btn-primary">Change Password</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php require_once('partials/_footer.php');
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
