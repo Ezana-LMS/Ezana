30516bd2905b0b955c0609cc5b61e2c672f42ee8
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 14:27:04 +0300

    View Student Details

--- view_student.php
+++ view_student.php
@@ -5,3 +5,63 @@ require_once('configs/checklogin.php');
 check_login();
 require_once('partials/_head.php');
 ?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+            
+            $view = $_GET['view'];
+            $ret = "SELECT * FROM `ezanaLMS_Students` WHERE id ='$view' ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($std = $res->fetch_object()) {
+                //Get Default Profile Picture
+                if ($std->profile_pic == '') {
+                    $dpic = "<img class='profile-user-img img-fluid img-circle' src='dist/img/logo.jpeg' alt='User profile picture'>";
+                } else {
+                    $dpic = "<img class='profile-user-img img-fluid img-circle' src='dist/img/students/$std->profile_pic' alt='User profile picture'>";
+                }
+        ?>
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark"><?php echo $std->name; ?> Profile</h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="students.php?faculty=<?php echo $f->id; ?>">Students</a></li>
+                                        <li class="breadcrumb-item active"> Profile </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <div class="container">
+
+                        </div>
+                    </div>
+                </div>
+        <?php require_once('partials/_footer.php');
+            }
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
