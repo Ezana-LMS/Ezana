acb5057cab94ca3a361f3d91b1cd4a398dfabe32
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 11:52:08 +0300

    Lecturers BackEnd

---
+++ lecturers.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+//Delete
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_Lecturers WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=manage_lectures.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+require_once('partials/_head.php');
