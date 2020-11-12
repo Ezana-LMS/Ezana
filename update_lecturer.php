6f71be2955af597a44ee71960dae53b0c3e3d23e
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 12:20:48 +0300

    Update Lecturer Front End Logic

--- update_lecturer.php
+++ update_lecturer.php
@@ -97,3 +97,112 @@ if (isset($_POST['change_password'])) {
 
 require_once('partials/_head.php');
 ?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+            $update = $_GET['update'];
+            $ret = "SELECT * FROM `ezanaLMS_Lecturers` WHERE id ='$update' ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($lec = $res->fetch_object()) {
+        ?>
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark">Add Lecturers</h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="lecturers.php?faculty=<?php echo $f->id; ?>">Lecturers</a></li>
+                                        <li class="breadcrumb-item active">Update </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <div class="container">
+                            <section class="content">
+                                <div class="container-fluid">
+                                    <div class="col-md-12">
+                                        <div class="card card-primary">
+                                            <div class="card-header">
+                                                <h3 class="card-title">Fill All Required Fields</h3>
+                                            </div>
+                                            <form method="post" enctype="multipart/form-data" role="form">
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Name</label>
+                                                            <input type="text" required name="name" value="<?php echo $lec->name; ?>" class="form-control" id="exampleInputEmail1">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Number</label>
+                                                            <input type="text" required name="number" value="<?php echo $lec->name; ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">ID / Passport Number</label>
+                                                            <input type="text" value="<?php echo $lec->idno; ?>" required name="idno" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="form-group col-md-6">
+                                                            <label for="">Email</label>
+                                                            <input type="email" value="<?php echo $lec->email; ?>" required name="email" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-md-6">
+                                                            <label for="">Phone Number</label>
+                                                            <input type="text" required value="<?php echo $lec->phone; ?>" name="phone" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="form-group col-md-12">
+                                                            <label for="">Profile Picture</label>
+                                                            <div class="input-group">
+                                                                <div class="custom-file">
+                                                                    <input required name="profile_pic" type="file" class="custom-file-input">
+                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="form-group col-md-12">
+                                                            <label for="exampleInputPassword1">Address</label>
+                                                            <textarea required name="adr" id="textarea" rows="5" class="form-control"><?php echo $lec->adr; ?></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="card-footer">
+                                                    <button type="submit" name="update_lec" class="btn btn-primary">Submit</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+        <?php require_once('partials/_footer.php');
+            }
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
