d184107cfcf494261a91f099640ebf49a3ff5a92
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 10:34:18 +0300

    Delete / Manage Front End And Backend Logic On Past Exam Papers

--- past_exam_papers.php
+++ past_exam_papers.php
@@ -7,35 +7,35 @@ check_login();
 //delete
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
-    $faculty = $_GET['faculty'];
-    $adn = "DELETE FROM ezanaLMS_Courses WHERE id=?";
+    $adn = "DELETE FROM ezanaLMS_PastPapers WHERE id=?";
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('s', $delete);
     $stmt->execute();
     $stmt->close();
     if ($stmt) {
-        $success = "Deleted" && header("refresh:1; url=courses.php?faculty=$faculty");
+        $success = "Deleted" && header("refresh:1; url=past_papers.php");
     } else {
         $info = "Please Try Again Or Try Later";
     }
 }
-
-
 require_once('partials/_head.php');
 
-$faculty = $_GET['faculty'];
-$ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
-$stmt = $mysqli->prepare($ret);
-$stmt->execute(); //ok
-$res = $stmt->get_result();
-while ($f = $res->fetch_object()) {
 ?>
 
-    <body class="hold-transition sidebar-collapse layout-top-nav">
-        <div class="wrapper">
 
-            <!-- Navbar -->
-            <?php require_once('partials/_faculty_nav.php'); ?>
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+            require_once('partials/_faculty_nav.php');
+        ?>
             <!-- /.navbar -->
 
             <div class="content-wrapper">
@@ -43,13 +43,13 @@ while ($f = $res->fetch_object()) {
                     <div class="container">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1 class="m-0 text-dark"><?php echo $f->name; ?> Courses</h1>
+                                <h1 class="m-0 text-dark"><?php echo $f->name; ?> Past Exam Papers</h1>
                             </div>
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
                                     <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                     <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
-                                    <li class="breadcrumb-item active"> Courses </li>
+                                    <li class="breadcrumb-item active"> Past Exam Papers </li>
                                 </ol>
                             </div>
                         </div>
@@ -58,71 +58,14 @@ while ($f = $res->fetch_object()) {
 
                 <div class="content">
                     <div class="container">
-                        <section class="content">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h2 class="text-right">
-                                                <a class="btn btn-outline-success" href="add_course.php?faculty=<?php echo $f->id; ?>">
-                                                    Register New Course
-                                                </a>
-                                            </h2>
-                                        </div>
-                                        <div class="card-body">
-                                            <table id="example1" class="table table-bordered table-striped">
-                                                <thead>
-                                                    <tr>
-                                                        <th>#</th>
-                                                        <th>Course Code</th>
-                                                        <th>Course Name</th>
-                                                        <th>Department Name</th>
-                                                        <th>Manage Course</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <?php
-                                                    $ret = "SELECT * FROM `ezanaLMS_Courses` WHERE faculty_id = '$f->id'  ";
-                                                    $stmt = $mysqli->prepare($ret);
-                                                    $stmt->execute(); //ok
-                                                    $res = $stmt->get_result();
-                                                    $cnt = 1;
-                                                    while ($course = $res->fetch_object()) {
-                                                    ?>
 
-                                                        <tr class="table-row" data-href="view_course.php?department=<?php echo $course->department_id; ?>&view=<?php echo $course->id; ?>&faculty=<?php echo $course->faculty_id; ?>">
-                                                            <td><?php echo $cnt; ?></td>
-                                                            <td><?php echo $course->code; ?></td>
-                                                            <td><?php echo $course->name; ?></td>
-                                                            <td><?php echo $course->department_name; ?></td>
-                                                            <td>
-                                                                <a class="badge badge-success" href="view_course.php?department=<?php echo $course->department_id; ?>&view=<?php echo $course->id; ?>&faculty=<?php echo $course->faculty_id; ?>">
-                                                                    <i class="fas fa-eye"></i>
-                                                                    View Course
-                                                                </a>
-
-                                                                <a class="badge badge-danger" href="courses.php?delete=<?php echo $course->id; ?>&faculty=<?php echo $f->id; ?>">
-                                                                    <i class="fas fa-trash"></i>
-                                                                    Delete
-                                                                </a>
-                                                            </td>
-                                                        </tr>
-                                                    <?php $cnt = $cnt + 1;
-                                                    } ?>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
                     </div>
                 </div>
             </div>
         <?php require_once('partials/_footer.php');
-    } ?>
-        </div>
-        <?php require_once('partials/_scripts.php'); ?>
-    </body>
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
 
-    </html>
+</html>
