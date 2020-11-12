49026eb62898579210ffacfe5160307334f2db0e
MartMbithi <martdevelopers254@gmail.com>
2020-11-12 12:03:45 +0300

    Delete / View Manage Lecturers

--- lecturers.php
+++ lecturers.php
@@ -3,18 +3,89 @@ session_start();
 require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
+
 //Delete
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
+    $faculty = $_GET['faculty'];
     $adn = "DELETE FROM ezanaLMS_Lecturers WHERE id=?";
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('s', $delete);
     $stmt->execute();
     $stmt->close();
     if ($stmt) {
-        $success = "Deleted" && header("refresh:1; url=manage_lectures.php");
+        $success = "Deleted" && header("refresh:1; url=lectures.php?faculty=$faculty");
     } else {
         $info = "Please Try Again Or Try Later";
     }
 }
 require_once('partials/_head.php');
+
+?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+        ?>
+            <!-- /.navbar -->
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark"><?php echo $f->name; ?> Lecturers</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                    <li class="breadcrumb-item active"> Lecturers </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="container">
+                        <section class="content">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h2 class="text-right">
+                                                <a class="btn btn-outline-success" href="add_lecturer.php?faculty=<?php echo $f->id; ?>">
+                                                    Register New Lecturer
+                                                </a>
+                                                <a class="btn btn-outline-primary" href="import_lecturer.php?faculty=<?php echo $f->id; ?>">
+                                                    Import Lecturers Details
+                                                </a>
+                                            </h2>
+                                        </div>
+                                        <div class="card-body">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        <?php require_once('partials/_footer.php');
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
