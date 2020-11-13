b7bd3cd8131c6de98d3b53fbf2882ad8fbc0ecb1
MartMbithi <martdevelopers254@gmail.com>
2020-11-13 11:15:13 +0300

    Front End Logic On Groups

--- student_groups.php
+++ student_groups.php
@@ -58,7 +58,69 @@ require_once('partials/_head.php');
 
                 <div class="content">
                     <div class="container">
+                        <section class="content">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h2 class="text-right">
+                                                <a class="btn btn-outline-success" href="add_groups.php">
+                                                    <i class="fas fa-plus"></i>
+                                                    Create New Group
+                                                </a>
 
+                                            </h2>
+                                        </div>
+                                        <div class="card-body">
+                                            <table id="example1" class="table table-bordered table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Group Code Number</th>
+                                                        <th>Group Name</th>
+                                                        <th>Created At</th>
+                                                        <th>Manage Groups</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php
+                                                    $ret = "SELECT * FROM `ezanaLMS_Groups` WHERE faculty_id = '$f->id'  ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    $cnt = 1;
+                                                    while ($g = $res->fetch_object()) {
+                                                    ?>
+
+                                                        <tr>
+                                                            <td><?php echo $cnt; ?></td>
+                                                            <td><?php echo $g->code; ?></td>
+                                                            <td><?php echo $g->name; ?></td>
+                                                            <td><?php echo $g->created_at; ?></td>
+                                                            <td>
+                                                                <a class="badge badge-success" href="view_group.php?view=<?php echo $g->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                                    <i class="fas fa-eye"></i>
+                                                                    View Members
+                                                                </a>
+                                                                <a class="badge badge-primary" href="update_group.php?update=<?php echo $g->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                                    <i class="fas fa-edit"></i>
+                                                                    Update
+                                                                </a>
+                                                                <a class="badge badge-danger" href="student_groups.php?delete=<?php echo $g->id; ?>&faculty=<?php echo $f->id; ?>">
+                                                                    <i class="fas fa-trash"></i>
+                                                                    Delete
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    <?php $cnt = $cnt + 1;
+                                                    } ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
