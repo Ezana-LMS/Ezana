f154b2854ae58760e3df3db0b11852225938e48d
MartMbithi <martdevelopers254@gmail.com>
2020-11-14 12:08:32 +0300

    Scaffolded Group Project

--- grade_group_project.php
+++ grade_group_project.php
@@ -57,3 +57,57 @@ if (isset($_POST['add_score'])) {
 }
 require_once('partials/_head.php');
 ?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+            $project_id = $_GET['project_id'];
+            $ret = "SELECT * FROM `ezanaLMS_GroupsAssignments` WHERE id ='$project_id'  ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($gcode = $res->fetch_object()) {
+        ?>
+                <!-- /.navbar -->
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark"><?php echo $g->name; ?></h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="student_groups.php?faculty=<?php echo $f->id; ?>">Student Groups</a></li>
+                                        <li class="breadcrumb-item"><a href="view_student_group.php?&name=<?php echo $g->name; ?>&code=<?php echo $g->code; ?>&view=<?php echo $g->id; ?>&faculty=<?php echo $f->id; ?>"><?php echo $g->code; ?></a></li>
+                                        <li class="breadcrumb-item active"> =Grade Assignment </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <div class="container">
+                            
+                        </div>
+                    </div>
+                </div>
+        <?php require_once('partials/_footer.php');
+            }
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
