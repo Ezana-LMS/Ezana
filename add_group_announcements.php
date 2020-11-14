751fc01136843fda4b140ffadf3be8a6d39a5373
MartMbithi <martdevelopers254@gmail.com>
2020-11-14 14:09:09 +0300

    Implemented View On Group Announcemet

--- add_group_announcements.php
+++ add_group_announcements.php
@@ -64,29 +64,9 @@ require_once('partials/_head.php');
 
                 <div class="content">
                     <div class="container">
-                        <section class="content-header">
-                            <div class="container-fluid">
-                                <div class="row mb-2">
-                                    <div class="col-sm-6">
-                                        <h1>Add Group Notices / Announcements</h1>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <ol class="breadcrumb float-sm-right">
-                                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                            <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                            <li class="breadcrumb-item"><a href="group_announcements.php">Groups</a></li>
-                                            <li class="breadcrumb-item active">Add Announcement</li>
-                                        </ol>
-                                    </div>
-                                </div>
-                            </div><!-- /.container-fluid -->
-                        </section>
-
-                        <!-- Main content -->
                         <section class="content">
                             <div class="container-fluid">
                                 <div class="col-md-12">
-                                    <!-- general form elements -->
                                     <div class="card card-primary">
                                         <div class="card-header">
                                             <h3 class="card-title">Fill All Required Fields</h3>
---
+++ view_group_announcement.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+            $view = $_GET['view'];
+            $ret = "SELECT * FROM `ezanaLMS_GroupsAnnouncements` WHERE id ='$view'  ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($ga = $res->fetch_object()) {
+        ?>
+                <!-- /.navbar -->
+
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark"><?php echo $ga->group_name; ?> Announcement</h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="student_groups.php?faculty=<?php echo $f->id; ?>">Student Groups</a></li>
+                                        <li class="breadcrumb-item"><a href="student_group_notices.php?faculty=<?php echo $f->id; ?>">Notices</a></li>
+                                        <li class="breadcrumb-item active"> View </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <div class="container">
+                            <section class="content">
+                                <div class="container-fluid">
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-header">
+                                            <h3 class="card-title">
+                                                Announcement Posted On <span class="text-success"> <?php echo  date('d M Y', strtotime($ga->created_at)); ?></span> By <span class="text-warning"><?php echo $ga->created_by; ?></span>
+                                            </h3>
+                                        </div>
+                                        <div class="card-body">
+                                            <ul class="nav nav-tabs" id="custom-content-below-tab" role="tablist">
+                                                <li class="nav-item">
+                                                    <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill" href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home" aria-selected="true">Announcement</a>
+                                                </li>
+                                            </ul>
+                                            <div class="tab-content" id="custom-content-below-tabContent">
+                                                <div class="tab-pane fade show active" id="custom-content-below-home" role="tabpanel" aria-labelledby="custom-content-below-home-tab">
+                                                    <br>
+                                                    <?php echo $ga->announcement; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+        <?php require_once('partials/_footer.php');
+            }
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
