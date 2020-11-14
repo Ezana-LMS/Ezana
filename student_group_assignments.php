c7b813dcc712de32e490bfe56dce3bdd1daa6c5b
MartMbithi <martdevelopers254@gmail.com>
2020-11-14 12:59:55 +0300

    Group Assignments Scaffold

--- student_group_assignments.php
+++ student_group_assignments.php
@@ -7,3 +7,51 @@ check_login();
 require_once('partials/_head.php');
 
 ?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        require_once('partials/_faculty_nav.php');
+        $faculty = $_GET['faculty'];
+        $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($f = $res->fetch_object()) {
+        ?>
+            <!-- /.navbar -->
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark">Group Assignments</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
+                                    <li class="breadcrumb-item"><a href="student_groups.php?faculty=<?php echo $f->id; ?>">Student Groups</a></li>
+                                    <li class="breadcrumb-item active"> Assignments </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="container">
+                        
+                    </div>
+                </div>
+            </div>
+        <?php require_once('partials/_footer.php');
+        } ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
