36cbaa727c4d269a0068ab5fbbfa78dd821f1105
MartMbithi <martdevelopers254@gmail.com>
2020-11-15 15:37:01 +0300

    Students Scaffold

--- dashboard.php
+++ dashboard.php
@@ -92,7 +92,7 @@ require_once('partials/_head.php');
 
                         <!-- Lecturers -->
                         <div class="col-12 col-sm-6 col-md-3">
-                            <a href="total_modules.php">
+                            <a href="total_lecs.php">
                                 <div class="info-box mb-3">
                                     <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-tie"></i></span>
                                     <div class="info-box-content">
@@ -105,9 +105,9 @@ require_once('partials/_head.php');
 
                         <!-- Students -->
                         <div class="col-12 col-sm-6 col-md-3">
-                            <a href="total_modules.php">
+                            <a href="total_students.php">
                                 <div class="info-box mb-3">
-                                    <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-graduate"></i></span>
+                                    <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-user-graduate"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">Total Students</span>
                                         <span class="info-box-number"><?php echo $students; ?></span>
---
+++ total_students.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+
+require_once('partials/_head.php');
+
+?>
+
+<body class="hold-transition sidebar-collapse layout-top-nav">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php require_once('partials/_nav.php'); ?>
+        <!-- /.navbar -->
+
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Students</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item active"> Students </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="content">
+                <div class="container">
+                    <section class="content">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <!-- <div class="card-header">
+                                            <h2 class="text-right">
+                                                <a class="btn btn-outline-success" href="add_module.php?faculty=<?php echo $f->id; ?>">
+                                                    Register New Module
+                                                </a>
+
+                                                <a class="btn btn-outline-success" href="module_notices.php?faculty=<?php echo $f->id; ?>">
+                                                    Module Notices
+                                                </a>
+
+                                                <a class="btn btn-outline-success" href="module_reading_materials.php?faculty=<?php echo $f->id; ?>">
+                                                    Module Reading Materials
+                                                </a>
+                                            </h2>
+                                        </div> -->
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <?php require_once('partials/_footer.php');
+        ?>
+    </div>
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
