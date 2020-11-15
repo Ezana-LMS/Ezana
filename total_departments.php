fa39b7eb691047780a55846e0ab6c5d1706c2e41
MartMbithi <martdevelopers254@gmail.com>
2020-11-15 15:27:50 +0300

    Departmental Export Function

--- total_departments.php
+++ total_departments.php
@@ -54,7 +54,7 @@ require_once('partials/_head.php');
                                             </h2>
                                         </div> -->
                                     <div class="card-body">
-                                        <table id="example1" class="table table-bordered table-striped">
+                                        <table id="export-dt" class="table table-bordered table-striped">
                                             <thead>
                                                 <tr>
                                                     <th>#</th>
