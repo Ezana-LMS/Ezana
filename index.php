71d8caa976cf14d0280dd6a1714f7ad09d388304
MartMbithi <martdevelopers254@gmail.com>
2020-11-18 09:54:41 +0300

    Removed Login Box Div

--- index.php
+++ index.php
@@ -23,57 +23,57 @@ require_once('partials/_head.php')
 ?>
 
 <body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="card">
-            <div class="card-body login-card-body">
-                <div class="login-logo">
-                    <img height="150" width="150" src="dist/img/logo.jpeg" alt="">
+    <!--  <div class="login-box"> -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <div class="login-logo">
+                <img height="150" width="150" src="dist/img/logo.jpeg" alt="">
+            </div>
+            <p class="login-box-msg">
+                Sign In To Start Your Session
+                <br>
+                Demo Credentials
+                <br>
+                <span class="text-success"> Email : sysadmin@ezana.org </span>
+                <br>
+                <span class="text-success">Passsword: 123 </span>
+            </p>
+
+            <form method="post">
+                <div class="input-group mb-3">
+                    <input type="email" required name="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
                 </div>
-                <p class="login-box-msg">
-                    Sign In To Start Your Session
-                    <br>
-                    Demo Credentials
-                    <br>
-                    <span class="text-success"> Email : sysadmin@ezana.org </span>
-                    <br>
-                    <span class="text-success">Passsword: 123 </span>
-                </p>
+                <div class="input-group mb-3">
+                    <input type="password" required name="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
 
-                <form method="post">
-                    <div class="input-group mb-3">
-                        <input type="email" required name="email" class="form-control" placeholder="Email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" required name="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-
-                <!--  <div class="social-auth-links text-center mb-3">
+            <!--  <div class="social-auth-links text-center mb-3">
                     <p>- OR -</p>
                     <a href="#" class="btn btn-block btn-primary">
                         <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
@@ -83,15 +83,15 @@ require_once('partials/_head.php')
                     </a>
                 </div> -->
 
-                <p class="mb-1">
-                    <a href="forgot-password.php">I forgot my password</a>
-                </p>
-                <!-- <p class="mb-0">
+            <p class="mb-1">
+                <a href="forgot-password.php">I forgot my password</a>
+            </p>
+            <!-- <p class="mb-0">
                     <a href="register.html" class="text-center">Register a new membership</a>
                 </p> -->
-            </div>
         </div>
     </div>
+    <!--     </div> -->
     <?php require_once('partials/_scripts.php'); ?>
 </body>
 
