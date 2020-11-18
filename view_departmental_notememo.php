cce773a040870a8730a7ac449d9ff0f67c06dfee
MartMbithi <martdevelopers254@gmail.com>
2020-11-18 12:52:43 +0300

    Departmental Scaffold

--- view_departmental_notememo.php
+++ view_departmental_notememo.php
@@ -49,51 +49,54 @@ require_once('partials/_head.php');
                     <hr>
                     <div class="content">
                         <div class="container">
-                            <div class="card card-primary card-outline">
-                                <div class="card-header">
-                                    <h3 class="card-title">
-                                        <?php echo $memo->department_name; ?> <?php echo $memo->type; ?> Created On <span class='text-success'><?php echo $memo->created_at; ?></span>
-                                    </h3>
+                        <div class="col-md-12">
+    <div class="card">
+        <div class="card-header p-2">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link active" href="#notices" data-toggle="tab">Notices</a></li>
+            </ul>
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link " href="#memos" data-toggle="tab">Memos</a></li>
+            </ul>
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link " href="#settings" data-toggle="tab">Department Settings</a></li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="tab-content">
+                <div class="active tab-pane" id="settings">
+                    <form method="post" enctype="multipart/form-data" role="form">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="">Department Name</label>
+                                    <input type="text" required name="name" value="<?php echo $row->name; ?>" class="form-control" id="exampleInputEmail1">
                                 </div>
-                                <div class="card-body">
-                                    <ul class="nav nav-tabs" id="custom-content-below-tab" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill" href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home" aria-selected="true"><?php echo $memo->type;?> Content</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="custom-content-below-profile-tab" data-toggle="pill" href="#custom-content-below-profile" role="tab" aria-controls="custom-content-below-profile" aria-selected="false">Download <?php echo $memo->type;?> Attachments</a>
-                                        </li>
-                                    </ul>
-                                    <div class="tab-content" id="custom-content-below-tabContent">
-                                        <div class="tab-pane fade show active" id="custom-content-below-home" role="tabpanel" aria-labelledby="custom-content-below-home-tab">
-                                            <br>
-                                            <?php echo $memo->departmental_memo; ?>
-                                        </div>
-                                        <div class="tab-pane fade" id="custom-content-below-profile" role="tabpanel" aria-labelledby="custom-content-below-profile-tab">
-                                            <br>
-                                            <?php
-                                            if ($memo->attachments != '') {
-                                                echo
-                                                    "
-                                                        <a href='EzanaLMSData/memos/$memo->attachments' target='_blank' class='btn btn-outline-success'>
-                                                            <i class='fas fa-download'></i>
-                                                                Download $memo->type
-                                                        </a>
-                                                        ";
-                                            } else {
-                                                echo
-                                                    "
-                                                        <a  class='btn btn-outline-danger'>
-                                                            <i class='fas fa-times'></i>
-                                                               $memo->type Attachment Not Available
-                                                        </a>
-                                                        ";
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
+                                <div class="form-group col-md-4">
+                                    <label for="">Department Number / Code</label>
+                                    <input type="text" required name="code" value="<?php echo $row->code; ?>" class="form-control">
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="">Department HOD</label>
+                                    <input type="text" required value="<?php echo $row->hod; ?>" name="hod" class="form-control">
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="exampleInputPassword1">Department Details</label>
+                                    <textarea name="details" id="textarea" rows="10" class="form-control"><?php echo $row->details; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer text-right">
+                            <button type="submit" name="update_dept" class="btn btn-primary">Update Department</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
                         </div>
                     </div>
                 </div>
