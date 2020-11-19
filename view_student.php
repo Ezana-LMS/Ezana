2c35da527a58800d56c5d2ddc123a38d96134b83
MartMbithi <martdevelopers254@gmail.com>
2020-11-19 11:21:31 +0300

    View Student Details On Student Details

--- view_student.php
+++ view_student.php
@@ -17,7 +17,7 @@ require_once('partials/_head.php');
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
-        while ($f = $res->fetch_object()) {
+        while ($row = $res->fetch_object()) {
 
             $view = $_GET['view'];
             $ret = "SELECT * FROM `ezanaLMS_Students` WHERE id ='$view' ";
@@ -31,6 +31,7 @@ require_once('partials/_head.php');
                 } else {
                     $dpic = "<img class='profile-user-img img-fluid img-circle' src='dist/img/students/$std->profile_pic' alt='User profile picture'>";
                 }
+                require_once('partials/_faculty_sidebar.php');
         ?>
                 <div class="content-wrapper">
                     <div class="content-header">
@@ -42,8 +43,8 @@ require_once('partials/_head.php');
                                 <div class="col-sm-6">
                                     <ol class="breadcrumb float-sm-right">
                                         <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $f->id; ?>"><?php echo $f->name; ?></a></li>
-                                        <li class="breadcrumb-item"><a href="students.php?faculty=<?php echo $f->id; ?>">Students</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $row->id; ?>"><?php echo $row->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="students.php?faculty=<?php echo $row->id; ?>">Students</a></li>
                                         <li class="breadcrumb-item active"> Profile </li>
                                     </ol>
                                 </div>
