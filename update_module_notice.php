8bfd15346aa1f87b81a072e5a4f8fc1853c21a16
MartMbithi <martdevelopers254@gmail.com>
2020-11-19 12:51:55 +0300

    Udpate On Module Announcements

--- update_module_notice.php
+++ update_module_notice.php
@@ -52,101 +52,98 @@ require_once('partials/_head.php');
         <?php
         require_once('partials/_faculty_nav.php');
         $faculty = $_GET['faculty'];
+        $view = $_GET['view'];
         $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id = '$faculty' ";
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
         while ($row = $res->fetch_object()) {
-            require_once('partials/_faculty_sidebar.php');
+            $ret = "SELECT * FROM `ezanaLMS_ModulesAnnouncements` WHERE id = '$view' ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            $cnt = 1;
+            while ($not = $res->fetch_object()) {
+                require_once('partials/_faculty_sidebar.php');
         ?>
-            <!-- /.navbar -->
+                <!-- /.navbar -->
 
-            <div class="content-wrapper">
-                <div class="content-header">
-                    <div class="container">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1 class="m-0 text-dark">Add Module Announcement</h1>
-                            </div>
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $row->id; ?>"><?php echo $row->name; ?></a></li>
-                                    <li class="breadcrumb-item"><a href="modules.php?faculty=<?php echo $row->id; ?>">Modules</a></li>
-                                    <li class="breadcrumb-item"><a href="module_notices.php?faculty=<?php echo $row->id; ?>">Announcements</a></li>
-                                    <li class="breadcrumb-item active">Add</li>
-                                </ol>
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark">Update Module Announcement</h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $row->id; ?>"><?php echo $row->name; ?></a></li>
+                                        <li class="breadcrumb-item"><a href="modules.php?faculty=<?php echo $row->id; ?>">Modules</a></li>
+                                        <li class="breadcrumb-item"><a href="module_notices.php?faculty=<?php echo $row->id; ?>">Announcements</a></li>
+                                        <li class="breadcrumb-item active">Update</li>
+                                    </ol>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="content">
-                    <div class="container">
-                        <section class="content">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="card card-primary">
-                                        <div class="card-header">
-                                            <h3 class="card-title">Fill All Required Fields</h3>
-                                        </div>
-                                        <form method="post" enctype="multipart/form-data" role="form">
-                                            <div class="card-body">
-                                                <div class="row">
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Module Name</label>
-                                                        <select class='form-control basic' id="ModuleName" onchange="getModuleDetails(this.value);" name="module_name">
-                                                            <option selected>Select Module Name </option>
+                    <div class="content">
+                        <div class="container">
+                            <section class="content">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="card card-primary">
+                                            <div class="card-header">
+                                                <h3 class="card-title">Fill All Required Fields</h3>
+                                            </div>
+                                            <form method="post" enctype="multipart/form-data" role="form">
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Module Name</label>
+                                                            <input type="text" value="<?php echo $not->module_name; ?>" id="ModuleCode" required name="module_code" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Module Code</label>
+                                                            <input type="text" id="ModuleCode" value="<?php echo $not->module_code; ?>" required name="module_code" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Announcement Posted By</label>
                                                             <?php
-                                                            $ret = "SELECT * FROM `ezanaLMS_Modules` WHERE faculty_id = '$row->id'  ";
+                                                            $id = $_SESSION['id'];
+                                                            $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id = '$id'  ";
                                                             $stmt = $mysqli->prepare($ret);
                                                             $stmt->execute(); //ok
                                                             $res = $stmt->get_result();
-                                                            while ($mod = $res->fetch_object()) {
+                                                            while ($user = $res->fetch_object()) {
                                                             ?>
-                                                                <option><?php echo $mod->name; ?></option>
-                                                            <?php } ?>
-                                                        </select>
+                                                                <input type="text" required name="created_by" value="<?php echo $user->name; ?>" class="form-control" id="exampleInputEmail1">
+                                                            <?php
+                                                            } ?>
+                                                        </div>
                                                     </div>
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Module Code</label>
-                                                        <input type="text" id="ModuleCode"  required name="module_code" class="form-control">
-                                                    </div>
-                                                    <div class="form-group col-md-4">
-                                                        <label for="">Announcement  Posted By</label>
-                                                        <?php
-                                                        $id = $_SESSION['id'];
-                                                        $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id = '$id'  ";
-                                                        $stmt = $mysqli->prepare($ret);
-                                                        $stmt->execute(); //ok
-                                                        $res = $stmt->get_result();
-                                                        while ($user = $res->fetch_object()) {
-                                                        ?>
-                                                            <input type="text" required name="created_by" value="<?php echo $user->name; ?>" class="form-control" id="exampleInputEmail1">
-                                                        <?php
-                                                        } ?>
+                                                    <div class="row">
+                                                        <div class="form-group col-md-12">
+                                                            <label for="exampleInputPassword1">Module Announcements</label>
+                                                            <textarea required id="textarea" name="announcements" rows="20" class="form-control"><?php echo $not->announcements; ?></textarea>
+                                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="form-group col-md-12">
-                                                        <label for="exampleInputPassword1">Module Announcements</label>
-                                                        <textarea required id="textarea" name="announcements" rows="20" class="form-control"></textarea>
-                                                        <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
-                                                    </div>
+                                                <div class="card-footer text-right">
+                                                    <button type="submit" name="add_notice" class="btn btn-primary">Add Notice</button>
                                                 </div>
-                                            </div>
-                                            <div class="card-footer text-right">
-                                                <button type="submit" name="add_notice" class="btn btn-primary">Add Notice</button>
-                                            </div>
-                                        </form>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </section>
+                            </section>
+                        </div>
                     </div>
                 </div>
-            </div>
         <?php require_once('partials/_footer.php');
+            }
         } ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
