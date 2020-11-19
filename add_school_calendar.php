16d7b36cfebfa8e244d9e0be406bf5ac985c07c1
MartMbithi <martdevelopers254@gmail.com>
2020-11-19 10:23:32 +0300

    Faculty Id Refactor On Add School Caledars

--- add_school_calendar.php
+++ add_school_calendar.php
@@ -80,7 +80,9 @@ require_once('partials/_head.php');
         $stmt->execute(); //ok
         $res = $stmt->get_result();
         while ($row = $res->fetch_object()) {
-            require_once('partials/_faculty_nav.php'); ?>
+            require_once('partials/_faculty_nav.php'); 
+            require_once('partials/_faculty_sidebar.php')
+            ?>
             <!-- /.navbar -->
             <div class="content-wrapper">
                 <div class="content-header">
