beac2a35750262f29dc5da2c0a8eb91d26a70f75
MartMbithi <martdevelopers254@gmail.com>
2020-11-19 10:13:54 +0300

    Faculty Id Refactor On Add Departmental Note Memo

--- add_departmental_notememos.php
+++ add_departmental_notememos.php
@@ -48,7 +48,8 @@ require_once('partials/_head.php');
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
-        while ($f = $res->fetch_object()) {
+        while ($row = $res->fetch_object()) {
+            require_once('partials/_faculty_sidebar.php');
         ?>
             <!-- /.navbar -->
 
@@ -63,7 +64,7 @@ require_once('partials/_head.php');
                                 <ol class="breadcrumb float-sm-right">
                                     <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                     <li class="breadcrumb-item"><a href="dashboard.php">Faculties</a></li>
-                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $faculty; ?>"><?php echo $f->name; ?></a></li>
+                                    <li class="breadcrumb-item"><a href="faculty_dashboard.php?faculty=<?php echo $faculty; ?>"><?php echo $row->name; ?></a></li>
                                     <li class="breadcrumb-item"><a href="departmental_notememos.php?faculty=<?php echo $faculty; ?>">Memos & Notices</a></li>
                                     <li class="breadcrumb-item active">Add</li>
                                 </ol>
