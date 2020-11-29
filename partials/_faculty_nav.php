4015db9b75b2780ee782fde52f5f174350353754
MartMbithi <martdevelopers254@gmail.com>
2020-11-29 22:29:39 +0300

    Intergrated new logo

--- partials/_faculty_nav.php
+++ partials/_faculty_nav.php
@@ -16,7 +16,7 @@ while ($admin = $res->fetch_object()) {
         <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
             <div class="container">
                 <a href="dashboard.php" class="navbar-brand">
-                    <img src="dist/img/logo.jpeg" alt="Ezana LMS Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <img src="dist/img/Main_Logo.png" alt="Ezana LMS Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                     <span class="brand-text font-weight-light">Ezana LMS</span>
                 </a>
 
