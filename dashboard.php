e0b6c761dbcb43dfad04ed4ca21217674a28f2cb
MartMbithi <martdevelopers254@gmail.com>
2020-12-06 15:50:17 +0300

    Segmented Main Dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,91 +5,13 @@
 
 <div id="wrapper">
 
-	<header id="header">
-
-		<h1 id="site-logo">
-			<a href="index-2.html">
-				<img src="img/logos/logo.png" alt="Site Logo" />
-			</a>
-		</h1>	
-
-		<a href="javascript:;" data-toggle="collapse" data-target=".top-bar-collapse" id="top-bar-toggle" class="navbar-toggle collapsed">
-			<i class="fa fa-cog"></i>
-		</a>
-
-		<a href="javascript:;" data-toggle="collapse" data-target=".sidebar-collapse" id="sidebar-toggle" class="navbar-toggle collapsed">
-			<i class="fa fa-reorder"></i>
-		</a>
-
-	</header> <!-- header -->
+    <?php 
+        require_once('dashboard/partials/_topbar.php');
+    ?>
+	<!-- /#top-bar -->
 
 
-	<nav id="top-bar" class="collapse top-bar-collapse">
-
-		<ul class="nav navbar-nav pull-left">
-			<li class="">
-				<a href="index-2.html">
-					<i class="fa fa-home"></i> 
-					Home
-				</a>
-			</li>
-
-			<li class="dropdown">
-		    	<a class="dropdown-toggle" data-toggle="dropdown" href="javascript:;">
-		        	Dropdown <span class="caret"></span>
-		    	</a>
-
-		    	<ul class="dropdown-menu" role="menu">
-			        <li><a href="javascript:;"><i class="fa fa-user"></i>&nbsp;&nbsp;Example #1</a></li>
-			        <li><a href="javascript:;"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Example #2</a></li>
-			        <li class="divider"></li>
-			        <li><a href="javascript:;"><i class="fa fa-tasks"></i>&nbsp;&nbsp;Example #3</a></li>
-		    	</ul>
-		    </li>
-		    
-		</ul>
-
-		<ul class="nav navbar-nav pull-right">
-			<li class="dropdown">
-				<a class="dropdown-toggle" data-toggle="dropdown" href="javascript:;">
-					<i class="fa fa-user"></i>
-		        	Rod Howard 
-		        	<span class="caret"></span>
-		    	</a>
-
-		    	<ul class="dropdown-menu" role="menu">
-			        <li>
-			        	<a href="page-profile.html">
-			        		<i class="fa fa-user"></i> 
-			        		&nbsp;&nbsp;My Profile
-			        	</a>
-			        </li>
-			        <li>
-			        	<a href="page-calendar.html">
-			        		<i class="fa fa-calendar"></i> 
-			        		&nbsp;&nbsp;My Calendar
-			        	</a>
-			        </li>
-			        <li>
-			        	<a href="page-settings.html">
-			        		<i class="fa fa-cogs"></i> 
-			        		&nbsp;&nbsp;Settings
-			        	</a>
-			        </li>
-			        <li class="divider"></li>
-			        <li>
-			        	<a href="page-login.html">
-			        		<i class="fa fa-sign-out"></i> 
-			        		&nbsp;&nbsp;Logout
-			        	</a>
-			        </li>
-		    	</ul>
-		    </li>
-		</ul>
-
-	</nav> <!-- /#top-bar -->
-
-
+    <!-- Static Sidebar -->
 	<div id="sidebar-wrapper" class="collapse sidebar-collapse">
 	
 		<div id="search">
@@ -98,7 +20,7 @@
 
 				<button type="submit" id="search-btn" class="btn"><i class="fa fa-search"></i></button>
 			</form>		
-		</div> <!-- #search -->
+		</div> 
 	
 		<nav id="sidebar">		
 			
@@ -351,13 +273,14 @@
 					
 		</nav> <!-- #sidebar -->
 
-	</div> <!-- /#sidebar-wrapper -->
+    </div>
+    <!-- Static Sidebar -->
 
 	
 	<div id="content">		
 		
 		<div id="content-header">
-			<h1>Dashboard</h1>
+			<h1>Main Dashboard</h1>
 		</div> <!-- #content-header -->	
 
 
