a8934c3d8f55d26627870033d99383607067cb9d
MartMbithi <martdevelopers254@gmail.com>
2020-12-06 15:44:22 +0300

    Segmented Dashboard Header Files

--- dashboard.php
+++ dashboard.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-
-<!-- Mirrored from jumpstartthemespreview.com/canvas-admin-1.1/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 04 Dec 2020 10:01:14 GMT -->
-<head>
-    <title>Dashboard - Canvas Admin</title>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="">
-	<meta name="author" content="" />
-	
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,800italic,400,600,800" type="text/css">
-
-	<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />		
-	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />	
-	<link rel="stylesheet" href="js/libs/css/ui-lightness/jquery-ui-1.9.2.custom.css" type="text/css" />
-		
-	<link rel="stylesheet" href="js/plugins/icheck/skins/minimal/blue.css" type="text/css" />
-	<link rel="stylesheet" href="js/plugins/select2/select2.css" type="text/css" />
-	<link rel="stylesheet" href="js/plugins/fullcalendar/fullcalendar.css" type="text/css" />
-	
-	<link rel="stylesheet" href="css/App.css" type="text/css" />
-
-	<link rel="stylesheet" href="css/custom.css" type="text/css" />	
-</head>
-
+<?php
+    require_once('dashboard/partials/_head.php');
+?>
 <body>
 
 <div id="wrapper">
