0bc9f2b2e8f0361cb4c17da9f9f22f23cccd1e2b
MartMbithi <martdevelopers254@gmail.com>
2020-12-07 11:55:02 +0300

    Auth Backend Logic On Index

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+session_start();
+include('configs/config.php');
 
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = sha1(md5($_POST['password'])); //double encrypt to increase security
+    $stmt = $mysqli->prepare("SELECT email, password, id, name  FROM ezanaLMS_Admins  WHERE email =? AND password =?");
+    $stmt->bind_param('ss', $email, $password); //bind fetched parameters
+    $stmt->execute(); //execute bind 
+    $stmt->bind_result($email, $password, $id, $name); //bind result
+    $rs = $stmt->fetch();
+    $_SESSION['id'] = $id;
+    $_SESSION['email'] = $email;
+    $_SESSION['name'] = $name;
+    if ($rs) {
+        header("location:dashboard.php");
+    } else {
+        $err = "Access Denied Please Check Your Credentials";
+    }
+}
 include __DIR__ . "/public/partials/_authhead.php"
 ?>
 
@@ -22,14 +41,14 @@ include __DIR__ . "/public/partials/_authhead.php"
                         </span>
                     </div>
                     <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <input class="input100" type="password" name="password" placeholder="Password">
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="fa fa-lock" aria-hidden="true"></i>
                         </span>
                     </div>
                     <div class="container-login100-form-btn">
-                        <input type="submit" value="Login" class="login100-form-btn">
+                        <input type="submit" name="login" value="Login" class="login100-form-btn">
                     </div>
                     <div class="text-center p-t-12">
                         <span class="txt1">
