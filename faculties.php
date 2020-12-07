aa684fb65733b01d2a96f623106e5c597c032006
MartMbithi <martdevelopers254@gmail.com>
2020-12-07 16:24:35 +0300

    Refactored Commented Code

--- faculties.php
+++ faculties.php
@@ -228,41 +228,32 @@ require_once('public/partials/_head.php');
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Departments
-                                                    <span class="badge badge-primary badge-pill">14</span>
                                                 </a>
                                             </li>
 
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Courses
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
                                                 </a>
                                             </li>
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Modules
-
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
                                                 </a>
                                             </li>
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Calendar
-
                                                 </a>
                                             </li>
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Lecturers
-
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
                                                 </a>
                                             </li>
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <a href="">
                                                     Students
-
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
                                                 </a>
                                             </li>
                                         </ul>
@@ -272,21 +263,6 @@ require_once('public/partials/_head.php');
                         <?php
                             $cnt = $cnt + 1;
                         } ?>
-                        <!-- 
-                            <div class="col-md-9">
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="jumbotron">
-                                            <h1 class="display-4">Hello, world!</h1>
-                                            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                                            <hr class="my-4">
-                                            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> -->
-
                     </div>
                 </section>
                 <!-- Main Footer -->
