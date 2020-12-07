0e7b507059cc8df9353bfdf253bbaf6de13c210b
MartMbithi <martdevelopers254@gmail.com>
2020-12-07 13:54:06 +0300

    Integrated Search On Faculties

--- faculty_seacrh_result.php
+++ faculty_seacrh_result.php
@@ -156,7 +156,7 @@ require_once('public/partials/_head.php');
                             <nav class="navbar navbar-light bg-light col-md-12">
                                 <form class="form-inline" action="faculty_search_result.php" method="GET">
                                     <input class="form-control mr-sm-2" type="search" placeholder="Type Faculty Name">
-                                    <button class="btn btn-outline-success my-2 my-sm-0" name="query" type="submit">Search</button>
+                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                                 </form>
                                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add New Faculty</button>
                                 <div class="modal fade" id="modal-default">
@@ -204,90 +204,83 @@ require_once('public/partials/_head.php');
                         </div>
                         <br>
                         <?php
-                        $ret = "SELECT * FROM `ezanaLMS_Faculties` ORDER BY `name` ASC ";
-                        $stmt = $mysqli->prepare($ret);
-                        $stmt->execute(); //ok
-                        $res = $stmt->get_result();
-                        $cnt = 1;
-                        while ($faculty = $res->fetch_object()) {
+                        $query = $_GET['query'];
+                        $min_length = 5;
+                        if (strlen($query) >= $min_length) {
+                            $query = htmlspecialchars($query);
+                            $query = mysqli_real_escape_string($mysqli, $query);
+                            $raw_results = mysqli_query($mysqli, "SELECT * FROM ezanaLMS_Faculties WHERE (`name` LIKE '%" . $query . "%') OR (`code` LIKE '%" . $query . "%') ");
+                            if (mysqli_num_rows($raw_results) > 0) {
+                                while ($results = mysqli_fetch_array($raw_results)) {
                         ?>
-                            <div class="col-md-12">
-                                <div class="card card-primary collapsed-card">
-                                    <div class="card-header">
-                                        <a href="faculty_dashboard.php?view=<?php echo $faculty->id; ?>">
-                                            <h3 class="card-title">#<?php echo $cnt; ?> <?php echo $faculty->name; ?></h3>
-                                            <div class="card-tools text-right">
-                                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                                                </button>
+                                    <div class="col-md-12">
+                                        <div class="card card-primary collapsed-card">
+                                            <div class="card-header">
+                                                <a href="faculty_dashboard.php?view=<?php echo $faculty->id; ?>">
+                                                    <h3 class="card-title">#<?php echo $cnt; ?> <?php echo $faculty->name; ?></h3>
+                                                    <div class="card-tools text-right">
+                                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                                                        </button>
+                                                    </div>
+                                                </a>
                                             </div>
-                                        </a>
-                                    </div>
 
-                                    <div class="card-body">
-                                        <ul class="list-group">
+                                            <div class="card-body">
+                                                <ul class="list-group">
 
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Departments
-                                                    <span class="badge badge-primary badge-pill">14</span>
-                                                </a>
-                                            </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Departments
+                                                            <span class="badge badge-primary badge-pill">14</span>
+                                                        </a>
+                                                    </li>
 
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Courses
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
-                                                </a>
-                                            </li>
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Modules
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Courses
+                                                            <span class="badge badge-primary badge-pill"> 14</span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Modules
 
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
-                                                </a>
-                                            </li>
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Calendar
+                                                            <span class="badge badge-primary badge-pill"> 14</span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Calendar
 
-                                                </a>
-                                            </li>
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Lecturers
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Lecturers
 
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
-                                                </a>
-                                            </li>
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <a href="">
-                                                    Students
+                                                            <span class="badge badge-primary badge-pill"> 14</span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="">
+                                                            Students
 
-                                                    <span class="badge badge-primary badge-pill"> 14</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php
-                            $cnt = $cnt + 1;
-                        } ?>
-                        <!-- 
-                            <div class="col-md-9">
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="jumbotron">
-                                            <h1 class="display-4">Hello, world!</h1>
-                                            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                                            <hr class="my-4">
-                                            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                                            
+                                                            <span class="badge badge-primary badge-pill"> 14</span>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div> -->
-
+                        <?php
+                                }
+                            } else {
+                                echo "No results";
+                            }
+                        } else {
+                            echo "Minimum length is " . $min_length;
+                        }
+                        ?>
                     </div>
                 </section>
                 <!-- Main Footer -->
