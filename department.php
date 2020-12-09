616ca1d270d56d065f6768b5b35bdf377a4402ad
MartMbithi <martdevelopers254@gmail.com>
2020-12-09 18:48:18 +0300

    Add Department Course Details

--- department.php
+++ department.php
@@ -197,35 +197,19 @@ require_once('public/partials/_head.php');
                                                     <form method="post" enctype="multipart/form-data" role="form">
                                                         <div class="card-body">
                                                             <div class="row">
-                                                                <div class="form-group col-md-6">
+                                                                <div class="form-group col-md-4">
                                                                     <label for="">Course Name</label>
                                                                     <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
                                                                     <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                                    <input type="hidden" required name="department_id" value="<?php echo $department->id; ?>" class="form-control">
                                                                 </div>
-                                                                <div class="form-group col-md-6">
+                                                                <div class="form-group col-md-4">
                                                                     <label for="">Course Number / Code</label>
                                                                     <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
                                                                 </div>
-                                                            </div>
-                                                            <div class="row">
-                                                                <div class="form-group col-md-6">
+                                                                <div class="form-group col-md-4">
                                                                     <label for="">Department Name</label>
-                                                                    <select class='form-control basic' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
-                                                                        <option selected>Select Department Name</option>
-                                                                        <?php
-                                                                        $ret = "SELECT * FROM `ezanaLMS_Departments`  ";
-                                                                        $stmt = $mysqli->prepare($ret);
-                                                                        $stmt->execute(); //ok
-                                                                        $res = $stmt->get_result();
-                                                                        while ($dep = $res->fetch_object()) {
-                                                                        ?>
-                                                                            <option><?php echo $dep->name; ?></option>
-                                                                        <?php } ?>
-                                                                    </select>
-                                                                </div>
-                                                                <div class="form-group col-md-6">
-                                                                    <label for="">Department ID</label>
-                                                                    <input type="text" id="DepartmentID" readonly required name="department_id" class="form-control">
+                                                                    <input type="text" required name="department_name" value="<?php echo $department->name; ?>" class="form-control">
                                                                 </div>
                                                             </div>
                                                             <div class="row">
@@ -288,81 +272,6 @@ require_once('public/partials/_head.php');
                                             </div>
                                         </div>
                                     </div>
-
-                                    <!-- All Other Faculties 
-                                    //This Has A Bug It Only Fetches The Active Faculty And Only One Inactive Faculty"
-                                    <?php
-                                    $ret = "SELECT * FROM `ezanaLMS_Faculties` WHERE id != '$faculty->id' ORDER BY `name` ASC ";
-                                    $stmt = $mysqli->prepare($ret);
-                                    $stmt->execute(); //ok
-                                    $res = $stmt->get_result();
-                                    while ($unactive_faculties = $res->fetch_object()) {
-                                    ?>
-                                        <div class="col-md-12">
-                                            <div class="card card-primary collapsed-card">
-                                                <div class="card-header">
-                                                    <a href="faculty_dashboard.php?view=<?php echo $unactive_faculties->id; ?>">
-                                                        <h3 class="card-title"><?php echo $unactive_faculties->name; ?></h3>
-                                                        <div class="card-tools text-right">
-                                                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                                                            </button>
-                                                        </div>
-                                                    </a>
-                                                </div>
-
-                                                <div class="card-body">
-                                                    <ul class="list-group">
-                                                    <?php
-                                                    /* List All Departments Under This Faculty */
-                                                    $InactivedepartmentFacultyID = $unactive_faculties->id;
-                                                    $ret = "SELECT * FROM `ezanaLMS_Departments` WHERE faculty_id = '$InactivedepartmentFacultyID' ORDER BY `name` ASC  ";
-                                                    $stmt = $mysqli->prepare($ret);
-                                                    $stmt->execute(); //ok
-                                                    $res = $stmt->get_result();
-                                                    $cnt = 1;
-                                                    while ($inactivefacultyDepartment = $res->fetch_object()) {
-                                                    ?>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="departments.php?view=<?php echo $inactivefacultyDepartment->id; ?>">
-                                                                <?php echo $inactivefacultyDepartment->name; ?>
-                                                            </a>
-                                                        </li>
-
-                                                    <?php } ?>
-
-                                                        
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="courses.php?view=<?php echo $unactive_faculties->id; ?>">
-                                                                Courses
-                                                            </a>
-                                                        </li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="modules.php?view=<?php echo $unactive_faculties->id; ?>">
-                                                                Modules
-                                                            </a>
-                                                        </li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="school_calendar.phpview=<?php echo $unactive_faculties->id; ?>">
-                                                                Calendar
-                                                            </a>
-                                                        </li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="lects.php?view=<?php echo $unactive_faculties->id; ?>">
-                                                                Lecturers
-                                                            </a>
-                                                        </li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <a href="students.php?view=<?php echo $unactive_faculties->id; ?>">
-                                                                Students
-                                                            </a>
-                                                        </li> 
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php
-                                    } ?>
-                                    -->
                                 </div>
 
                                 <div class="col-md-9">
