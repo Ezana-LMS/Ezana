5ae3ef5cc9268f041e3467bba3868923ec58372b
MartMbithi <martdevelopers254@gmail.com>
2020-12-09 18:44:57 +0300

    Added New Course Front End Logic

--- department.php
+++ department.php
@@ -50,7 +50,7 @@ if (isset($_POST['add_course'])) {
             $rc = $stmt->bind_param('ssssss', $id, $code, $name, $details, $department_id, $department_name);
             $stmt->execute();
             if ($stmt) {
-                $success = "Course Added" && header("refresh:1; url=add_course.php");
+                $success = "$name Course Added";
             } else {
                 $info = "Please Try Again Or Try Later";
             }
@@ -194,7 +194,51 @@ require_once('public/partials/_head.php');
                                                     </button>
                                                 </div>
                                                 <div class="modal-body">
-
+                                                    <form method="post" enctype="multipart/form-data" role="form">
+                                                        <div class="card-body">
+                                                            <div class="row">
+                                                                <div class="form-group col-md-6">
+                                                                    <label for="">Course Name</label>
+                                                                    <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
+                                                                    <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                                </div>
+                                                                <div class="form-group col-md-6">
+                                                                    <label for="">Course Number / Code</label>
+                                                                    <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="form-group col-md-6">
+                                                                    <label for="">Department Name</label>
+                                                                    <select class='form-control basic' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
+                                                                        <option selected>Select Department Name</option>
+                                                                        <?php
+                                                                        $ret = "SELECT * FROM `ezanaLMS_Departments`  ";
+                                                                        $stmt = $mysqli->prepare($ret);
+                                                                        $stmt->execute(); //ok
+                                                                        $res = $stmt->get_result();
+                                                                        while ($dep = $res->fetch_object()) {
+                                                                        ?>
+                                                                            <option><?php echo $dep->name; ?></option>
+                                                                        <?php } ?>
+                                                                    </select>
+                                                                </div>
+                                                                <div class="form-group col-md-6">
+                                                                    <label for="">Department ID</label>
+                                                                    <input type="text" id="DepartmentID" readonly required name="department_id" class="form-control">
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="form-group col-md-12">
+                                                                    <label for="exampleInputPassword1">Course Description</label>
+                                                                    <textarea required name="details" id="textarea" rows="10" class="form-control"></textarea>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card-footer">
+                                                            <button type="submit" name="add_course" class="btn btn-primary">Add Course</button>
+                                                        </div>
+                                                    </form>
                                                 </div>
                                                 <div class="modal-footer justify-content-between">
                                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
