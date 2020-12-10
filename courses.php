fe979831c65e84ad7b893c74736341a9985f8ae5
MartMbithi <martdevelopers254@gmail.com>
2020-12-10 12:11:20 +0300

    Add Course Scaffolding

--- courses.php
+++ courses.php
@@ -7,6 +7,58 @@ require_once('configs/codeGen.php');
 
 /* Add Course */
 
+if (isset($_POST['add_course'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['code']) && !empty($_POST['code'])) {
+        $code = mysqli_real_escape_string($mysqli, trim($_POST['code']));
+    } else {
+        $error = 1;
+        $err = "Couse  Code Cannot Be Empty";
+    }
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
+    } else {
+        $error = 1;
+        $err = "Course Name Cannot Be Empty";
+    }
+    if (isset($_POST['department_id']) && !empty($_POST['department_id'])) {
+        $department_id = mysqli_real_escape_string($mysqli, trim($_POST['department_id']));
+    } else {
+        $error = 1;
+        $err = "Department Name / ID  Cannot Be Empty";
+    }
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Courses WHERE  code='$code' || name ='$name' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($code == $row['code']) {
+                $err =  "Course With This Code Already Exists";
+            } else {
+                $err = "Course Name Already Exists";
+            }
+        } else {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $code = $_POST['code'];
+            $details = $_POST['details'];
+            $department_id = $_POST['department_id'];
+            $department_name = $_POST['department_name'];
+            $faculty_id = $_POST['faculty_id'];
+            $query = "INSERT INTO ezanaLMS_Courses (id, code, name, details, department_id, faculty_id, department_name) VALUES(?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssssss', $id, $code, $name, $details, $department_id, $faculty_id,  $department_name);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Course Added" && header("refresh:1; url=courses.php");
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
 /*  Update Course*/
 
 
@@ -141,7 +193,7 @@ require_once('public/partials/_head.php');
                                     <input class="form-control mr-sm-2" type="search" name="query" placeholder="Course Name Or Code">
                                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                                 </form>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add New Department</button>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add New Course</button>
                                 <div class="modal fade" id="modal-default">
                                     <div class="modal-dialog  modal-lg">
                                         <div class="modal-content">
@@ -153,7 +205,51 @@ require_once('public/partials/_head.php');
                                             </div>
                                             <div class="modal-body">
                                                 <!-- Add Course Form -->
-
+                                                <form method="post" enctype="multipart/form-data" role="form">
+                                                    <div class="card-body">
+                                                        <div class="row">
+                                                            <div class="form-group col-md-6">
+                                                                <label for="">Course Name</label>
+                                                                <input type="text" required name="name" class="form-control" id="exampleInputEmail1">
+                                                                <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                            </div>
+                                                            <div class="form-group col-md-6">
+                                                                <label for="">Course Number / Code</label>
+                                                                <input type="text" required name="code" value="<?php echo $a; ?><?php echo $b; ?>" class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="form-group col-md-12">
+                                                                <label for="">Department Name</label>
+                                                                <select class='form-control basic' id="DepartmentName" onchange="getDepartmentDetails(this.value);" name="department_name">
+                                                                    <option selected>Select Department Name</option>
+                                                                    <?php
+                                                                    $ret = "SELECT * FROM `ezanaLMS_Departments`  WHERE faculty_id = '$faculty' ";
+                                                                    $stmt = $mysqli->prepare($ret);
+                                                                    $stmt->execute(); //ok
+                                                                    $res = $stmt->get_result();
+                                                                    while ($dep = $res->fetch_object()) {
+                                                                    ?>
+                                                                        <option><?php echo $dep->name; ?></option>
+                                                                    <?php } ?>
+                                                                </select>
+                                                            </div>
+                                                            <div class="form-group col-md-6" style="display:none">
+                                                                <label for="">Department ID</label>
+                                                                <input type="text" id="DepartmentID" readonly required name="department_id" class="form-control">
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="form-group col-md-12">
+                                                                <label for="exampleInputPassword1">Course Description</label>
+                                                                <textarea required name="details" id="textarea" rows="10" class="form-control"></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card-footer text-right">
+                                                        <button type="submit" name="add_course" class="btn btn-primary">Add Course</button>
+                                                    </div>
+                                                </form>
                                             </div>
                                             <div class="modal-footer justify-content-between">
                                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -173,7 +269,7 @@ require_once('public/partials/_head.php');
                                 $stmt->execute(); //ok
                                 $res = $stmt->get_result();
                                 $cnt = 1;
-                                while ($courses = $res->fetch_object()) {
+                                while ($course = $res->fetch_object()) {
                                 ?>
                                     <div class="col-md-12">
                                         <div class="card card-primary collapsed-card">
