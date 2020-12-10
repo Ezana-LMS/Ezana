71801b45b04cf74f601bf424a71c4dad3abfdcc7
MartMbithi <martdevelopers254@gmail.com>
2020-12-10 12:33:27 +0300

    Minor Updates On Ajax

--- ajax.php
+++ ajax.php
@@ -1,10 +1,10 @@
 <?php
 include('configs/pdoconfig.php');
 
-/* Departmnt ID */
-if (!empty($_POST["DepartmentCode"])) {
-    $id = $_POST['DepartmentCode'];
-    $stmt = $DB_con->prepare("SELECT * FROM ezanaLMS_Departments WHERE code = :id");
+/* Department ID */
+if (!empty($_POST["DepartmentName"])) {
+    $id = $_POST['DepartmentName'];
+    $stmt = $DB_con->prepare("SELECT * FROM ezanaLMS_Departments WHERE name = :id");
     $stmt->execute(array(':id' => $id));
 ?>
 <?php
@@ -15,10 +15,10 @@ if (!empty($_POST["DepartmentCode"])) {
     }
 }
 
-/* Department  Faculty ID  */
+/* Department Faculty ID */
 if (!empty($_POST["DepartmentID"])) {
     $id = $_POST['DepartmentID'];
-    $stmt = $DB_con->prepare("SELECT * FROM ezanaLMS_Departments WHERE id = :id");
+    $stmt = $DB_con->prepare("SELECT * FROM ezanaLMS_Departments WHERE name = :id");
     $stmt->execute(array(':id' => $id));
 ?>
 <?php
@@ -28,20 +28,6 @@ if (!empty($_POST["DepartmentID"])) {
 <?php
     }
 }
-/* Department Name */
-if (!empty($_POST["DepartmentFacultyId"])) {
-    $id = $_POST['DepartmentFacultyId'];
-    $stmt = $DB_con->prepare("SELECT * FROM ezanaLMS_Departments WHERE faculty_id = :id");
-    $stmt->execute(array(':id' => $id));
-?>
-<?php
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-?>
-<?php echo htmlentities($row['name']); ?>
-<?php
-    }
-}
-
 
 /* Course Details */
 if (!empty($_POST["Cname"])) {
