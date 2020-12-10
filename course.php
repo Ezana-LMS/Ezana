35430c8fa39bbb7bbf8acdf41d29cfae0b1ec14b
MartMbithi <martdevelopers254@gmail.com>
2020-12-10 16:12:17 +0300

    Courses Dashboard Scaffolding

---
+++ course.php
@@ -0,0 +1,315 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('configs/codeGen.php');
+
+/* Add Course */
+if (isset($_POST['add_course'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['code']) && !empty($_POST['code'])) {
+        $code = mysqli_real_escape_string($mysqli, trim($_POST['code']));
+    } else {
+        $error = 1;
+        $err = "Couse  Code Cannot Be Empty";
+    }
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
+    } else {
+        $error = 1;
+        $err = "Course Name Cannot Be Empty";
+    }
+    if (isset($_POST['department_id']) && !empty($_POST['department_id'])) {
+        $department_id = mysqli_real_escape_string($mysqli, trim($_POST['department_id']));
+    } else {
+        $error = 1;
+        $err = "Department Name / ID  Cannot Be Empty";
+    }
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Courses WHERE  code='$code' || name ='$name' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($code == $row['code']) {
+                $err =  "Course With This Code Already Exists";
+            } else {
+                $err = "Course Name Already Exists";
+            }
+        } else {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $code = $_POST['code'];
+            $details = $_POST['details'];
+            $department_id = $_POST['department_id'];
+            $department_name = $_POST['department_name'];
+            $faculty_id = $_POST['faculty_id'];
+            $query = "INSERT INTO ezanaLMS_Courses (id, code, name, details, department_id, faculty_id, department_name) VALUES(?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssssss', $id, $code, $name, $details, $department_id, $faculty_id,  $department_name);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Course Added" && header("refresh:1; url=courses.php");
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+
+/*  Update Course*/
+if (isset($_POST['update_course'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['code']) && !empty($_POST['code'])) {
+        $code = mysqli_real_escape_string($mysqli, trim($_POST['code']));
+    } else {
+        $error = 1;
+        $err = "Couse  Code Cannot Be Empty";
+    }
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
+    } else {
+        $error = 1;
+        $err = "Course Name Cannot Be Empty";
+    }
+    if (!$error) {
+
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $code = $_POST['code'];
+        $details = $_POST['details'];
+
+        $query = "UPDATE ezanaLMS_Courses SET  code =?, name =?, details =? WHERE id =?";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssss', $code, $name, $details, $id);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Course Updated" && header("refresh:1; url=courses.php");
+        } else {
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
+
+/* Delete Course */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $faculty = $_GET['faculty'];
+    $adn = "DELETE FROM ezanaLMS_Courses WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=courses.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+require_once('public/partials/_analytics.php');
+require_once('public/partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php
+        require_once('public/partials/_nav.php');
+        $view = $_GET['view'];
+        $ret = "SELECT * FROM `ezanaLMS_Courses` WHERE id = '$view' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        $cnt = 1;
+        while ($courses = $res->fetch_object()) {
+        ?>
+            <!-- /.navbar -->
+
+            <!-- Main Sidebar Container -->
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <!-- Brand Logo -->
+                <a href="dashboard.php" class="brand-link">
+                    <img src="public/dist/img/logo.png" alt="Ezana LMS Logo" class="brand-image img-circle elevation-3">
+                    <span class="brand-text font-weight-light">Ezana LMS</span>
+                </a>
+                <!-- Sidebar -->
+                <div class="sidebar">
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <li class="nav-item">
+                                <a href="dashboard.php" class=" nav-link">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p>
+                                        Dashboard
+                                    </p>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="faculties.php" class=" nav-link">
+                                    <i class="nav-icon fas fa-university"></i>
+                                    <p>
+                                        Faculties
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="departments.php" class=" nav-link">
+                                    <i class="nav-icon fas fa-building"></i>
+                                    <p>
+                                        Departments
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="courses.php" class="active nav-link">
+                                    <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                                    <p>
+                                        Courses
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="modules.php" class="nav-link">
+                                    <i class="nav-icon fas fa-chalkboard"></i>
+                                    <p>
+                                        Modules
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="lecturers.php" class="nav-link">
+                                    <i class="nav-icon fas fa-user-tie"></i>
+                                    <p>
+                                        Lecturers
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="students.php" class="nav-link">
+                                    <i class="nav-icon fas fa-user-graduate"></i>
+                                    <p>
+                                        Students
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="settings.php" class="nav-link">
+                                    <i class="nav-icon fas fa-cogs"></i>
+                                    <p>
+                                        System Settings
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </aside>
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark"><?php echo $courses->name; ?></h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="courses.php">Courses</a></li>
+                                    <li class="breadcrumb-item active"><?php echo $courses->name; ?></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+
+                    <section class="content">
+                        <div class="container-fluid">
+                            <div class="text-left">
+                                <nav class="navbar navbar-light bg-light col-md-12">
+                                    <form class="form-inline" action="course_search_result.php" method="GET">
+                                        <input class="form-control mr-sm-2" type="search" name="query" placeholder="Course Name Or Code">
+                                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                    </form>
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add New Module</button>
+                                    <div class="modal fade" id="modal-default">
+                                        <div class="modal-dialog  modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Fill All Required Values </h4>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <!-- Add Module Form -->
+
+                                                </div>
+                                                <div class="modal-footer justify-content-between">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="col-md-12">
+                                        <div class="card card-primary collapsed-card">
+                                            <div class="card-header">
+                                                <h3 class="card-title"><?php echo $course->name; ?></h3>
+                                                <div class="card-tools text-right">
+                                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div class="card-body">
+                                                <ul class="list-group">
+
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="modules.php?view=<?php echo $course->id; ?>">
+                                                            Modules
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="timetables.php?view=<?php echo $course->id; ?>">
+                                                            Time Table
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                        <a href="enrollments.php?view=<?php echo $course->id; ?>">
+                                                            Enrolled Students
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="row">
+                                        <div class="col-12">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <!-- Main Footer -->
+                <?php require_once('public/partials/_footer.php');
+            } ?>
+                </div>
+            </div>
+            <!-- ./wrapper -->
+            <?php require_once('public/partials/_scripts.php'); ?>
+</body>
+
+</html>
