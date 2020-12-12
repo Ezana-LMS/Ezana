b87dcfdb9164a8a8f776b0dc38917432305e5474
MartMbithi <martdevelopers254@gmail.com>
2020-12-12 13:22:39 +0300

    Faculty Student Add Back End

--- faculty_students.php
+++ faculty_students.php
@@ -7,6 +7,79 @@ check_login();
 
 /* Add Std */
 
+if (isset($_POST['add_student'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['admno']) && !empty($_POST['admno'])) {
+        $admno = mysqli_real_escape_string($mysqli, trim($_POST['admno']));
+    } else {
+        $error = 1;
+        $err = "Admission  Number Cannot Be Empty";
+    }
+    if (isset($_POST['idno']) && !empty($_POST['idno'])) {
+        $idno = mysqli_real_escape_string($mysqli, trim($_POST['idno']));
+    } else {
+        $error = 1;
+        $err = "National ID / Passport Number Cannot Be Empty";
+    }
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot Be Empty";
+    }
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $phone = mysqli_real_escape_string($mysqli, trim($_POST['phone']));
+    } else {
+        $error = 1;
+        $err = "Phone Number Cannot Be Empty";
+    }
+
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Students WHERE  email='$email' || phone ='$phone' || idno = '$idno' || admno ='$admno' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($email == $row['email']) {
+                $err =  "Account With This Email Already Exists";
+            } elseif ($admno == $row['admno']) {
+                $err = "Student Admission Number Already Exists";
+            } elseif ($idno == $row['idno']) {
+                $err = "National ID Number  / Passport Number Already Exists";
+            } else {
+                $err = "Account With That Phone Number Exists";
+            }
+        } else {
+            $faculty = $_GET['faculty'];
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $admno = $_POST['admno'];
+            $idno  = $_POST['idno'];
+            $adr = $_POST['adr'];
+            $dob = $_POST['dob'];
+            $gender = $_POST['gender'];
+            $acc_status = 'Active';
+            $created_at = date('d M Y');
+            $password = sha1(md5($_POST['password']));
+            $profile_pic = $_FILES['profile_pic']['name'];
+            move_uploaded_file($_FILES["profile_pic"]["tmp_name"], "dist/img/students/" . $_FILES["profile_pic"]["name"]);
+
+            $query = "INSERT INTO ezanaLMS_Students (id, faculty_id,  name, email, phone, admno, idno, adr, dob, gender, acc_status, created_at, password, profile_pic) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('ssssssssssssss', $id, $faculty,  $name, $email, $phone, $admno,  $idno, $adr, $dob, $gender, $acc_status, $created_at, $password,  $profile_pic);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Student Add " && header("refresh:1; url=add_student.php?faculty=$faculty");
+            } else {
+                //inject alert that profile update task failed
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
 /* Update Student */
 
 /* Delete Student */
@@ -162,7 +235,7 @@ require_once('public/partials/_head.php');
                                                     </button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    
+
                                                 </div>
                                                 <div class="modal-footer justify-content-between">
                                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -226,7 +299,53 @@ require_once('public/partials/_head.php');
                                                 <div class="col-12">
                                                     <div class="card">
                                                         <div class="card-body">
-                                                            
+                                                            <table id="example1" class="table table-bordered table-striped">
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th>#</th>
+                                                                        <th>Adm No</th>
+                                                                        <th>Name</th>
+                                                                        <th>Email</th>
+                                                                        <th>Phone</th>
+                                                                        <th>ID/Passport</th>
+                                                                        <th>Gender</th>
+                                                                        <th>Manage</th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <?php
+                                                                    $ret = "SELECT * FROM `ezanaLMS_Students` WHERE faculty_id = '$faculty->id'  ";
+                                                                    $stmt = $mysqli->prepare($ret);
+                                                                    $stmt->execute(); //ok
+                                                                    $res = $stmt->get_result();
+                                                                    $cnt = 1;
+                                                                    while ($std = $res->fetch_object()) {
+                                                                    ?>
+
+                                                                        <tr>
+                                                                            <td><?php echo $cnt; ?></td>
+                                                                            <td><?php echo $std->admno; ?></td>
+                                                                            <td><?php echo $std->name; ?></td>
+                                                                            <td><?php echo $std->email; ?></td>
+                                                                            <td><?php echo $std->phone; ?></td>
+                                                                            <td><?php echo $std->idno; ?></td>
+                                                                            <td><?php echo $std->gender; ?></td>
+                                                                            <td>
+                                                                                <a class="badge badge-primary" href="#update-student-<?php echo $std->id; ?>">
+                                                                                    <i class="fas fa-edit"></i>
+                                                                                    Update
+                                                                                </a>
+
+                                                                                <a class="badge badge-danger" href="faculty_students.php?delete=<?php echo $std->id; ?>&view=<?php echo $faculty->id; ?>">
+                                                                                    <i class="fas fa-trash"></i>
+                                                                                    Delete
+                                                                                </a>
+                                                                            </td>
+                                                                        </tr>
+                                                                    <?php $cnt = $cnt + 1;
+                                                                    } ?>
+                                                                </tbody>
+                                                            </table>
                                                         </div>
                                                     </div>
                                                 </div>
