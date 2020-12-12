0b12b35a91c0674ca2dc3b9c14edf9805616d987
MartMbithi <martdevelopers254@gmail.com>
2020-12-12 11:59:08 +0300

    CK Editor Bug Persists

--- departments.php
+++ departments.php
@@ -267,7 +267,7 @@ require_once('public/partials/_head.php');
                                                         <div class="row">
                                                             <div class="form-group col-md-12">
                                                                 <label for="exampleInputPassword1">Department Details</label>
-                                                                <textarea name="details" id="dep_details" rows="10" class="form-control"></textarea>
+                                                                <textarea name="details"  rows="10" class="form-control"></textarea>
                                                             </div>
                                                         </div>
                                                     </div>
