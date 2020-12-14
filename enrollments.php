0cb37a66c145d0832141372dbc4b03a7f3156453
MartMbithi <martdevelopers254@gmail.com>
2020-12-14 15:12:00 +0300

    Update On Enrollments

--- enrollments.php
+++ enrollments.php
@@ -61,7 +61,7 @@ if (isset($_POST['add_enroll'])) {
                 $err =  "Student $student_name Already Enrolled On $semester_enrolled ";
             }
         } else {
-            $faculty = $_GET['faculty'];
+            $faculty = $_POST['faculty'];
             $id = $_POST['id'];
             $code = $_POST['code'];
             $student_adm = $_POST['student_adm'];
@@ -75,12 +75,14 @@ if (isset($_POST['add_enroll'])) {
             $academic_year_enrolled = $_POST['academic_year_enrolled'];
             $module_name = $_POST['module_name'];
             $module_code = $_POST['module_code'];
+            /* Course ID */
+            $course_id = $_POST['course_id'];
             $query = "INSERT INTO ezanaLMS_Enrollments (id, faculty_id, code, student_adm, student_name, semester_enrolled, created_at, course_code, course_name, semester_start, semester_end, academic_year_enrolled, module_name, module_code) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             $stmt = $mysqli->prepare($query);
             $rc = $stmt->bind_param('ssssssssssssss', $id, $faculty, $code, $student_adm, $student_name, $semester_enrolled, $created_at, $course_code, $course_name, $semester_start, $semester_end, $academic_year_enrolled, $module_name, $module_code);
             $stmt->execute();
             if ($stmt) {
-                $success = "Student Enrolled" && header("refresh:1; url=add_student_enrollment.php?faculty=$faculty");
+                $success = "Student Enrolled" && header("refresh:1; url=enrollments.php?view=$course_id");
             } else {
                 $info = "Please Try Again Or Try Later";
             }
@@ -88,60 +90,10 @@ if (isset($_POST['add_enroll'])) {
     }
 }
 
-/*  Update Time Table*/
-
-if (isset($_POST['update_class'])) {
-    //Error Handling and prevention of posting double entries
-    $error = 0;
-    if (isset($_POST['classdate']) && !empty($_POST['classdate'])) {
-        $classdate = mysqli_real_escape_string($mysqli, trim($_POST['classdate']));
-    } else {
-        $error = 1;
-        $err = "Date Cannot Be Empty";
-    }
-    if (isset($_POST['classtime']) && !empty($_POST['classtime'])) {
-        $classtime = mysqli_real_escape_string($mysqli, trim($_POST['classtime']));
-    } else {
-        $error = 1;
-        $err = "Time Cannot Be Empty";
-    }
-    if (isset($_POST['classlocation']) && !empty($_POST['classlocation'])) {
-        $classlocation = mysqli_real_escape_string($mysqli, trim($_POST['classlocation']));
-    } else {
-        $error = 1;
-        $err = "Lecture Hall Be Empty";
-    }
-    if (isset($_POST['classlecturer']) && !empty($_POST['classlecturer'])) {
-        $classlecturer = mysqli_real_escape_string($mysqli, trim($_POST['classlecturer']));
-    } else {
-        $error = 1;
-        $err = "Lecturer Name Be Empty";
-    }
+/*  Update Enrollments*/
 
 
-
-    if (!$error) {
-        $id = $_POST['id'];
-        $classdate = $_POST['classdate'];
-        $classtime  = $_POST['classtime'];
-        $classlocation = $_POST['classlocation'];
-        $classlecturer = $_POST['classlecturer'];
-        $classname  = $_POST['classname'];
-        $classlink = $_POST['classlink'];
-        $course_id = $_POST['course_id'];
-        $query = "UPDATE ezanaLMS_TimeTable SET classdate =?, classtime =?, classlocation =?, classlecturer =?, classname =?, classlink =? WHERE id =?";
-        $stmt = $mysqli->prepare($query);
-        $rc = $stmt->bind_param('sssssss', $classdate, $classtime, $classlocation, $classlecturer, $classname, $classlink, $id);
-        $stmt->execute();
-        if ($stmt) {
-            $success = "Class Updated" && header("refresh:1; url=timetables.php?view=$course_id");
-        } else {
-            $info = "Please Try Again Or Try Later";
-        }
-    }
-}
-
-/* Delete Class */
+/* Delete Enrollment */
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
     $view = $_GET['view'];
