bee7a234dcabb19fd05274e489e50f45a3c0151b
MartMbithi <martdevelopers254@gmail.com>
2020-12-16 18:26:03 +0300

    Add Student Groups On Student Groups

--- student_groups.php
+++ student_groups.php
@@ -7,6 +7,67 @@ require_once('configs/codeGen.php');
 
 /* Add Students Groups  */
 
+if (isset($_POST['add_group'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['code']) && !empty($_POST['code'])) {
+        $code = mysqli_real_escape_string($mysqli, trim($_POST['code']));
+    } else {
+        $error = 1;
+        $err = "Group Code Cannot Be Empty";
+    }
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
+    } else {
+        $error = 1;
+        $err = "Group Name Cannot Be Empty";
+    }
+    /* 
+    if (isset($_POST['student_admn']) && !empty($_POST['student_admn'])) {
+        $student_admn = mysqli_real_escape_string($mysqli, trim($_POST['student_admn']));
+    } else {
+        $error = 1;
+        $err = "Student Admission Number Cannot Be Empty";
+    }
+    if (isset($_POST['student_name']) && !empty($_POST['student_name'])) {
+        $student_admn = mysqli_real_escape_string($mysqli, trim($_POST['student_name']));
+    } else {
+        $error = 1;
+        $err = "Student Name Number Cannot Be Empty";
+    } */
+
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Groups WHERE  code='$code'   ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($code == $row['code']) {
+                $err =  "Group With This Code Already Exists";
+            } /* elseif (($code  && $student_admn) == ($row['code'] && $row['student_admn'])) {
+                $err = "Student Already Added To Group";
+            } */
+        } else {
+            $id = $_POST['id'];
+            $module_id = $_POST['module_id'];
+            $name = $_POST['name'];
+            $code = $_POST['code'];
+            $created_at = date('d M Y');
+            $details = $_POST['details'];
+            $faculty = $_GET['faculty'];
+
+            $query = "INSERT INTO ezanaLMS_Groups (id, module_id, faculty_id, name, code, created_at, details) VALUES(?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssssss', $id, $module_id, $faculty, $name, $code, $created_at, $details);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Student Group  Added" && header("refresh:1; url=student_groups.php?$view=module_id");
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
 
 /* Update Students Groups  */
 
@@ -150,7 +211,7 @@ require_once('public/partials/_head.php');
                                         <input class="form-control mr-sm-2" type="search" name="query" placeholder="Module Name Or Code">
                                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                                     </form>
-                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Upload Class Recording</button>
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add Student Group</button>
                                     <div class="modal fade" id="modal-default">
                                         <div class="modal-dialog  modal-lg">
                                             <div class="modal-content">
