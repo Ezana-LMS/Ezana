1308f96f2b5bfbd689a0325e30c46489c37ed135
MartMbithi <martdevelopers254@gmail.com>
2020-12-16 17:59:23 +0300

    Upload Video Back End Logic

--- class_recordings.php
+++ class_recordings.php
@@ -6,6 +6,51 @@ check_login();
 require_once('configs/codeGen.php');
 
 /* Add Class Recordings  */
+if (isset($_POST['add_class_recording'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['class_name']) && !empty($_POST['class_name'])) {
+        $class_name = mysqli_real_escape_string($mysqli, trim($_POST['class_name']));
+    } else {
+        $error = 1;
+        $err = "Class Name Cannot Be Empty";
+    }
+    if (isset($_POST['lecturer_name']) && !empty($_POST['lecturer_name'])) {
+        $lecturer_name = mysqli_real_escape_string($mysqli, trim($_POST['lecturer_name']));
+    } else {
+        $error = 1;
+        $err = "Lectuer Name Cannot Be Empty";
+    }
+    if (isset($_POST['details']) && !empty($_POST['details'])) {
+        $details = mysqli_real_escape_string($mysqli, trim($_POST['details']));
+    } else {
+        $error = 1;
+        $err = "Video Transcription Cannot Be Empty";
+    }
+    if (!$error) {
+        $id = $_POST['id'];
+        $class_name = $_POST['class_name'];
+        $lecturer_name  = $_POST['lecturer_name'];
+        $external_link = $_POST['external_link'];
+        $details  = $_POST['details'];
+        $created_at  = date('d M Y');
+        $video = $_FILES['video']['name'];
+        move_uploaded_file($_FILES["video"]["tmp_name"], "public/uploads/EzanaLMSData/ClassVideos/" . $_FILES["video"]["name"]);
+        $faculty = $_POST['faculty'];
+        /* Module ID */
+        $view = $_POST['view'];
+
+        $query = "INSERT INTO ezanaLMS_ClassRecordings (id, faculty_id, class_name, lecturer_name, external_link, details, created_at, video) VALUES(?,?,?,?,?,?,?,?)";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssssssss', $id, $faculty, $class_name, $lecturer_name, $external_link, $details, $created_at, $video);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Class Recoding Added" && header("refresh:1; url=class_recordings.php?view=$view");
+        } else {
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
 
 
 /* Update Class Recordings   */
