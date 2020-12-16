8e5430af1cdae82132a3d5bc042bf024f86f4a5b
MartMbithi <martdevelopers254@gmail.com>
2020-12-16 18:42:42 +0300

    Intergrated Edit Form On Student Groups

--- student_groups.php
+++ student_groups.php
@@ -400,7 +400,37 @@ require_once('public/partials/_head.php');
                                                                             </div>
                                                                             <div class="modal-body">
                                                                                 <!-- Form -->
+                                                                                <form method="post" enctype="multipart/form-data" role="form">
+                                                                                    <div class="card-body">
+                                                                                        <div class="row">
+                                                                                            <div class="form-group col-md-6">
+                                                                                                <label for="">Group Name</label>
+                                                                                                <input type="text" value="<?php echo $g->name; ?>" required name="name" class="form-control" id="exampleInputEmail1">
+                                                                                                <input type="hidden" required name="id" value="<?php echo $g->id; ?>" class="form-control">
+                                                                                                <input type="hidden" required name="view" value="<?php echo $mod->id; ?>" class="form-control">
 
+                                                                                            </div>
+                                                                                            <div class="form-group col-md-6">
+                                                                                                <label for="">Group Number / Code</label>
+                                                                                                <input type="text" required name="code" value="<?php echo $g->code; ?>" class="form-control">
+                                                                                            </div>
+                                                                                        </div>
+                                                                                        <div class="row">
+                                                                                            <div class="form-group col-md-12">
+                                                                                                <label for="exampleInputPassword1">Group Description</label>
+                                                                                                <!-- Inline CK EDitor -->
+                                                                                                <script>
+                                                                                                    CKEDITOR.replace('<?php echo $g->id; ?>');
+                                                                                                </script>
+                                                                                                <!-- Inline CK Editoe -->
+                                                                                                <textarea required id="<?php echo $g->id; ?>" name="details" rows="10" class="form-control"><?php echo $g->details; ?></textarea>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                    <div class="card-footer text-right">
+                                                                                        <button type="submit" name="update_group" class="btn btn-primary">Update Group</button>
+                                                                                    </div>
+                                                                                </form>
                                                                             </div>
                                                                             <div class="modal-footer justify-content-between">
                                                                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
