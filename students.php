0f74505d85d33c421fb28da744564b6fd53bcede
MartMbithi <martdevelopers254@gmail.com>
2020-12-19 19:27:22 +0300

    Student Add

--- students.php
+++ students.php
@@ -4,9 +4,88 @@ require_once('configs/config.php');
 require_once('configs/checklogin.php');
 check_login();
 require_once('configs/codeGen.php');
-/* Add Lects */
-/* Update Lec */
-/* Delete Lec */
+/* Add STD */
+if (isset($_POST['add_student'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['admno']) && !empty($_POST['admno'])) {
+        $admno = mysqli_real_escape_string($mysqli, trim($_POST['admno']));
+    } else {
+        $error = 1;
+        $err = "Admission  Number Cannot Be Empty";
+    }
+    if (isset($_POST['idno']) && !empty($_POST['idno'])) {
+        $idno = mysqli_real_escape_string($mysqli, trim($_POST['idno']));
+    } else {
+        $error = 1;
+        $err = "National ID / Passport Number Cannot Be Empty";
+    }
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot Be Empty";
+    }
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $phone = mysqli_real_escape_string($mysqli, trim($_POST['phone']));
+    } else {
+        $error = 1;
+        $err = "Phone Number Cannot Be Empty";
+    }
+    if (isset($_POST['faculty']) && !empty($_POST['faculty'])) {
+        $faculty = mysqli_real_escape_string($mysqli, trim($_POST['faculty']));
+    } else {
+        $error = 1;
+        $err = "Faculty Cannot Be Empty";
+    }
+
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Students WHERE  email='$email' || phone ='$phone' || idno = '$idno' || admno ='$admno' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($email == $row['email']) {
+                $err =  "Account With This Email Already Exists";
+            } elseif ($admno == $row['admno']) {
+                $err = "Student Admission Number Already Exists";
+            } elseif ($idno == $row['idno']) {
+                $err = "National ID Number  / Passport Number Already Exists";
+            } else {
+                $err = "Account With That Phone Number Exists";
+            }
+        } else {
+            $faculty = $_POST['faculty'];
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $admno = $_POST['admno'];
+            $idno  = $_POST['idno'];
+            $adr = $_POST['adr'];
+            $dob = $_POST['dob'];
+            $gender = $_POST['gender'];
+            $acc_status = 'Active';
+            $created_at = date('d M Y');
+            $password = sha1(md5($_POST['password']));
+            $profile_pic = $_FILES['profile_pic']['name'];
+            move_uploaded_file($_FILES["profile_pic"]["tmp_name"], "public/uploads/UserImages/students/" . $_FILES["profile_pic"]["name"]);
+
+            $query = "INSERT INTO ezanaLMS_Students (id, faculty_id,  name, email, phone, admno, idno, adr, dob, gender, acc_status, created_at, password, profile_pic) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('ssssssssssssss', $id, $faculty,  $name, $email, $phone, $admno,  $idno, $adr, $dob, $gender, $acc_status, $created_at, $password,  $profile_pic);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Student Add " && header("refresh:1; url=students.php");
+            } else {
+                //inject alert that profile update task failed
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+/* Update Students */
+/* Delete sTUDENT */
 require_once('public/partials/_analytics.php');
 require_once('public/partials/_head.php');
 ?>
