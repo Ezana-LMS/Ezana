9369fd41b4dd2e7f42fa98377445b980fc659119
MartMbithi <martdevelopers254@gmail.com>
2020-12-19 17:59:40 +0300

    Fixed Navigation Bar

--- public/partials/_nav.php
+++ public/partials/_nav.php
@@ -8,16 +8,13 @@ while ($admin = $res->fetch_object()) {
 ?>
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Right navbar links -->
+        <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
             </li>
         </ul>
-        </div>
+        <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
             <!-- Notifications Dropdown Menu -->
             <li class="nav-item dropdown">
