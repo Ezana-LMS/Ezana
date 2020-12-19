eec94aba80707d70594b557280fe5fc762b56aa3
MartMbithi <martdevelopers254@gmail.com>
2020-12-19 18:24:16 +0300

    Student Groups - Assignments

--- student_groups_assignments.php
+++ student_groups_assignments.php
@@ -354,13 +354,15 @@ require_once('public/partials/_head.php');
                                                                                     <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill" href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home" aria-selected="true">Assignment Instructions</a>
                                                                                 </li>
                                                                                 <li class="nav-item">
-                                                                                    <a class="nav-link" id="custom-content-below-enrollment-tab" data-toggle="pill" href="#custom-content-below-add_member" role="tab" aria-controls="custom-content-below-notices" aria-selected="false">Groups Atttemps</a>
+                                                                                    <a class="nav-link" id="custom-content-below-enrollment-tab" data-toggle="pill" href="#custom-content-below-add_member" role="tab" aria-controls="custom-content-below-notices" aria-selected="false">Groups Assignment Atttemps</a>
                                                                                 </li>
                                                                             </ul>
                                                                             <div class="tab-content" id="custom-content-below-tabContent">
                                                                                 <div class="tab-pane fade show active" id="custom-content-below-home" role="tabpanel" aria-labelledby="custom-content-below-home-tab">
+                                                                                    <br>
                                                                                     <?php echo $gcode->details; ?>
                                                                                     <br>
+                                                                                    <small class="text-danger">Submission Deadline: <?php echo $gcode->submitted_on; ?></small>
                                                                                     <a target='_blank' href='public/uploads/EzanaLMSData/Group_Projects/<?php echo $gcode->attachments; ?>' class='btn btn-outline-success'>
                                                                                         Open Assignment
                                                                                     </a>
