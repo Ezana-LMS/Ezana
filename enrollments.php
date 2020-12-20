1af15762c27797e659783e4530cab7e1897dfbd3
MartMbithi <martdevelopers254@gmail.com>
2020-12-20 19:27:13 +0300

    Course Enrollments

--- enrollments.php
+++ enrollments.php
@@ -295,7 +295,7 @@ require_once('public/partials/_head.php');
                                     <ol class="breadcrumb float-sm-right">
                                         <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                         <li class="breadcrumb-item"><a href="courses.php">Courses</a></li>
-                                        <li class="breadcrumb-item active"><?php echo $course->name; ?> TT</li>
+                                        <li class="breadcrumb-item active"><?php echo $course->name; ?> Enrollments</li>
                                     </ol>
                                 </div>
                             </div>
@@ -466,11 +466,13 @@ require_once('public/partials/_head.php');
                                         <div class="col-md-12">
                                             <div class="card card-primary">
                                                 <div class="card-header">
-                                                    <h3 class="card-title"><?php echo $course->name; ?></h3>
-                                                    <div class="card-tools text-right">
-                                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                                                        </button>
-                                                    </div>
+                                                    <a href="course.php?view=<?php echo $course->id; ?>">
+                                                        <h3 class="card-title"><?php echo $course->name; ?></h3>
+                                                        <div class="card-tools text-right">
+                                                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                                                            </button>
+                                                        </div>
+                                                    </a>
                                                 </div>
 
                                                 <div class="card-body">
