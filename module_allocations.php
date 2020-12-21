6a58b1ab539c0da63102b808b62ffa66dadc5517
MartMbithi <martdevelopers254@gmail.com>
2020-12-21 11:36:00 +0300

    Delete On Allocations

--- module_allocations.php
+++ module_allocations.php
@@ -73,75 +73,25 @@ if (isset($_POST['assign_module'])) {
     }
 }
 
-/*  Update Modue Allocations*/
-if (isset($_POST['update_assign'])) {
-    //Error Handling and prevention of posting double entries
-    $error = 0;
-    if (isset($_POST['module_code']) && !empty($_POST['module_code'])) {
-        $module_code = mysqli_real_escape_string($mysqli, trim($_POST['module_code']));
-    } else {
-        $error = 1;
-        $err = "Module Code Cannot Be Empty";
-    }
-    if (isset($_POST['module_name']) && !empty($_POST['module_name'])) {
-        $module_name = mysqli_real_escape_string($mysqli, trim($_POST['module_name']));
-    } else {
-        $error = 1;
-        $err = "Module Name Cannot Be Empty";
-    }
-    if (isset($_POST['lec_id']) && !empty($_POST['lec_id'])) {
-        $lec_id = mysqli_real_escape_string($mysqli, trim($_POST['lec_id']));
-    } else {
-        $error = 1;
-        $err = "Lec ID Cannot Be Empty";
-    }
-    if (isset($_POST['lec_name']) && !empty($_POST['lec_name'])) {
-        $lec_name = mysqli_real_escape_string($mysqli, trim($_POST['lec_name']));
-    } else {
-        $error = 1;
-        $err = "Lec Name Cannot Be Empty";
-    }
-    if (!$error) {
-
-        $id = $_POST['id'];
-        $module_code = $_POST['module_code'];
-        $module_name = $_POST['module_name'];
-        $lec_id = $_POST['lec_id'];
-        $lec_name = $_POST['lec_name'];
-        $updated_at = date('d M Y');
-        $faculty = $_POST['faculty'];
-        $view = $_POST['view']; /* Module ID */
-
-        //On Assign, Update Module Status to Assigned
-        $ass_status = 1;
-
-        $query = "UPDATE ezanaLMS_ModuleAssigns  set  module_code =? , module_name =?, lec_id =?, lec_name =?, updated_at =? WHERE id =?";
-        $modUpdate = "UPDATE ezanaLMS_Modules SET ass_status =?  WHERE code = ?";
-        $stmt = $mysqli->prepare($query);
-        $modstmt = $mysqli->prepare($modUpdate);
-        $rc = $stmt->bind_param('ssssss',  $module_code, $module_name, $lec_id, $lec_name, $updated_at, $update);
-        $rc = $modstmt->bind_param('is', $ass_status, $module_code);
-        $stmt->execute();
-        $modstmt->execute();
-        if ($stmt && $modstmt) {
-            $success = "Module Assignment Updated"; //&& header("refresh:1; url=module_allocations.php?view=$view");
-        } else {
-            $info = "Please Try Again Or Try Later";
-        }
-    }
-}
-
 /* Delete Module Alloca */
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
+    $faculty = $_GET['faculty'];
+    $code = $_GET['code'];
     $view = $_GET['view'];
-    $adn = "DELETE FROM ezanaLMS_Modules WHERE id=?";
+    $status = 0;
+    $adn = "DELETE FROM ezanaLMS_ModuleAssigns WHERE id=?";
+    $up = "UPDATE ezanaLMS_Modules SET ass_status =? WHERE code =? ";
     $stmt = $mysqli->prepare($adn);
+    $upst = $mysqli->prepare($up);
     $stmt->bind_param('s', $delete);
+    $upst->bind_param('is', $status, $code);
     $stmt->execute();
+    $upst->execute();
+    $upst->close();
     $stmt->close();
-    if ($stmt) {
-        $success = "Deleted" && header("refresh:1; url=course_modules.php?view=$view");
+    if ($stmt && $upst) {
+        $success = "Deleted" && header("refresh:1; url=module_allocations.php?view=$view");
     } else {
         $info = "Please Try Again Or Try Later";
     }
