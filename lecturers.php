107909f6a04f7ce1d65ef29bb7ef481814de8dfb
MartMbithi <martdevelopers254@gmail.com>
2020-12-21 12:01:28 +0300

    Intergrated PHPOfice Package To Handle Data Import

--- lecturers.php
+++ lecturers.php
@@ -562,13 +562,30 @@ require_once('public/partials/_head.php');
                                 <div class="modal-dialog  modal-lg">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h4 class="modal-title">Fill All Values </h4>
+                                            <h4 class="modal-title text-danger">This Feature Is At Beta </h4>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <div class="modal-body">
-
+                                            <form method="post" enctype="multipart/form-data" role="form">
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="form-group col-md-12">
+                                                            <label for="exampleInputFile">Select File</label>
+                                                            <div class="input-group">
+                                                                <div class="custom-file">
+                                                                    <input required name="file" accept=".xls,.xlsx" type="file" class="custom-file-input" id="exampleInputFile">
+                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="card-footer">
+                                                    <button type="submit" name="upload" class="btn btn-primary">Upload File</button>
+                                                </div>
+                                            </form>
                                         </div>
                                         <div class="modal-footer justify-content-between">
                                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
