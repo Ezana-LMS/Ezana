445fef4641ffa186cc7d923a16a84ef325762069
MartMbithi <martdevelopers254@gmail.com>
2020-12-21 12:27:00 +0300

    Active Faculty Reports

--- reports.php
+++ reports.php
@@ -6,51 +6,6 @@ check_login();
 require_once('configs/codeGen.php');
 require_once('public/partials/_analytics.php');
 check_login();
-if (isset($_POST['add_faculty'])) {
-    //Error Handling and prevention of posting double entries
-    $error = 0;
-    if (isset($_POST['code']) && !empty($_POST['code'])) {
-        $code = mysqli_real_escape_string($mysqli, trim($_POST['code']));
-    } else {
-        $error = 1;
-        $err = "Faculty Code Cannot Be Empty";
-    }
-    if (isset($_POST['name']) && !empty($_POST['name'])) {
-        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
-    } else {
-        $error = 1;
-        $err = "Faculty Name Cannot Be Empty";
-    }
-    if (!$error) {
-        //prevent Double entries
-        $sql = "SELECT * FROM  ezanaLMS_Faculties WHERE  code='$code' || name ='$name' ";
-        $res = mysqli_query($mysqli, $sql);
-        if (mysqli_num_rows($res) > 0) {
-            $row = mysqli_fetch_assoc($res);
-            if ($code == $row['code']) {
-                $err =  "Faculty With This Code Already Exists";
-            } else {
-                $err = "Faculty Name Already Exists";
-            }
-        } else {
-            $id = $_POST['id'];
-            $name = $_POST['name'];
-            $code = $_POST['code'];
-            $details = $_POST['details'];
-
-            $query = "INSERT INTO ezanaLMS_Faculties (id, code, name, details) VALUES(?,?,?,?)";
-            $stmt = $mysqli->prepare($query);
-            $rc = $stmt->bind_param('ssss', $id, $code, $name, $details);
-            $stmt->execute();
-            if ($stmt) {
-                $success = "Faculty Added";
-            } else {
-                //inject alert that profile update task failed
-                $info = "Please Try Again Or Try Later";
-            }
-        }
-    }
-}
 require_once('public/partials/_head.php');
 ?>
 
@@ -187,7 +142,7 @@ require_once('public/partials/_head.php');
                             <div class="col-md-12">
                                 <div class="row">
                                     <div class="col-lg-4 col-6">
-                                        <a href="faculties.php">
+                                        <a href="reports_faculties.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Faculties</h3>
@@ -203,7 +158,7 @@ require_once('public/partials/_head.php');
                                         </a>
                                     </div>
                                     <div class="col-lg-4 col-6">
-                                        <a href="departments.php">
+                                        <a href="reports_departments.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Departments</h3>
@@ -220,7 +175,7 @@ require_once('public/partials/_head.php');
                                     </div>
 
                                     <div class="col-lg-4 col-6">
-                                        <a href="courses.php">
+                                        <a href="reports_courses.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Courses</h3>
@@ -237,7 +192,7 @@ require_once('public/partials/_head.php');
                                     </div>
 
                                     <div class="col-lg-4 col-6">
-                                        <a href="modules.php">
+                                        <a href="reports_modules.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Modules</h3>
@@ -254,7 +209,7 @@ require_once('public/partials/_head.php');
                                     </div>
 
                                     <div class="col-lg-4 col-6">
-                                        <a href="overall_school_calendar.php">
+                                        <a href="reports_overall_school_calendar.php">
 
                                             <div class="small-box bg-info">
                                                 <div class="inner">
@@ -270,8 +225,7 @@ require_once('public/partials/_head.php');
                                         </a>
                                     </div>
                                     <div class="col-lg-4 col-6">
-                                        <a href="overall_school_calendar.php">
-
+                                        <a href="reports_overall_school_timetable.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Time Table</h3>
@@ -286,7 +240,7 @@ require_once('public/partials/_head.php');
                                         </a>
                                     </div>
                                     <div class="col-lg-4 col-6">
-                                        <a href="overall_school_calendar.php">
+                                        <a href="reports_overall_school_enrollments.php">
 
                                             <div class="small-box bg-info">
                                                 <div class="inner">
@@ -302,8 +256,7 @@ require_once('public/partials/_head.php');
                                         </a>
                                     </div>
                                     <div class="col-lg-4 col-6">
-                                        <a href="lecturers.php">
-
+                                        <a href="reports_lecturers.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Lecturers</h3>
@@ -320,7 +273,7 @@ require_once('public/partials/_head.php');
                                     </div>
 
                                     <div class="col-lg-4 col-6">
-                                        <a href="students.php">
+                                        <a href="reports_students.php">
                                             <div class="small-box bg-info">
                                                 <div class="inner">
                                                     <h3>Students</h3>
---
+++ reports_faculties.php
@@ -0,0 +1,475 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('configs/codeGen.php');
+/* Import Student Details */
+
+check_login();
+
+use EzanaLmsAPI\DataSource;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+
+require_once('configs/DataSource.php');
+$db = new DataSource();
+$conn = $db->getConnection();
+require_once('vendor/autoload.php');
+
+
+if (isset($_POST["upload"])) {
+
+    $allowedFileType = [
+        'application/vnd.ms-excel',
+        'text/xls',
+        'text/xlsx',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ];
+
+    if (in_array($_FILES["file"]["type"], $allowedFileType)) {
+
+        $targetPath = 'EzanaLMSData/XLSFiles/' . $_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
+
+        $Reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+
+        $spreadSheet = $Reader->load($targetPath);
+        $excelSheet = $spreadSheet->getActiveSheet();
+        $spreadSheetAry = $excelSheet->toArray();
+        $sheetCount = count($spreadSheetAry);
+
+        for ($i = 0; $i <= $sheetCount; $i++) {
+
+            $id = "";
+            if (isset($spreadSheetAry[$i][0])) {
+                $id = mysqli_real_escape_string($conn, $spreadSheetAry[$i][0]);
+            }
+
+            $admno = "";
+            if (isset($spreadSheetAry[$i][1])) {
+                $admno = mysqli_real_escape_string($conn, $spreadSheetAry[$i][1]);
+            }
+            $name = "";
+            if (isset($spreadSheetAry[$i][2])) {
+                $name = mysqli_real_escape_string($conn, $spreadSheetAry[$i][2]);
+            }
+
+            $email = "";
+            if (isset($spreadSheetAry[$i][3])) {
+                $email = mysqli_real_escape_string($conn, $spreadSheetAry[$i][3]);
+            }
+
+            $password = "";
+            if (isset($spreadSheetAry[$i][4])) {
+                $password = mysqli_real_escape_string($conn, $spreadSheetAry[$i][4]);
+            }
+
+            $phone = "";
+            if (isset($spreadSheetAry[$i][5])) {
+                $phone = mysqli_real_escape_string($conn, $spreadSheetAry[$i][5]);
+            }
+
+            $adr = "";
+            if (isset($spreadSheetAry[$i][6])) {
+                $adr = mysqli_real_escape_string($conn, $spreadSheetAry[$i][6]);
+            }
+
+            $dob = "";
+            if (isset($spreadSheetAry[$i][7])) {
+                $dob = mysqli_real_escape_string($conn, $spreadSheetAry[$i][7]);
+            }
+
+            $idno = "";
+            if (isset($spreadSheetAry[$i][8])) {
+                $idno = mysqli_real_escape_string($conn, $spreadSheetAry[$i][8]);
+            }
+
+            $gender = "";
+            if (isset($spreadSheetAry[$i][9])) {
+                $gender = mysqli_real_escape_string($conn, $spreadSheetAry[$i][9]);
+            }
+
+            $acc_status = "";
+            if (isset($spreadSheetAry[$i][10])) {
+                $acc_status = mysqli_real_escape_string($conn, $spreadSheetAry[$i][10]);
+            }
+
+            $created_at = "";
+            if (isset($spreadSheetAry[$i][11])) {
+                $created_at = mysqli_real_escape_string($conn, $spreadSheetAry[$i][11]);
+            }
+            $faculty = "";
+            if (isset($spreadSheetAry[$i][12])) {
+                $faculty = mysqli_real_escape_string($conn, $spreadSheetAry[$i][12]);
+            }
+
+            if (!empty($name) || !empty($admno) || !empty($idno) || !empty($gender) || !empty($email)) {
+                $query = "INSERT INTO ezanaLMS_Students (id, faculty_id,  admno, name, email, password, phone, adr, dob, idno, gender, acc_status, created_at) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                $paramType = "sssssssssssss";
+                $paramArray = array(
+                    $id,
+                    $faculty,
+                    $admno,
+                    $name,
+                    $email,
+                    $password,
+                    $phone,
+                    $adr,
+                    $dob,
+                    $idno,
+                    $gender,
+                    $acc_status,
+                    $created_at
+                );
+                $insertId = $db->insert($query, $paramType, $paramArray);
+                // $query = "insert into tbl_info(name,description) values('" . $name . "','" . $description . "')";
+                // $result = mysqli_query($conn, $query);
+                if (!empty($insertId)) {
+                    $success = "Excel Data Imported into the Database";
+                } else {
+                    $success = "Excel Data Imported into the Database";
+                }
+            }
+        }
+    } else {
+        $info = "Invalid File Type. Upload Excel File.";
+    }
+}
+/* Add STD */
+if (isset($_POST['add_student'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['admno']) && !empty($_POST['admno'])) {
+        $admno = mysqli_real_escape_string($mysqli, trim($_POST['admno']));
+    } else {
+        $error = 1;
+        $err = "Admission  Number Cannot Be Empty";
+    }
+    if (isset($_POST['idno']) && !empty($_POST['idno'])) {
+        $idno = mysqli_real_escape_string($mysqli, trim($_POST['idno']));
+    } else {
+        $error = 1;
+        $err = "National ID / Passport Number Cannot Be Empty";
+    }
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot Be Empty";
+    }
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $phone = mysqli_real_escape_string($mysqli, trim($_POST['phone']));
+    } else {
+        $error = 1;
+        $err = "Phone Number Cannot Be Empty";
+    }
+    if (isset($_POST['faculty']) && !empty($_POST['faculty'])) {
+        $faculty = mysqli_real_escape_string($mysqli, trim($_POST['faculty']));
+    } else {
+        $error = 1;
+        $err = "Faculty Cannot Be Empty";
+    }
+
+    if (!$error) {
+        //prevent Double entries
+        $sql = "SELECT * FROM  ezanaLMS_Students WHERE  email='$email' || phone ='$phone' || idno = '$idno' || admno ='$admno' ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($email == $row['email']) {
+                $err =  "Account With This Email Already Exists";
+            } elseif ($admno == $row['admno']) {
+                $err = "Student Admission Number Already Exists";
+            } elseif ($idno == $row['idno']) {
+                $err = "National ID Number  / Passport Number Already Exists";
+            } else {
+                $err = "Account With That Phone Number Exists";
+            }
+        } else {
+            $faculty = $_POST['faculty'];
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $admno = $_POST['admno'];
+            $idno  = $_POST['idno'];
+            $adr = $_POST['adr'];
+            $dob = $_POST['dob'];
+            $gender = $_POST['gender'];
+            $acc_status = 'Active';
+            $created_at = date('d M Y');
+            $password = sha1(md5($_POST['password']));
+            $profile_pic = $_FILES['profile_pic']['name'];
+            move_uploaded_file($_FILES["profile_pic"]["tmp_name"], "public/uploads/UserImages/students/" . $_FILES["profile_pic"]["name"]);
+
+            $query = "INSERT INTO ezanaLMS_Students (id, faculty_id,  name, email, phone, admno, idno, adr, dob, gender, acc_status, created_at, password, profile_pic) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('ssssssssssssss', $id, $faculty,  $name, $email, $phone, $admno,  $idno, $adr, $dob, $gender, $acc_status, $created_at, $password,  $profile_pic);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Student Add " && header("refresh:1; url=students.php");
+            } else {
+                //inject alert that profile update task failed
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+/* Update Student */
+if (isset($_POST['update_student'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['admno']) && !empty($_POST['admno'])) {
+        $admno = mysqli_real_escape_string($mysqli, trim($_POST['admno']));
+    } else {
+        $error = 1;
+        $err = "Admission  Number Cannot Be Empty";
+    }
+    if (isset($_POST['idno']) && !empty($_POST['idno'])) {
+        $idno = mysqli_real_escape_string($mysqli, trim($_POST['idno']));
+    } else {
+        $error = 1;
+        $err = "National ID / Passport Number Cannot Be Empty";
+    }
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot Be Empty";
+    }
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $phone = mysqli_real_escape_string($mysqli, trim($_POST['phone']));
+    } else {
+        $error = 1;
+        $err = "Phone Number Cannot Be Empty";
+    }
+
+    if (!$error) {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $admno = $_POST['admno'];
+        $idno  = $_POST['idno'];
+        $adr = $_POST['adr'];
+        $dob = $_POST['dob'];
+        $gender = $_POST['gender'];
+        $acc_status = $_POST['acc_status'];
+        $updated_at = date('d M Y');
+        $profile_pic = $_FILES['profile_pic']['name'];
+        move_uploaded_file($_FILES["profile_pic"]["tmp_name"], "public/uploads/UserImages/students/" . $_FILES["profile_pic"]["name"]);
+
+        $query = "UPDATE ezanaLMS_Students SET name =?, email =?, phone =?, admno =?, idno =?, adr =?, dob =?, gender =?, acc_status =?, updated_at =?, profile_pic =? WHERE id =?";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssssssssssss', $name, $email, $phone, $admno,  $idno, $adr, $dob, $gender, $acc_status, $updated_at, $profile_pic, $id);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Updated Profile" && header("refresh:1; url=students.php");
+        } else {
+            //inject alert that profile update task failed
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
+
+/* Delete Student */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_Students WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=students.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+
+
+/* Update Student Passwords */
+if (isset($_POST['change_password'])) {
+    $error = 0;
+    if (isset($_POST['new_password']) && !empty($_POST['new_password'])) {
+        $new_password = mysqli_real_escape_string($mysqli, trim(sha1(md5($_POST['new_password']))));
+    } else {
+        $error = 1;
+        $err = "New Password Cannot Be Empty";
+    }
+    if (isset($_POST['confirm_password']) && !empty($_POST['confirm_password'])) {
+        $confirm_password = mysqli_real_escape_string($mysqli, trim(sha1(md5($_POST['confirm_password']))));
+    } else {
+        $error = 1;
+        $err = "Confirmation Password Cannot Be Empty";
+    }
+
+    if (!$error) {
+        if ($new_password != $confirm_password) {
+            $err = "Password Does Not Match";
+        } else {
+            $student = $_POST['student'];
+            $new_password  = sha1(md5($_POST['new_password']));
+            $query = "UPDATE ezanaLMS_Students SET  password =? WHERE id =?";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('ss', $new_password, $student);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Password Changed" && header("refresh:1; url=students.php");
+            } else {
+                $err = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+
+require_once('public/partials/_analytics.php');
+require_once('public/partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('public/partials/_nav.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="dashboard.php" class="brand-link">
+                <img src="public/dist/img/logo.png" alt="Ezana LMS Logo" class="brand-image img-circle elevation-3">
+                <span class="brand-text font-weight-light">Ezana LMS</span>
+            </a>
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item">
+                            <a href="dashboard.php" class=" nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="faculties.php" class=" nav-link">
+                                <i class="nav-icon fas fa-university"></i>
+                                <p>
+                                    Faculties
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="departments.php" class=" nav-link">
+                                <i class="nav-icon fas fa-building"></i>
+                                <p>
+                                    Departments
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="courses.php" class="nav-link">
+                                <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                                <p>
+                                    Courses
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="modules.php" class="nav-link">
+                                <i class="nav-icon fas fa-chalkboard"></i>
+                                <p>
+                                    Modules
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="lecturers.php" class="nav-link">
+                                <i class="nav-icon fas fa-user-tie"></i>
+                                <p>
+                                    Lecturers
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="students.php" class="nav-link">
+                                <i class="nav-icon fas fa-user-graduate"></i>
+                                <p>
+                                    Students
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item  active has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-cogs"></i>
+                                <p>
+                                    System Settings
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="reports.php" class="active nav-link">
+                                        <i class="fas fa-angle-right nav-icon"></i>
+                                        <p>Reports</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="data_backup.php" class="nav-link">
+                                        <i class="fas fa-angle-right nav-icon"></i>
+                                        <p>Data Backup</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="system_settings.php" class="nav-link">
+                                        <i class="fas fa-angle-right nav-icon"></i>
+                                        <p>Settings</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Faculty Reports</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="reports.php">System</a></li>
+                                <li class="breadcrumb-item"><a href="reports.php">Reports</a></li>
+                                <li class="breadcrumb-item active">Faculties</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
+                <section class="content">
+                    <div class="container-fluid">
+                        <hr>
+                        <div class="row">
+                            <div class="col-12">
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Main Footer -->
+                <?php require_once('public/partials/_footer.php'); ?>
+            </div>
+        </div>
+        <!-- ./wrapper -->
+        <?php require_once('public/partials/_scripts.php'); ?>
+</body>
+
+</html>
