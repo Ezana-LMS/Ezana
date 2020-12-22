67400f2d9d491b43bff2874ec7384ddbe073c891
MartMbithi <martdevelopers254@gmail.com>
2020-12-22 11:10:05 +0300

    Modal Delete On Departments

--- departments.php
+++ departments.php
@@ -441,10 +441,30 @@ require_once('public/partials/_head.php');
                                                             </div>
                                                             <!-- Update Department Modal -->
 
-                                                            <a class="badge badge-danger" href="departments.php?delete=<?php echo $dep->id; ?>">
+                                                            <a class="badge badge-danger" href="#delete-<?php echo $dep->id; ?>" data-toggle="modal">
                                                                 <i class="fas fa-trash"></i>
                                                                 Delete
                                                             </a>
+                                                            <!-- Delete Confirmation Modal -->
+                                                            <div class="modal fade" id="delete-<?php echo $dep->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h5 class="modal-title" id="exampleModalLabel">CONFIRM</h5>
+                                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                <span aria-hidden="true">&times;</span>
+                                                                            </button>
+                                                                        </div>
+                                                                        <div class="modal-body text-center text-danger">
+                                                                            <h4>Delete <?php echo $dep->name; ?> ?</h4>
+                                                                            <br>
+                                                                            <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                            <a href="departments.php?delete=<?php echo $dep->id; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- End Delete Confirmation Modal -->
                                                         </td>
                                                     </tr>
                                                 <?php $cnt = $cnt + 1;
