aaf3b26d07c37e2345cc1b9b509ec7f0c9d4f457
MartMbithi <martdevelopers254@gmail.com>
2020-12-23 14:56:22 +0300

    Dump / Restore Kinda Faile

--- system_settings.php
+++ system_settings.php
@@ -35,56 +35,9 @@ if (isset($_POST['systemSettings'])) {
 /* Back Up Database */
 if (isset($_POST['DumpDatabase'])) {
 }
+
 /* Restore Database */
 if (isset($_POST['RestoreDatabase'])) {
-    $dbHost     = 'localhost';
-    $dbUname = 'root';
-    $dbPass = '';
-    $dbName     = 'ezana_lms';
-    $targetPath = 'public/backup/' . $_FILES['restore_sql']['name'];
-    move_uploaded_file($_FILES['restore_sql']['tmp_name'], $targetPath);
-    $filePath   = $targetPath;
- 
-    if (file_exists($filePath)) {
-        importDatabaseTables($dbHost, $dbUname, $dbPass, $dbName, $filePath);
-    }
- 
-    function importDatabaseTables($dbHost, $dbUname, $dbPass, $dbName, $filePath)
-    {
-        // Connect & select the database
-        $db = new mysqli($dbHost, $dbUname, $dbPass, $dbName);
- 
-        // Temporary variable, used to store current query
-        $templine = '';
-    
-        // Read in entire file
-        $lines = file($filePath);
-    
-        $err = '';
-    
-        // Loop through each line
-        foreach ($lines as $line) {
-            // Skip it if it's a comment
-            if (substr($line, 0, 2) == '--' || $line == '') {
-                continue;
-            }
-        
-            // Add this line to the current segment
-            $templine .= $line;
-        
-            // If it has a semicolon at the end, it's the end of the query
-            if (substr(trim($line), -1, 1) == ';') {
-                // Perform the query
-                if (!$db->query($templine)) {
-                    $err .= 'Error importing query "<b>' . $templine . '</b>": ' . $db->error . '<br /><br />';
-                }
-            
-                // Reset temp variable to empty
-                $templine = '';
-            }
-        }
-        return !empty($error)?$error:true;
-    }
 }
 require_once('configs/codeGen.php');
 require_once('public/partials/_analytics.php');
