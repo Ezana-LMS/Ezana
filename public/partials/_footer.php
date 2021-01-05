6c483dc2436a12bf5543017a53c3cea4795dbb9f
MartMbithi <martdevelopers254@gmail.com>
2021-01-05 09:45:06 +0300

    Persistent Footer System Settings

--- public/partials/_footer.php
+++ public/partials/_footer.php
@@ -1,7 +1,19 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2020 - <?php echo date('Y'); ?> EzanaLMS. Powered By <a target="_blank" href="https://martdev.info/">MartDevelopers Inc</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
-    </div>
-</footer>
+<?php
+/* Persisit System Settings On Footer */
+$ret = "SELECT * FROM `ezanaLMS_Settings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+    <footer class="main-footer">
+        Copyright &copy; 2020 - <?php echo date('Y'); ?> <?php echo $sys->sysname; ?>.
+        Powered By <a target="_blank" href="https://ezana.org">EzanaLMS ORG</a>.
+        A <a target="_blank" href="https://ezana.org">MartDevelopers Inc </a> Production.
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> <?php echo $sys->version;?>
+        </div>
+    </footer>
+<?php
+} ?>
