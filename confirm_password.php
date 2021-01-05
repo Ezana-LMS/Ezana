c13da35a1b865a158c04a96c9c9178c7daaf3bbf
MartMbithi <martdevelopers254@gmail.com>
2021-01-05 17:59:51 +0300

    Persistent System Settings On Confirm Password

--- confirm_password.php
+++ confirm_password.php
@@ -41,57 +41,64 @@ if (isset($_POST['reset_pwd'])) {
         }
     }
 }
-include __DIR__ . "/public/partials/_authhead.php"
+include __DIR__ . "/public/partials/_authhead.php";
+/* Persisit System Settings On Auth */
+$ret = "SELECT * FROM `ezanaLMS_Settings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
 ?>
 
-<body style="background-color: #666666;">
-    <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100">
-                <form method="POST" class="login100-form validate-form">
-                    <div class="text-center">
-                        <img height="150" width="160" src="public/dist/img/logo.png" alt="wrapkit">
+    <body style="background-color: #666666;">
+        <div class="limiter">
+            <div class="container-login100">
+                <div class="wrap-login100">
+                    <form method="POST" class="login100-form validate-form">
+                        <div class="text-center">
+                            <img height="150" width="160" src="public/dist/img/<?php echo $sys->logo;?>" alt="wrapkit">
+                        </div>
+                        <h2 class="mt-3 text-center">Password Reset</h2>
+                        <?php
+                        $email  = $_SESSION['email'];
+                        $ret = "SELECT * FROM  ezanaLMS_Admins  WHERE email = '$email'";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        while ($row = $res->fetch_object()) {
+                        ?>
+                            <h5 class="mt-3 text-center">
+                                <span class="badge badge-success">
+                                    <small>
+                                        Token: <?php echo $row->password; ?>
+                                    </small>
+                                </span>
+                            </h5>
+                            <p class="text-center"><?php echo $row->name; ?> Please Enter New Password</p>
+                        <?php
+                        } ?> <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <input class="input100" type="password" name="new_password">
+                            <span class="focus-input100"></span>
+                            <span class="label-input100">New Password</span>
+                        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <input class="input100" type="password" name="confirm_password">
+                            <span class="focus-input100"></span>
+                            <span class="label-input100">Confirm Password</span>
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <button type="submit" name="reset_pwd" class="login100-form-btn">
+                                Confirm Password
+                            </button>
+                        </div>
+                    </form>
+                    <div class="login100-more" style="background-image: url('public/dist/img/<?php echo $sys->logo;?>');">
                     </div>
-                    <h2 class="mt-3 text-center">Password Reset</h2>
-                    <?php
-                    $email  = $_SESSION['email'];
-                    $ret = "SELECT * FROM  ezanaLMS_Admins  WHERE email = '$email'";
-                    $stmt = $mysqli->prepare($ret);
-                    $stmt->execute(); //ok
-                    $res = $stmt->get_result();
-                    while ($row = $res->fetch_object()) {
-                    ?>
-                        <h5 class="mt-3 text-center">
-                            <span class="badge badge-success">
-                                <small>
-                                    Token: <?php echo $row->password; ?>
-                                </small>
-                            </span>
-                        </h5>
-                        <p class="text-center"><?php echo $row->name; ?> Please Enter New Password</p>
-                    <?php
-                    } ?> <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input class="input100" type="password" name="new_password">
-                        <span class="focus-input100"></span>
-                        <span class="label-input100">New Password</span>
-                    </div>
-                    <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input class="input100" type="password" name="confirm_password">
-                        <span class="focus-input100"></span>
-                        <span class="label-input100">Confirm Password</span>
-                    </div>
-                    <div class="container-login100-form-btn">
-                        <button type="submit" name="reset_pwd" class="login100-form-btn">
-                            Confirm Password
-                        </button>
-                    </div>
-                </form>
-                <div class="login100-more" style="background-image: url('public/dist/img/logo.png');">
                 </div>
             </div>
         </div>
-    </div>
-    <?php require_once("public/partials/_authscripts.php"); ?>
-</body>
-
+        <?php require_once("public/partials/_authscripts.php"); ?>
+    </body>
+<?php
+} ?>
 </html>
