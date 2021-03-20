c12a3854981bfa90e9cb358a6639f2df72e92810
MartMbithi <martdevelopers254@gmail.com>
2021-03-20 12:15:17 +0300

    Logging Auth IP Address Is Buggy AF

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 session_start();
 include('configs/config.php');
 
-if (isset($_POST['login'])) {
+/* if (isset($_POST['login'])) {
     $email = $_POST['email'];
-    $password = sha1(md5($_POST['password']));
-    //query for match  the user inputs
-    $ret = mysqli_query($mysqli, "SELECT * FROM ezanaLMS_Admins WHERE email='$email'  and password='$password'");
+    $password = sha1(md5($_POST['password'])); // get password
+    $ret = mysqli_query($mysqli, "SELECT * FROM ezanaLMS_Admins WHERE email='$email'  AND  password='$password'");
     $num = mysqli_fetch_array($ret);
     // if user inputs match if condition will runn
     if ($num > 0) {
-        $_SESSION['id'] = $id;
+        $_SESSION['id'] = $num['id']; // hold the user id in session
         $_SESSION['email'] = $email;
         $_SESSION['name'] = $name;
         $uip = $_SERVER['REMOTE_ADDR']; // get the user ip
         // query for inser user log in to data base
-        mysqli_query($con, "INSERT  INTO ezanaLMS_UserLog (userId, username, userIp) values('" . $_SESSION['id'] . "','" . $_SESSION['email'] . "','$uip')");
+        mysqli_query($mysqli, "INSERT INTO ezanaLMS_UserLog (userId,username,userIp) values('" . $_SESSION['id'] . "','" . $_SESSION['email'] . "','$uip')");
         // code redirect the page after login
+        header("location:dashboard.php");
         $extra = "dashboard.php";
         $host = $_SERVER['HTTP_HOST'];
         $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
@@ -25,12 +25,38 @@ if (isset($_POST['login'])) {
     }
     // If the userinput no matched with database else condition will run
     else {
-        $err = "Invalid username or password";
-        $extra = "index.php";
+        $err =  "Invalid username or password";
+        /* $extra = "index.php";
         $host  = $_SERVER['HTTP_HOST'];
         $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         header("location:http://$host$uri/$extra");
+        exit(); 
+    }
+} */
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = sha1(md5($_POST['password'])); //double encrypt to increase security
+    $stmt = $mysqli->prepare("SELECT email, password, id, name  FROM ezanaLMS_Admins  WHERE email =? AND password =?");
+    $stmt->bind_param('ss', $email, $password); //bind fetched parameters
+    $stmt->execute(); //execute bind 
+    $stmt->bind_result($email, $password, $id, $name); //bind result
+    $rs = $stmt->fetch();
+    $_SESSION['id'] = $id;
+    $_SESSION['email'] = $email;
+    $_SESSION['name'] = $name;
+    $uip = $_SERVER['REMOTE_ADDR'];
+
+    if ($rs) {
+        mysqli_query($mysqli, "INSERT INTO ezanaLMS_UserLog (userId,username,userIp) values('" . $_SESSION['id'] . "','" . $_SESSION['email'] . "','$uip')");
+        $extra = "dashboard.php";
+        $host = $_SERVER['HTTP_HOST'];
+        $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        header("location:http://$host$uri/$extra");
         exit();
+        //header("location:dashboard.php");
+    } else {
+        $err = "Access Denied Please Check Your Credentials";
     }
 }
 include __DIR__ . "/public/partials/_authhead.php";
