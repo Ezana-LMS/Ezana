cbbb1cbb74bff45c21ffbddf240540f6d64c28c5
MartMbithi <martdevelopers254@gmail.com>
2021-03-22 12:44:32 +0300

    Persistent Eror Logs In Dashboard

--- dashboard.php
+++ dashboard.php
@@ -446,8 +446,7 @@ require_once('public/partials/_head.php');
                                                 <div class="card col-md-6">
                                                     <div class="card-header">
                                                         <h3 class="card-title">
-                                                            <i class="ion ion-clipboard mr-1"></i>
-                                                            Students Requests
+                                                            Requests
                                                         </h3>
 
                                                         <div class="card-tools">
@@ -464,121 +463,44 @@ require_once('public/partials/_head.php');
                                                     <div class="card-body">
                                                         <ul class="todo-list" data-widget="todo-list">
                                                             <li>
-                                                                <!-- drag handle -->
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
                                                                 <!-- checkbox -->
                                                                 <div class="icheck-primary d-inline ml-2">
                                                                     <input type="checkbox" value="" name="todo1" id="todoCheck1">
                                                                     <label for="todoCheck1"></label>
                                                                 </div>
                                                                 <!-- todo text -->
-                                                                <span class="text">Design a nice theme</span>
+                                                                <span class="text">Please Share Course Materials </span>
                                                                 <!-- Emphasis label -->
                                                                 <small class="badge badge-danger"><i class="far fa-clock"></i> 2 mins</small>
                                                                 <!-- General tools such as edit or delete-->
-                                                                <div class="tools">
+                                                                <!-- <div class="tools">
                                                                     <i class="fas fa-edit"></i>
                                                                     <i class="fas fa-trash-o"></i>
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
-                                                                <div class="icheck-primary d-inline ml-2">
-                                                                    <input type="checkbox" value="" name="todo2" id="todoCheck2" checked>
-                                                                    <label for="todoCheck2"></label>
-                                                                </div>
-                                                                <span class="text">Make the theme responsive</span>
-                                                                <small class="badge badge-info"><i class="far fa-clock"></i> 4 hours</small>
-                                                                <div class="tools">
-                                                                    <i class="fas fa-edit"></i>
-                                                                    <i class="fas fa-trash-o"></i>
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
-                                                                <div class="icheck-primary d-inline ml-2">
-                                                                    <input type="checkbox" value="" name="todo3" id="todoCheck3">
-                                                                    <label for="todoCheck3"></label>
-                                                                </div>
-                                                                <span class="text">Let theme shine like a star</span>
-                                                                <small class="badge badge-warning"><i class="far fa-clock"></i> 1 day</small>
-                                                                <div class="tools">
-                                                                    <i class="fas fa-edit"></i>
-                                                                    <i class="fas fa-trash-o"></i>
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
-                                                                <div class="icheck-primary d-inline ml-2">
-                                                                    <input type="checkbox" value="" name="todo4" id="todoCheck4">
-                                                                    <label for="todoCheck4"></label>
-                                                                </div>
-                                                                <span class="text">Let theme shine like a star</span>
-                                                                <small class="badge badge-success"><i class="far fa-clock"></i> 3 days</small>
-                                                                <div class="tools">
-                                                                    <i class="fas fa-edit"></i>
-                                                                    <i class="fas fa-trash-o"></i>
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
-                                                                <div class="icheck-primary d-inline ml-2">
-                                                                    <input type="checkbox" value="" name="todo5" id="todoCheck5">
-                                                                    <label for="todoCheck5"></label>
-                                                                </div>
-                                                                <span class="text">Check your messages and notifications</span>
-                                                                <small class="badge badge-primary"><i class="far fa-clock"></i> 1 week</small>
-                                                                <div class="tools">
-                                                                    <i class="fas fa-edit"></i>
-                                                                    <i class="fas fa-trash-o"></i>
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <span class="handle">
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                    <i class="fas fa-ellipsis-v"></i>
-                                                                </span>
-                                                                <div class="icheck-primary d-inline ml-2">
-                                                                    <input type="checkbox" value="" name="todo6" id="todoCheck6">
-                                                                    <label for="todoCheck6"></label>
-                                                                </div>
-                                                                <span class="text">Let theme shine like a star</span>
-                                                                <small class="badge badge-secondary"><i class="far fa-clock"></i> 1 month</small>
-                                                                <div class="tools">
-                                                                    <i class="fas fa-edit"></i>
-                                                                    <i class="fas fa-trash-o"></i>
-                                                                </div>
+                                                                </div> -->
                                                             </li>
                                                         </ul>
                                                     </div>
                                                     <!-- /.card-body -->
-                                                    <div class="card-footer clearfix">
+                                                    <!-- <div class="card-footer clearfix">
                                                         <button type="button" class="btn btn-info float-right"><i class="fas fa-plus"></i> Add item</button>
-                                                    </div>
+                                                    </div> -->
                                                 </div>
 
                                                 <div class="card col-md-6">
-                                                    <div class="card-head text-center">
-                                                        <br>
-                                                        <h4>User Login Activity Chart</h4>
+                                                    <div class="card-header text-center">
+                                                        <h3 class="card-title">
+                                                            Server Status, Database Status And Error Checkings
+                                                        </h3>
                                                     </div>
                                                     <div class="card-body">
-
+                                                        <?php
+                                                        $server_info = mysqli_get_server_info($mysqli);
+                                                        echo "Database Server: " . $server_info . "<br>";
+                                                        $array = explode("  ", mysqli_stat($mysqli));
+                                                        foreach ($array as $value) {
+                                                            echo "Server " . $value . "<br />";
+                                                        }
+                                                        ?>
                                                     </div>
                                                 </div>
                                             </div>
