a01e69bebf8f9745aaddf4a90189d99b328fd783
MartMbithi <martdevelopers254@gmail.com>
2021-04-22 03:25:37 +0300

    Dropdown Options on student current year

--- edu_admn_faculty_students.php
+++ edu_admn_faculty_students.php
@@ -277,7 +277,12 @@ require_once('public/partials/_head.php');
                                                                 </div>
                                                                 <div class="form-group col-md-6">
                                                                     <label for="">Current Year</label>
-                                                                    <input type="text" required name="current_year" class="form-control">
+                                                                    <select name="current_year" class='form-control basic'>
+                                                                        <option>1st Year </option>
+                                                                        <option>2nd Year </option>
+                                                                        <option>3rd Year </option>
+                                                                        <option>4th Year </option>
+                                                                    </select>
                                                                 </div>
                                                                 <div class="form-group col-md-6">
                                                                     <label for="">Date Enrolled</label>
@@ -428,7 +433,13 @@ require_once('public/partials/_head.php');
                                                                                             </div>
                                                                                             <div class="form-group col-md-6">
                                                                                                 <label for="">Current Year</label>
-                                                                                                <input type="text" required name="current_year" value="<?php echo $std->current_year; ?>" class="form-control">
+                                                                                                <select name="current_year" class='form-control basic'>
+                                                                                                    <option><?php echo $std->current_year; ?></option>
+                                                                                                    <option>1st Year </option>
+                                                                                                    <option>2nd Year </option>
+                                                                                                    <option>3rd Year </option>
+                                                                                                    <option>4th Year </option>
+                                                                                                </select>
                                                                                             </div>
                                                                                             <div class="form-group col-md-6">
                                                                                                 <label for="">Date Enrolled</label>
