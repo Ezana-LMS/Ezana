e645a8f59d2e8a8722daef2ef0f29953c4e9a1c7
MartMbithi <martdevelopers254@gmail.com>
2021-04-25 09:13:18 +0300

    Minor Course Fixes

--- edu_admn_reports_overall_module_grades.php
+++ edu_admn_reports_overall_module_grades.php
@@ -54,7 +54,7 @@ require_once('public/partials/_reportshead.php');
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0 text-dark"><?php echo $faculty->name; ?> Student Module Grades</h1>
+                            <h1 class="m-0 text-dark"><?php echo $faculty->name; ?> Courses</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
