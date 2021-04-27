9092070b0e7234360124c6b93ff748b60d4463b4
MartMbithi <martdevelopers254@gmail.com>
2021-04-27 09:29:44 +0300

    Deleted Assignments Attemps

---
+++ group_assignment_attempts.php
@@ -0,0 +1,280 @@
+<?php
+/*
+ * Created on Thu Apr 01 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('configs/codeGen.php');
+
+/* Delete Attempt  */
+if (isset($_GET['delete_assignment_attempt'])) {
+    $view = $_GET['view'];
+    $code = $_GET['code'];
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_GroupsAssignmentsGrades WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=group_details.php?view=$view&group=$group");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+require_once('public/partials/_analytics.php');
+require_once('public/partials/_head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php
+        require_once('public/partials/_nav.php');
+        $view = $_GET['view'];
+        $ret = "SELECT * FROM `ezanaLMS_Modules` WHERE id ='$view'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($mod = $res->fetch_object()) {
+            /* Group Details Based On Given Group ID */
+            $code = $_GET['code'];
+            $ret = "SELECT * FROM `ezanaLMS_Groups` WHERE code = '$code'  ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            $cnt = 1;
+            while ($g = $res->fetch_object()) {
+        ?>
+                <!-- Main Sidebar Container -->
+                <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                    <!-- Brand Logo -->
+                    <?php require_once('public/partials/_brand.php'); ?>
+                    <!-- Sidebar -->
+                    <div class="sidebar">
+                        <!-- Sidebar Menu -->
+                        <nav class="mt-2">
+                            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                                <li class="nav-item">
+                                    <a href="dashboard.php" class=" nav-link">
+                                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                                        <p>
+                                            Dashboard
+                                        </p>
+                                    </a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a href="faculties.php" class=" nav-link">
+                                        <i class="nav-icon fas fa-university"></i>
+                                        <p>
+                                            Faculties
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="departments.php" class=" nav-link">
+                                        <i class="nav-icon fas fa-building"></i>
+                                        <p>
+                                            Departments
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="courses.php" class=" nav-link">
+                                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                                        <p>
+                                            Courses
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="modules.php" class="active nav-link">
+                                        <i class="nav-icon fas fa-chalkboard"></i>
+                                        <p>
+                                            Modules
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="non_teaching_staff.php" class="nav-link">
+                                        <i class="nav-icon fas fa-user-secret"></i>
+                                        <p>
+                                            Non Teaching Staff
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="lecturers.php" class="nav-link">
+                                        <i class="nav-icon fas fa-user-tie"></i>
+                                        <p>
+                                            Lecturers
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="students.php" class="nav-link">
+                                        <i class="nav-icon fas fa-user-graduate"></i>
+                                        <p>
+                                            Students
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item has-treeview">
+                                    <a href="#" class="nav-link">
+                                        <i class="nav-icon fas fa-cogs"></i>
+                                        <p>
+                                            System Settings
+                                            <i class="right fas fa-angle-left"></i>
+                                        </p>
+                                    </a>
+                                    <ul class="nav nav-treeview">
+                                        <li class="nav-item">
+                                            <a href="reports.php" class="nav-link">
+                                                <i class="fas fa-angle-right nav-icon"></i>
+                                                <p>Reports</p>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="system_settings.php" class="nav-link">
+                                                <i class="fas fa-angle-right nav-icon"></i>
+                                                <p>System Settings</p>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </aside>
+
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <div class="container-fluid">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <h1 class="m-0 text-dark"><?php echo $g->name; ?> Asignment Attempts </h1>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="modules.php">Modules</a></li>
+                                        <li class="breadcrumb-item active"><?php echo $mod->name; ?></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+
+                        <section class="content">
+                            <div class="container-fluid">
+                                <div class="text-left">
+                                    <nav class="navbar navbar-light bg-light col-md-12">
+                                        <form class="form-inline" action="module_search_result.php" method="GET">
+                                            <input class="form-control mr-sm-2" type="search" name="query" placeholder="Module Name Or Code">
+                                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                        </form>
+                                    </nav>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <!-- Module Side Menu -->
+                                    <?php require_once('public/partials/_modulemenu.php'); ?>
+                                    <!-- Module Side Menu -->
+                                    <div class="col-md-9">
+                                        <div class="row">
+                                            <div class="col-md-12 col-lg-12">
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <table id="example1" class="table table-bordered table-striped">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th data-toggle="true">Group Details</th>
+                                                                    <th data-hide="all">Manage</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <?php
+                                                                $ret = "SELECT * FROM `ezanaLMS_GroupsAssignmentsGrades` WHERE group_code = '$code'   ";
+                                                                $stmt = $mysqli->prepare($ret);
+                                                                $stmt->execute(); //ok
+                                                                $res = $stmt->get_result();
+                                                                while ($attempts = $res->fetch_object()) {
+                                                                ?>
+                                                                    <tr>
+                                                                        <td><?php echo $attempts->group_code . " " . $attempts->group_name; ?></td>
+                                                                        <td>
+                                                                            <a target="_blank" href="public/uploads/EzanaLMSData/Group_Projects_Attemps/<?php echo $attempts->Submitted_Files; ?>" class="badge badge-secondary">
+                                                                                <i class="fas fa-download"></i>
+                                                                                Download Attachment
+                                                                            </a>
+                                                                            <!-- End Update Modal -->
+                                                                            <a class="badge badge-danger" data-toggle="modal" href="#delete-<?php echo $attempts->id; ?>">
+                                                                                <i class="fas fa-trash"></i>
+                                                                                Delete Attempt
+                                                                            </a>
+                                                                            <!-- Delete Confirmation Modal -->
+                                                                            <div class="modal fade" id="delete-<?php echo $attempts->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                                    <div class="modal-content">
+                                                                                        <div class="modal-header">
+                                                                                            <h5 class="modal-title" id="exampleModalLabel">CONFIRM</h5>
+                                                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                                <span aria-hidden="true">&times;</span>
+                                                                                            </button>
+                                                                                        </div>
+                                                                                        <div class="modal-body text-center text-danger">
+                                                                                            <h4>Delete?</h4>
+                                                                                            <br>
+                                                                                            <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                                            <a href="delete_assignment_attempt?delete=<?php echo $attempts->id; ?>view=<?php echo $mod->id; ?>&code=<?php echo $g->code; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <!-- End Delete Confirmation Modal -->
+                                                                        </td>
+
+                                                                    </tr>
+                                                                <?php $cnt = $cnt + 1;
+                                                                } ?>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- Main Footer -->
+                        <?php require_once('public/partials/_footer.php'); ?>
+                    </div>
+                </div>
+                <!-- ./wrapper -->
+        <?php
+            }
+        }
+        require_once('public/partials/_scripts.php'); ?>
+</body>
+
+</html>
