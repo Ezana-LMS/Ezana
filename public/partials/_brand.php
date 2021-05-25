b39c6c4c6242487c94c620568805cb8542cdc8e8
MartMbithi <martdevelopers254@gmail.com>
2021-05-25 15:19:49 +0300

    Minor Fixes On Brand

--- public/partials/_brand.php
+++ public/partials/_brand.php
@@ -7,7 +7,7 @@ $res = $stmt->get_result();
 while ($sys = $res->fetch_object()) {
 ?>
     <a href="" class="brand-link">
-        <img src="public/dist/img/<?php echo $sys->logo; ?>" alt="<?php echo $sys->name; ?> Logo" class="brand-image img-circle elevation-3">
+        <img src="public/dist/img/<?php echo $sys->logo; ?>" alt="<?php echo $sys->sysname; ?> Logo" class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light"><?php echo $sys->sysname; ?></span>
     </a>
 <?php
