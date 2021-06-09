9b98cf4b75781fb4cf8210753ecb861f586ab5b7
MartMbithi <martdevelopers254@gmail.com>
2021-06-09 14:36:16 +0300

    Implemented Landing Pages'

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Ezana LMS </title>
+    <!-- SEO META TAGS -->
+    <meta name="title" content="Ezana LMS">
+    <meta name="description" content="Ezana Learning Management System">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://ezana.org">
+    <meta property="og:title" content="Ezana LMS">
+    <meta property="og:description" content="Ezana Learning Management System">
+    <meta property="og:image" content="public/dist/img/logo.jpeg">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://ezana.org">
+    <meta property="twitter:title" content="Ezana LMS">
+    <meta property="twitter:description" content="Ezana Learning Management System">
+    <meta property="twitter:image" content="public/dist/img/logo.jpeg">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="public/dist/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/dist/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/dist/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="public/dist/img/favicons/site.webmanifest">
+    <link href="public/dist/landing_assets/css/robust.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-lg navbar-expand-lg navbar-transparant navbar-dark navbar-absolute w-100">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Ezana LMS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="admin_index.php">Admin Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="edu_admn_index.php">Educational Admin Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="lec_index.php">Lecturer Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="std_index.php">Student Portal</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="intro py-5 py-lg-9 position-relative text-white">
+        <div class="bg-overlay-primary">
+            <img src="" class="img-fluid img-cover" alt="Ezana LMS" />
+        </div>
+        <div class="intro-content py-6 text-center">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto text-center">
+                        <h1 class="my-3 display-4 d-none d-lg-inline-block">Ezana LMS</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="public/dist/landing_assets/js/bundle.js"></script>
+</body>
+
+</html>
