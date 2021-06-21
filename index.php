02ddfef6bc95ff146c2d3a2bbe8b9bbb96a6743e
MartMbithi <martdevelopers254@gmail.com>
2021-06-21 10:37:03 +0300

    Minor Fixes On Index Page

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Ezana LMS </title>
+    <!-- SEO META TAGS -->
+    <meta name="title" content="Ezana LMS">
+    <meta name="description" content="Ezana Learning Management System">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://ezana.org">
+    <meta property="og:title" content="Ezana LMS">
+    <meta property="og:description" content="Ezana Learning Management System">
+    <meta property="og:image" content="assets/img/logo.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://ezana.org">
+    <meta property="twitter:title" content="Ezana LMS">
+    <meta property="twitter:description" content="Ezana Learning Management System">
+    <meta property="twitter:image" content="assets/img/logo.png">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicons/site.webmanifest">
+    <link href="assets/css/robust.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-lg navbar-expand-lg navbar-transparant navbar-dark navbar-absolute w-100">
+        <div class="container">
+            <a class="navbar-brand" href="index">Ezana LMS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="admin/">Admin Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="admin/">Educational Admin Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="lecturer/">Lecturer Portal</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="student/">Student Portal</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="intro py-5 py-lg-9 position-relative text-white">
+        <div class="bg-overlay-primary">
+            <img src="assets/img/logo.png" class="img-fluid img-cover" alt="Ezana LMS" />
+        </div>
+        <div class="intro-content py-6 text-center">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto text-center">
+                        <h1 class="my-3 display-4 d-none d-lg-inline-block ">Ezana LMS</h1>
+                        <span class="h1 my-3 d-inline-block d-lg-none">Robust UI Kit</span>
+                        <p class="lead mb-3">Ezana is a premium, revolutionary e learning management system.</p>
+                        <br>
+                        <a class="btn btn-success btn-lg mr-lg-2 my-1" href="https://ezana.org" role="button">Get started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/bundle.js"></script>
+</body>
+
+</html>
