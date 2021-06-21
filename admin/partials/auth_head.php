cede4d5ce7e0a3aa409860fbc3b9f7dbd295fcda
MartMbithi <martdevelopers254@gmail.com>
2021-06-21 10:54:02 +0300

    Configured Auth Scripts

---
+++ admin/partials/auth_head.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <?php
+    /* Persisit System Settings On Auth */
+    $ret = "SELECT * FROM `ezanaLMS_Settings` ";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($sys = $res->fetch_object()) {
+    ?>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo $sys->sysname; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- SEO META TAGS -->
+        <meta name="title" content="Ezana LMS">
+        <meta name="description" content="Ezana Learning Management System">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://ezana.org">
+        <meta property="og:title" content="Ezana LMS">
+        <meta property="og:description" content="Ezana Learning Management System">
+        <meta property="og:image" content="../assets/img/logo.png">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://ezana.org">
+        <meta property="twitter:title" content="Ezana LMS">
+        <meta property="twitter:description" content="Ezana Learning Management System">
+        <meta property="twitter:image" content="../assets/img/logo.png">
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/img/favicons/favicon-16x16.png">
+        <link rel="manifest" href="assets/img/favicons/site.webmanifest">
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/plugins/animate/animate.css">
+        <link rel="stylesheet" type="text/css" href="../assets/plugins/css-hamburgers/hamburgers.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/plugins/animsition/css/animsition.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/css/util.css">
+        <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+        <!-- IziAlerts Css -->
+        <link rel="stylesheet" type="text/css" href="../assets/plugins/iziToast/iziToast.min.css">
+
+    <?php
+    } ?>
+</head>
---
+++ admin/partials/auth_scripts.php
@@ -0,0 +1,58 @@
+<script src="../assets/plugins/jquery/jquery-3.2.1.min.js"></script>
+<script src="../assets/plugins/animsition/js/animsition.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/popper.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/js/auth_js.js"></script>
+<!-- iZi Toast Js -->
+<script src="../assets/plugins/iziToast/iziToast.min.js" type="text/javascript"></script>
+<?php if (isset($success)) { ?>
+    <!--This code for injecting success alert-->
+    <script>
+        iziToast.success({
+            title: 'Success',
+            position: 'center',
+            transitionIn: 'flipInX',
+            transitionOut: 'flipOutX',
+            transitionInMobile: 'fadeInUp',
+            transitionOutMobile: 'fadeOutDown',
+            message: '<?php echo $success; ?>',
+        });
+    </script>
+
+<?php } ?>
+
+<?php if (isset($err)) { ?>
+    <!--This code for injecting error alert-->
+    <script>
+        iziToast.error({
+            title: 'Error',
+            timeout: 10000,
+            resetOnHover: true,
+            position: 'topRight',
+            transitionIn: 'flipInX',
+            transitionOut: 'flipOutX',
+            transitionInMobile: 'fadeInUp',
+            transitionOutMobile: 'fadeOutDown',
+            message: '<?php echo $err; ?>',
+        });
+    </script>
+
+<?php } ?>
+
+<?php if (isset($info)) { ?>
+    <!--This code for injecting info alert-->
+    <script>
+        iziToast.warning({
+            title: 'Warning',
+            position: 'topLeft',
+            transitionIn: 'flipInX',
+            transitionOut: 'flipOutX',
+            transitionIn: 'fadeInUp',
+            transitionInMobile: 'fadeInUp',
+            transitionOutMobile: 'fadeOutDown',
+            message: '<?php echo $info; ?>',
+        });
+    </script>
+
+<?php }
+?>
