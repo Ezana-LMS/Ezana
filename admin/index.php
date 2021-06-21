09b7d8ebe4fe993f962396ad9a0d8c52968942ac
MartMbithi <martdevelopers254@gmail.com>
2021-06-21 10:56:59 +0300

    Reconfigured Index Files

--- admin/index.php
+++ admin/index.php
@@ -64,6 +64,8 @@ if (isset($_POST['login'])) {
     }
 }
 
+/* Load Head*/
+require_once('partials/auth_head.php');
 
 /* Persisit System Settings On Auth */
 $ret = "SELECT * FROM `ezanaLMS_Settings` ";
@@ -79,7 +81,7 @@ while ($sys = $res->fetch_object()) {
                 <div class="wrap-login100">
                     <form method="POST" class="login100-form validate-form">
                         <div class="text-center">
-                            <img height="150" width="160" src="public/dist/img/<?php echo $sys->logo; ?>" alt="wrapkit">
+                            <img height="150" width="160" src="../Data/SystemLogo/<?php echo $sys->logo; ?>" alt="wrapkit">
                         </div>
                         <h2 class="mt-3 text-center">Log In</h2>
                         <p class="text-center">
@@ -100,12 +102,6 @@ while ($sys = $res->fetch_object()) {
                             <span class="label-input100">Password</span>
                         </div>
                         <div class="flex-sb-m w-full p-t-3 p-b-32">
-                            <!-- <div class="contact100-form-checkbox">
-                                <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-                                <label class="label-checkbox100" for="ckb1">
-                                    Remember me
-                                </label>
-                            </div> -->
                             <div>
                                 <a href="reset_password.php" class="txt1">
                                     Forgot Password?
@@ -118,12 +114,12 @@ while ($sys = $res->fetch_object()) {
                             </button>
                         </div>
                     </form>
-                    <div class="login100-more" style="background-image: url('public/dist/img/<?php echo $sys->logo; ?>');">
+                    <div class="login100-more" style="background-image: url('../Data/SystemLogo/<?php echo $sys->logo; ?>');">
                     </div>
                 </div>
             </div>
         </div>
-        <?php require_once("public/partials/_authscripts.php"); ?>
+        <?php require_once("partials/auth_scripts.php"); ?>
     </body>
 <?php
 } ?>
