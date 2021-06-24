10cebb3235949ba8ddca0a0ed82ab356d0b05dbe
MartMbithi <martdevelopers254@gmail.com>
2021-06-24 12:44:03 +0300

    Crafted Module Notices

--- admin/module.php
+++ admin/module.php
@@ -64,44 +64,6 @@ if (isset($_POST['update_module'])) {
     }
 }
 
-/* Add Module Notice */
-if (isset($_POST['add_notice'])) {
-    //Error Handling and prevention of posting double entries
-    $error = 0;
-    if (isset($_POST['module_name']) && !empty($_POST['module_name'])) {
-        $module_name = mysqli_real_escape_string($mysqli, trim($_POST['module_name']));
-    } else {
-        $error = 1;
-        $err = "Module Name Cannot Be Empty";
-    }
-    if (isset($_POST['module_code']) && !empty($_POST['module_code'])) {
-        $module_code = mysqli_real_escape_string($mysqli, trim($_POST['module_code']));
-    } else {
-        $error = 1;
-        $err = "Module Name Cannot Be Empty";
-    }
-    if (!$error) {
-        $id = $_POST['id'];
-        $announcements = $_POST['announcements'];
-        $created_by = $_POST['created_by'];
-        $faculty_id = $_POST['faculty_id'];
-        $module_id = $_POST['module_id'];
-
-        $attachments = $time . $_FILES['attachments']['name'];
-        move_uploaded_file($_FILES["attachments"]["tmp_name"], "../Data/Memos/" . $time . $_FILES["attachments"]["name"]);
-        $query = "INSERT INTO ezanaLMS_ModulesAnnouncements (id, module_name, module_code, announcements, created_by,attachments, faculty_id) VALUES(?,?,?,?,?,?,?)";
-        $stmt = $mysqli->prepare($query);
-        $rc = $stmt->bind_param('sssssss', $id, $module_name, $module_code, $announcements, $created_by, $attachments, $faculty_id);
-        $stmt->execute();
-        if ($stmt) {
-            $success = "$module_name Notice / Announcement Posted";
-        } else {
-            $info = "Please Try Again Or Try Later";
-        }
-    }
-}
-
-
 require_once('partials/head.php');
 ?>
 
