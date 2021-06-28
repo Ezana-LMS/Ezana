bc32c4174b08ef0f05573603489229e66036d248
MartMbithi <martdevelopers254@gmail.com>
2021-06-28 17:59:04 +0300

    Crafted System Settings Bulk Delete Modals

--- admin/system_bulk_delete.php
+++ admin/system_bulk_delete.php
@@ -61,7 +61,51 @@ require_once('partials/head.php');
                         <hr>
                         <div class="row">
                             <div class="col-md-12">
-                                
+                                <div class="card card-danger card-outline">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-5 col-sm-3">
+                                                <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
+                                                    <a class="nav-link active" data-toggle="pill" href="#faculties" role="tab">Faculties</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#departments" role="tab">Departments</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#courses" role="tab">Courses</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#modules" role="tab">Modules</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#non-teaching-staff" role="tab">Non Teaching Staff</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#lecturers" role="tab">Lecturers</a>
+                                                    <a class="nav-link" data-toggle="pill" href="#students" role="tab">Students</a>
+
+
+                                                </div>
+                                            </div>
+                                            <div class="col-7 col-sm-9">
+                                                <div class="tab-content" id="vert-tabs-tabContent">
+                                                    <div class="tab-pane text-left fade show active" id="faculties" role="tabpanel">
+                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada lacus ullamcorper dui molestie, sit amet congue quam finibus. Etiam ultricies nunc non magna feugiat commodo. Etiam odio magna, mollis auctor felis vitae, ullamcorper ornare ligula. Proin pellentesque tincidunt nisi, vitae ullamcorper felis aliquam id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin id orci eu lectus blandit suscipit. Phasellus porta, ante et varius ornare, sem enim sollicitudin eros, at commodo leo est vitae lacus. Etiam ut porta sem. Proin porttitor porta nisl, id tempor risus rhoncus quis. In in quam a nibh cursus pulvinar non consequat neque. Mauris lacus elit, condimentum ac condimentum at, semper vitae lectus. Cras lacinia erat eget sapien porta consectetur.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="departments" role="tabpanel">
+                                                        Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="courses" role="tabpanel">
+                                                        Morbi turpis dolor, vulputate vitae felis non, tincidunt congue mauris. Phasellus volutpat augue id mi placerat mollis. Vivamus faucibus eu massa eget condimentum. Fusce nec hendrerit sem, ac tristique nulla. Integer vestibulum orci odio. Cras nec augue ipsum. Suspendisse ut velit condimentum, mattis urna a, malesuada nunc. Curabitur eleifend facilisis velit finibus tristique. Nam vulputate, eros non luctus efficitur, ipsum odio volutpat massa, sit amet sollicitudin est libero sed ipsum. Nulla lacinia, ex vitae gravida fermentum, lectus ipsum gravida arcu, id fermentum metus arcu vel metus. Curabitur eget sem eu risus tincidunt eleifend ac ornare magna.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="modules" role="tabpanel">
+                                                        Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra. Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim. In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="non-teaching-staff" role="tabpanel">
+                                                        Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra. Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim. In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="lecturers" role="tabpanel">
+                                                        Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra. Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim. In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis.
+                                                    </div>
+                                                    <div class="tab-pane fade" id="students" role="tabpanel">
+                                                        Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra. Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim. In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /.card -->
+                                </div>
                             </div>
                         </div>
                     </div>
