7052ab40fd38f80787b89639ea8c775e8745145f
MartMbithi <martdevelopers254@gmail.com>
2021-06-29 13:21:22 +0300

    Crafted Educational Admin Dashboard

---
+++ educational_admin/dashboard.php
@@ -0,0 +1,268 @@
+<?php
+/*
+ * Created on Tue Jun 29 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+edu_admn_checklogin();
+require_once('../config/codeGen.php');
+require_once('partials/analytics.php');
+
+/* Delete User Requests */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM ezanaLMS_UserRequests WHERE id = ?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=dashboard");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+
+require_once('partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('partials/header.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('partials/aside.php');
+        /* Load This Page With Logged In User Session */
+        $id  = $_SESSION['id'];
+        $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id ='$id' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($admin = $res->fetch_object()) {
+        ?>
+
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark">Educational Administrator Dashboard</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                                    <li class="breadcrumb-item active">Dashboard</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+
+                    <section class="content">
+                        <div class="container-fluid">
+                            <hr>
+                            <div class="row">
+
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="row">
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_faculties.php?view=<?php echo $admin->school_id; ?>">
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Faculties</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-university"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $faculties; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_departments.php?view=<?php echo $admin->school_id; ?>">
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Departments</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-building"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $departments; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_courses.php?view=<?php echo $admin->school_id; ?>">
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Courses</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-chalkboard-teacher"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $courses; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_modules.php?view=<?php echo $admin->school_id; ?>">
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Modules</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-chalkboard"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $modules; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_overall_school_calendar.php?view=<?php echo $admin->school_id; ?>">
+
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Important Dates</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-calendar"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_lecturers.php?view=<?php echo $admin->school_id; ?>">
+
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Lecturers</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-user-tie"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $lecs; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                                <div class="col-lg-4 col-6">
+                                                    <a href="edu_admn_students.php?view=<?php echo $admin->school_id; ?>">
+                                                        <div class="small-box bg-info">
+                                                            <div class="inner">
+                                                                <h3>Students</h3>
+                                                            </div>
+                                                            <div class="icon">
+                                                                <i class="fas fa-user-graduate"></i>
+                                                            </div>
+                                                            <div class="small-box-footer">
+                                                                <i class="fas fa-arrow-circle-right"></i>
+                                                                <?php echo $students; ?>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                            </div>
+                                            <hr>
+                                            <div class="col-md-12">
+                                                <div class="row">
+                                                    <div class="card col-md-12">
+                                                        <div class="card-header">
+                                                            <h3 class="card-title">
+                                                                Users Requests
+                                                            </h3>
+                                                        </div>
+                                                        <!-- /.card-header -->
+                                                        <div class="card-body">
+                                                            <ul class="todo-list" data-widget="todo-list">
+                                                                <?php
+                                                                /* Load User Requests */
+                                                                $ret = "SELECT * FROM `ezanaLMS_UserRequests` ORDER BY `created_at` ASC   ";
+                                                                $stmt = $mysqli->prepare($ret);
+                                                                $stmt->execute(); //ok
+                                                                $res = $stmt->get_result();
+                                                                while ($req = $res->fetch_object()) {
+                                                                ?>
+                                                                    <li>
+                                                                        <a href="edu_admn_user_requests.php?view=<?php echo $req->id; ?>">
+                                                                            <span class="text"><?php echo $req->request; ?></span>
+                                                                            <small class="badge badge-success"><i class="far fa-clock"></i> <?php echo date('d M Y - g:ia', strtotime($req->created_at)); ?></small>
+                                                                            <div class="progress">
+                                                                                <div class="progress-bar" style="width: <?php echo $req->progress; ?>%"></div>
+                                                                            </div>
+                                                                            <span class="progress-description">
+                                                                                Progress : <?php echo $req->progress; ?> %
+                                                                            </span>
+                                                                        </a>
+                                                                    </li>
+                                                                    </a>
+                                                                <?php
+                                                                } ?>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <!-- Main Footer -->
+                    <?php require_once('partials/footer.php'); ?>
+                </div>
+            </div>
+            <!-- ./wrapper -->
+        <?php
+        }
+        require_once('partials/scripts.php');
+        ?>
+</body>
+
+</html>
--- educational_admin/partials/aside.php
+++ educational_admin/partials/aside.php
@@ -1,5 +1,4 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <?php
     /* Persisit System Settings On Brand */
     $ret = "SELECT * FROM `ezanaLMS_Settings` ";
@@ -9,117 +8,106 @@
     while ($sys = $res->fetch_object()) {
     ?>
         <a href="" class="brand-link">
-            <img src="../Data/SystemLogo/<?php echo $sys->logo; ?>" alt="<?php echo $sys->sysname; ?> Logo" class="brand-image img-circle elevation-3">
+            <img src="public/dist/img/<?php echo $sys->logo; ?>" alt="<?php echo $sys->sysname; ?> Logo" class="brand-image img-circle elevation-3">
             <span class="brand-text font-weight-light"><?php echo $sys->sysname; ?></span>
         </a>
     <?php
-    } ?>
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="dashboard" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
-                        <p>
-                            Dashboard
-                        </p>
-                    </a>
-                </li>
+    }
+    $id  = $_SESSION['id'];
+    $ret = "SELECT * FROM `ezanaLMS_Admins` WHERE id ='$id' ";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($admin = $res->fetch_object()) {
+    ?>
+        <div class="sidebar">
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="edu_admn_dashboard.php" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Dashboard
+                            </p>
+                        </a>
+                    </li>
 
-                <li class="nav-item">
-                    <a href="faculties" class=" nav-link">
-                        <i class="nav-icon fas fa-university"></i>
-                        <p>
-                            Faculties
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="departments" class="nav-link">
-                        <i class="nav-icon fas fa-building"></i>
-                        <p>
-                            Departments
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="courses" class="nav-link">
-                        <i class="nav-icon fas fa-chalkboard"></i>
-                        <p>
-                            Courses
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="modules" class="nav-link">
-                        <i class="nav-icon fas fa-cubes"></i>
-                        <p>
-                            Modules
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="non_teaching_staff" class="nav-link">
-                        <i class="nav-icon fas fa-user-tie"></i>
-                        <p>
-                            Non Teaching Staff
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="lecturers" class="nav-link">
-                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
-                        <p>
-                            Lecturers
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="students" class="nav-link">
-                        <i class="nav-icon fas fa-user-graduate"></i>
-                        <p>
-                            Students
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-cogs"></i>
-                        <p>
-                            System Settings
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="reports" class="nav-link">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>Reports</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="system_batch_addition" class="nav-link">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>Batch Addition</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="system_bulk_delete" class="nav-link">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>Delete Operations</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="system_settings" class="nav-link">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>System Settings</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-    </div>
+                    <li class="nav-item">
+                        <a href="edu_admn_faculties.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-university"></i>
+                            <p>
+                                Faculties
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edu_admn_departments.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-building"></i>
+                            <p>
+                                Departments
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edu_admn_courses.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                            <p>
+                                Courses
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edu_admn_modules.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-chalkboard"></i>
+                            <p>
+                                Modules
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edu_admn_lecturers.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-user-tie"></i>
+                            <p>
+                                Lecturers
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edu_admn_students.php?view=<?php echo $admin->school_id; ?>" class="nav-link">
+                            <i class="nav-icon fas fa-user-graduate"></i>
+                            <p>
+                                Students
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-cogs"></i>
+                            <p>
+                                System Settings
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="edu_admn_reports.php" class="nav-link">
+                                    <i class="fas fa-angle-right nav-icon"></i>
+                                    <p>Reports</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="edu_admn_system_settings.php" class="nav-link">
+                                    <i class="fas fa-angle-right nav-icon"></i>
+                                    <p>System Settings</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    <?php
+    }
+    ?>
 </aside>
