17aa87baa51999a2ed16b1bfd00df52bfabaa6db
MartMbithi <martdevelopers254@gmail.com>
2021-06-29 14:25:24 +0300

    Implemented Faculty Students

--- educational_admin/faculty_students.php
+++ educational_admin/faculty_students.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Created on Mon Jun 21 2021
+ * Created on Tue Jun 29 2021
  *
  * The MIT License (MIT)
  * Copyright (c) 2021 MartDevelopers Inc
@@ -20,11 +20,12 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
 session_start();
 require_once('../config/config.php');
-require_once('../config/checklogin.php');
+require_once('../config/edu_admn_checklogin.php');
 require_once('../config/codeGen.php');
-admin_checklogin();
+edu_admn_checklogin();
 
 /* Add Student */
 if (isset($_POST['add_student'])) {
@@ -173,7 +174,7 @@ require_once('partials/head.php');
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right small">
                                     <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="faculties">Faculties</a></li>
+                                    <li class="breadcrumb-item"><a href="faculty?view=<?php echo $view; ?>">Faculties</a></li>
                                     <li class="breadcrumb-item active">Students</li>
                                 </ol>
                             </div>
@@ -184,7 +185,7 @@ require_once('partials/head.php');
                         <div class="container-fluid">
                             <div class="col-md-12">
                                 <div class="text-center">
-                                    <h1 class="m-0 text-dark"><?php echo $faculty->name; ?> Students</h1>
+                                    <h1 class="m-0 text-bold"><?php echo $faculty->name; ?> Students</h1>
                                     <br>
                                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">Add Student</button>
 
