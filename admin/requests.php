6804778fefbe60ef78ce08d5aac53c0ead1f1d99
MartMbithi <martdevelopers254@gmail.com>
2021-06-30 11:46:19 +0300

    Minor Fixes On Requests

--- admin/requests.php
+++ admin/requests.php
@@ -72,7 +72,7 @@ require_once('partials/head.php');
                                 <h1 class="m-0 text-bold">User Request</h1>
                             </div>
                             <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
+                                <ol class="breadcrumb float-sm-right small">
                                     <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
                                     <li class="breadcrumb-item active">Requests</li>
                                 </ol>
